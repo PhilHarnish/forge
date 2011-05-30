248495f0d198df07e2e8d43dcd8d7f84a8e50f7d
Phil Harnish <philharnish@gmail.com>
2011-05-29 19:26:28 -0700

    Added steps for NPM install. Memoized jQuery builds.

--- bin/setup.sh
+++ bin/setup.sh
@@ -15,12 +15,19 @@ if [ -z "$(which node)" ]; then
   brew -v install node
 fi
 
+echo Install npm
+if [ -z "$(which npm)" ]; then
+  curl http://npmjs.org/install.sh | sh
+fi
+
 # Download and update submodules.
 git submodule update --init
 
 # Update jquery submodule.
 pushd third_party/jquery
 git submodule update --init
-make
+if [ ! -f dist/jquery.js ]; then
+  make
+fi
 popd
 
