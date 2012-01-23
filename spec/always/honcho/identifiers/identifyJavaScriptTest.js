5931c2e22cfeb9b7be0f7d450311ad6b910fcbea
Phil Harnish <philharnish@gmail.com>
2012-01-22 21:20:12 -0800

    Add identifyJavaScriptTest.js

---
+++ spec/always/honcho/identifiers/identifyJavaScriptTest.js
@@ -0,0 +1,26 @@
+var spec = require("../../../../test"),
+
+    fixtures = require("spec/revolver/fixtures"),
+    identifyJavaScript = require("src/always/honcho/identifiers/" +
+        "identifyJavaScript.js")["application/javascript"];
+
+describe("Identifies attributes", function () {
+  var type;
+  beforeEach(function () {
+    type = "application/javascript";
+  });
+
+  it("should identify NodeJS", function () {
+    var fileName = "events/EventDispatcher.js";
+    var result = identifyJavaScript(fileName,
+        fixtures.getFileContents(fileName), type);
+    expect(result).toEqual("application/javascript;nodejs=1");
+  });
+
+  it("should identify NodeJS tests.", function () {
+    var fileName = "tests/events/EventDispatcherTest.js";
+    var result = identifyJavaScript(fileName,
+        fixtures.getFileContents(fileName), type);
+    expect(result).toEqual("application/javascript;nodejs=1;test=1");
+  });
+});
