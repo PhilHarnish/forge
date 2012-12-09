80249d3e92fd3b8068baf6ee7cd22802386d4f37
Phil Harnish <philharnish@gmail.com>
2012-12-08 20:40:56 -0800

    Move items.js to sim-items.js to mirror mongolab. Also adds sim-locations.js

---
+++ spec/sim/model/fixtures/sim-locations.js
@@ -0,0 +1,33 @@
+exports.items = [
+  {
+    "_id":{
+      "$oid":"50b82462e4b0afba6ecc54b7"
+    },
+    "name":"locked door",
+    "memorable":true,
+    "x":0,
+    "y":10,
+    "z":0
+  },
+  {
+    "_id":{
+      "$oid":"50b82488e4b0afba6ecc54b8"
+    },
+    "name":"hostile",
+    "memorable":false,
+    "x":10,
+    "y":10,
+    "z":0
+  },
+  {
+    "_id":{
+      "$oid":"50b823fce4b0afba6ecc54b5"
+    },
+    "name":"base",
+    "memorable":true,
+    "x":0,
+    "y":0,
+    "z":0,
+    "memorize":true
+  }
+];
