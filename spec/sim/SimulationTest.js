54aa88d496333ab6009a0c60ac5a8daa32b55b26
Phil Harnish <philharnish@gmail.com>
2012-12-09 15:52:28 -0800

    Set up SimulationTest to use mocked HTTP responses.

--- spec/sim/SimulationTest.js
+++ spec/sim/SimulationTest.js
@@ -1,15 +1,37 @@
 var spec = require('../../test');
+var items = require("./model/fixtures/sim-items.js");
+var locations = require("./model/fixtures/sim-locations.js");
+var players = require("./model/fixtures/sim-players.js");
 
 describe('Simulation', function() {
+  var simulation;
+  var scope;
   beforeEach(register('sim/Simulation.js'));
+  beforeEach(items.mockRequests);
+  beforeEach(players.mockRequests);
+  beforeEach(locations.mockRequests);
+  afterEach(players.verifyRequests);
 
-  it('should resolve MongolabEndpoint factory.', function(Simulation) {
+  beforeEach(inject(function ($rootScope, $httpBackend, Simulation, Item,
+      Location, Player) {
+    scope = $rootScope.$new();
+    simulation = new Simulation(scope, Item, Location, Player);
+    $httpBackend.flush();
+  }));
+
+  it('should resolve Simulation factory.', function(Simulation) {
     expect(Simulation).toBeTruthy();
   });
 
-  it('should initialize "message"', function(Simulation) {
-    var mockScope = {};
-    var s = new Simulation(mockScope);
-    expect(mockScope.message).toBe("Hello world.");
+  it('should initialize "items"', function() {
+    expect(scope.items.length).toBeGreaterThan(0);
+  });
+
+  it('should initialize "locations"', function() {
+    expect(scope.locations.length).toBeGreaterThan(0);
+  });
+
+  it('should initialize "player"', function() {
+    expect(scope.player.initialized()).toBeTruthy();
   });
 });
--- src/sim/Simulation.js
+++ src/sim/Simulation.js
@@ -1,9 +1,11 @@
-angular.module('sim/Simulation.js', ['sim/model/Item.js']).
+angular.module('sim/Simulation.js', ['sim/model/Item.js', 'sim/model/Location.js', 'sim/model/Player.js']).
   factory('Simulation', function() {
       return Simulation;
     });
 
 // TODO(philharnish): Wrap object to avoid window pollution.
-function Simulation($scope) {
-  $scope.message = "Hello world.";
+function Simulation($scope, Item, Location, Player) {
+  $scope.items = Item.query();
+  $scope.locations = Location.query();
+  $scope.player = Player.get({id: "50b6f69be4b0dbae32c8ece1"});
 }
