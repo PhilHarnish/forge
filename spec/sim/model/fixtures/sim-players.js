62db28cb6815feef063ac0c56df6fdce4a1b72cb
Phil Harnish <philharnish@gmail.com>
2012-12-09 17:12:27 -0800

    sim-players fixture uses status fixture.

--- spec/sim/model/fixtures/sim-players.js
+++ spec/sim/model/fixtures/sim-players.js
@@ -1,15 +1,12 @@
 var MongolabFixture = require("./MongolabFixture.js");
+var status = require("./status.js");
 module.exports = MongolabFixture.exports("players", [
   {
     "_id": {
       "$oid": "50b6f69be4b0dbae32c8ece1"
     },
     "name": "philharnish",
-    "stats": {
-      "health": 50,
-      "hunger": 50,
-      "energy": 80
-    },
+    "stats": status.get(),
     "ui": {
       "mode": {
         "explore": "flight",
