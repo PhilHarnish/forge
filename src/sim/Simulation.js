aa229ab88e9a390f00bd456313b177879a854a62
Phil Harnish <philharnish@gmail.com>
2012-12-09 20:41:01 -0800

    Added tabs.

--- src/sim/Simulation.js
+++ src/sim/Simulation.js
@@ -11,6 +11,7 @@ angular.module("sim/Simulation.js", [
 // TODO(philharnish): Wrap object to avoid window pollution.
 function Simulation($scope, Item, Location, MongolabEndpoint, Player, Status) {
   MongolabEndpoint.$scope = $scope;
+  $scope.tabs = ["explore", "rest"];
   $scope.items = Item.query();
   $scope.locations = Location.query();
   $scope.player = Player.get({id: "50b6f69be4b0dbae32c8ece1"});
@@ -20,4 +21,7 @@ function Simulation($scope, Item, Location, MongolabEndpoint, Player, Status) {
       $scope.status.set($scope.player.stats);
     }
   });
+  $scope.modeIs = function(tab) {
+    return tab == "explore";
+  };
 }
