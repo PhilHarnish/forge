0ed157eff869c18ab214954f1067ed62b8173986
Phil Harnish <philharnish@gmail.com>
2012-12-09 20:28:54 -0800

    Rendering status bars.

--- src/sim/model/Player.js
+++ src/sim/model/Player.js
@@ -1,11 +1,6 @@
 // TODO(philharnish): Reusable library.
 angular.module('sim/model/Player.js', ['sim/model/MongolabEndpoint.js']).
     factory('Player', function(MongolabEndpoint) {
-      var STAT_CSS = {
-        health: "info",
-        hunger: "success",
-        energy: "warning"
-      };
       var Player = MongolabEndpoint('sim-players/:id');
 
       Player.prototype.initialized = function() {
--- src/sim/ui/Status.js
+++ src/sim/ui/Status.js
@@ -1,10 +1,18 @@
 angular.module('sim/ui/Status.js', []).
     factory('Status', function() {
-      var Status = function (stats) {
+      var STAT_CSS = {
+        health: "info",
+        hunger: "success",
+        energy: "warning"
+      };
+      var Status = function(stats) {
+        this.set(stats);
+      };
+      Status.prototype.set = function(stats) {
         this.stats = stats;
       };
-      Status.prototype.set = function (stats) {
-        this.stats = stats;
+      Status.prototype.progressClass = function(stat) {
+        return "progress progress-" + STAT_CSS[stat];
       };
       return Status;
     });
