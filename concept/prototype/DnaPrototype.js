857c5209c28d363fef6b9f02920f95cbfa54ea57
Phil Harnish <philharnish@gmail.com>
2013-01-06 23:26:38 -0800

    More iterations on dna prototype.

--- concept/prototype/DnaPrototype.js
+++ concept/prototype/DnaPrototype.js
@@ -2,16 +2,28 @@ angular.module("DnaPrototype.js", []).
     controller("DnaPrototype", DnaPrototype);
 
 function DnaPrototype($scope, $location) {
-  $scope.tabs = ["YAML", "Value", "Encoding"];
-  $scope.mode = "Value";
+  $scope.tabs = ["yaml", "value", "encoding"];
+  $scope.data = {
+    "yaml": "",
+    "value": [1, 1, 1, 1, 1, 1].join("\n"),
+    "encoding": ""
+  };
+  $scope.encoding = "";
   $scope.$watch(
       function() {
         // Wait until player is initialized before setting the mode.
         return $location.path().split("/").pop();
       },
       function(path) {
-        if (path) {
+        if (!path || !(path in $scope.data)) {
+          $location.path("/value");
+        } else {
+          update($scope.mode, path, $scope.data);
           $scope.mode = path;
         }
       });
+}
+
+function update(last, next, scope) {
+  //
 }
