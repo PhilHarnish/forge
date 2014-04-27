fdd521ee8cf5f32fd983b15eba79cef11d24ff19
Phil Harnish <philharnish@gmail.com>
2014-04-27 15:40:53 -0700

    Finished #1.
    Found an indentation style for tests gfmt and intellij agree on.

--- src/euler/go_euler/problem_1.go
+++ src/euler/go_euler/problem_1.go
@@ -1,1 +1,14 @@
 package go_euler
+
+func Problem1(ceil int, check ...int) int {
+	sum := 0
+	for i := 0; i < ceil; i++ {
+		for _, v := range check {
+			if i%v == 0 {
+				sum += i
+				break
+			}
+		}
+	}
+	return sum
+}
--- src/euler/go_euler/problem_1_test.go
+++ src/euler/go_euler/problem_1_test.go
@@ -6,8 +6,26 @@ import (
 	. "github.com/onsi/gomega"
 )
 
-var _ = Describe("Problem1", func() {
-	It("works?", func() {
-		Expect(WAT).To(Equal(2))
+var _ = Describe("Problem1",
+	func() {
+		It("exports correctly",
+			func() {
+				Expect(Problem1).NotTo(BeNil())
+			})
+
+		It("accepts variable args",
+			func() {
+				Expect(Problem1(0)).To(Equal(0))
+				Expect(Problem1(2, 1)).To(Equal(1))
+			})
+
+		It("returns example value",
+			func() {
+				Expect(Problem1(10, 3, 5)).To(Equal(23))
+			})
+
+		It("returns requested value",
+			func() {
+				Expect(Problem1(1000, 3, 5)).To(Equal(233168))
+			})
 	})
-})
