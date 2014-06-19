7eee83d04807bb40152ac040b2e1312db3c2386c
Phil Harnish <philharnish@gmail.com>
2014-06-18 21:00:20 -0700

    Fix typo.

--- src/euler/go_euler/problem_6.go
+++ src/euler/go_euler/problem_6.go
@@ -5,7 +5,7 @@ func Problem6(n int) int {
 	// Sum of squares (https://oeis.org/A000330).
 	// 1 + 4 + 9 + ... + n^2 = n*(n+1)*(2*n+1)/6
 	squareSum := n * (n + 1) * (2*n + 1) / 6
-	// Linear sum, squred:
+	// Linear sum, squared:
 	// 1 + 2 + 3 + 4 + 5 ... n = (n + 1) * (n / 2)
 	linearSum := (n + 1) * (n / 2)
 	linearSquare := linearSum * linearSum
