583bee9512de5481d4a9d3830788fdb81db2ce3f
Phil Harnish <philharnish@gmail.com>
2014-06-22 08:52:24 -0700

    Problem 14.
    Several variations tested. Best performance with fully cached array.
    Sparse maps (eg odd numbers only) did not perform as well.

---
+++ src/euler/go_euler/problem_14.go
@@ -0,0 +1,43 @@
+package go_euler
+
+func Problem14(n int) int {
+	lengths := make([]int, n)
+	lengths[1] = 1
+	largest := 0
+	longest := 1
+	history := make([]int, 1024) // ~550 needed for n = 1M.
+	for i := 2; i < n; i++ {
+		if lengths[i] > 0 {
+			// Already visited, return.
+			continue
+		}
+		current := i
+		// Truncate history.
+		history = history[:0]
+		distance := 0
+		// Iterate past any number greater than n until a known distance is found.
+		for current >= n || lengths[current] == 0 {
+			history = append(history, current)
+			distance++
+			if current%2 == 0 {
+				current /= 2
+			} else {
+				current = 3*current + 1
+			}
+		}
+		distance += lengths[current]
+		if distance > largest {
+			largest = distance
+			longest = i
+		}
+		for backfill := 0; backfill < len(history); backfill++ {
+			current = history[backfill]
+			if current < n {
+				// Only save distances less than n.
+				lengths[current] = distance
+			}
+			distance--
+		}
+	}
+	return longest
+}
---
+++ src/euler/go_euler/problem_14_test.go
@@ -0,0 +1,30 @@
+package go_euler_test
+
+import (
+	. "euler/go_euler"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Problem14",
+	func() {
+		It("should handle given example",
+			func() {
+				// 9: len([9 28 14 7 22 11 34 17 52 26 13 40 20 10 5 16 8 4 2]) = 20
+				Expect(Problem14(14)).To(Equal(9))
+			})
+
+		It("should handle problem asked",
+			func() {
+				Expect(Problem14(1000000)).To(Equal(837799))
+			})
+
+		XMeasure("with backfill, all",
+			func(b Benchmarker) {
+				b.Time("runtime",
+					func() {
+						output := Problem14(1000000)
+						Expect(output).To(Equal(837799))
+					})
+			}, 15)
+	})
