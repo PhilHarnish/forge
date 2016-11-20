7d0d8cbb67bf6e8cf9fb2fdf60c7ac60567902b5
Phil Harnish <philharnish@gmail.com>
2016-11-20 09:37:56 -0800

    Add rotatable alphabet.

---
+++ src/folding/alphabet.js
@@ -0,0 +1,23 @@
+// Base alphabet.
+const ALPHABET_STD = [
+    'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+    '       IH    ZO     C  XKN',
+    'VE GB DHI   WNO   S  AMX Z',
+    '  U    IH Y  ZO        X N'
+];
+
+// BE shared with MW.
+const ALPHABET_BEMW = [
+  'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+  ' W  M  IH   BZO     C EXKN',
+  'VE GB DHI   WNO   S  AMX Z',
+  ' MU W  IH Y EZO       BX N'
+];
+
+// BEMW + Q + FL + KTY + RJ.
+const ALPHABET_MAX = [
+  'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+  ' W  M  IH TFBZO Q   C EXKN',
+  'VE GB DHIR  WNO QJSY AMXTZ',
+  ' MU WL IH Y EZO Q  K  BX N'
+];
