e03db4cb55d0770b02614b4d8c50b1817d631712
Phil Harnish <philharnish@gmail.com>
2016-11-22 21:38:03 -0800

    Utility for translating characters.

---
+++ spec/util/stringTest.js
@@ -0,0 +1,38 @@
+const string = require('../../src/util/string.js');
+
+describe('string', () => {
+  it('exports string', () => {
+    expect(string).toBeDefined();
+  });
+
+  describe('mapAscii', () => {
+    it('should return same result for no-ops', () => {
+      const before = string.mapAscii('', '');
+      const after = string.mapAscii('', '');
+      expect(before).toEqual(after);
+    });
+
+    it('should return different results given input', () => {
+      const before = string.mapAscii('b', 'a');
+      const after = string.mapAscii('a', 'b');
+      expect(before).not.toEqual(after);
+    });
+  });
+
+  describe('translate', () => {
+    const NO_OP = string.mapAscii('', '');
+    const LEET = string.mapAscii('let', '137');
+
+    it('should be a no-op for empty strings', () => {
+      expect(string.translate('', '', '')).toEqual('');
+    });
+
+    it('should be a no-op for empty translations', () => {
+      expect(string.translate('example', NO_OP)).toEqual('example');
+    });
+
+    it('should translate characters', () => {
+      expect(string.translate('leet', LEET)).toEqual('1337');
+    });
+  });
+});
---
+++ src/util/string.js
@@ -0,0 +1,29 @@
+const generators = require('./generators.js');
+
+/** @const {string} 128 letter string where 0 = \0, 1 = \1 ... 65 = A, etc. */
+const INITIAL_ASCII_MAP_ = String.fromCharCode.apply(
+    null, generators.range(0, 128));
+
+module.exports = {
+  /**
+   * Intended for use with translate().
+   * @param {string} before Source characters to convert to `after`.
+   * @param {string} after Characters converted from `before`.
+   */
+  mapAscii(before, after) {
+    let result = INITIAL_ASCII_MAP_.split('');
+    for (let i = 0; i < before.length; i++) {
+      result[before.charCodeAt(i)] = after[i];
+    }
+    return result.join('');
+  },
+
+  translate(source, map) {
+    let result = source.split('');
+    let i = result.length;
+    while (i--) {
+      result[i] = map[source.charCodeAt(i)];
+    }
+    return result.join('');
+  },
+};
