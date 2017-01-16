19bbff4c940d628093ac14a2c7a4eab4ea692f4d
Phil Harnish <philharnish@gmail.com>
2017-01-16 13:28:14 -0800

    Fix alphabet exports.

--- src/folding/alphabet.js
+++ src/folding/alphabet.js
@@ -1,23 +1,26 @@
-// Base alphabet.
-const ALPHABET_STD = [
-  'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
-  '       IH    ZO     C  XKN',
-  'VE GB DHI   WNO   S  AMX Z',
-  '  U    IH Y  ZO        X N'
-];
 
-// BE shared with MW.
-const ALPHABET_BEMW = [
-  'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
-  ' W  M  IH   BZO     C EXKN',
-  'VE GB DHI   WNO   S  AMX Z',
-  ' MU W  IH Y EZO       BX N'
-];
+module.exports = {
+  // Base alphabet.
+  ALPHABET_STD: [
+    'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+    '       IH    ZO     C  XKN',
+    'VE GB DHI   WNO   S  AMX Z',
+    '  U    IH Y  ZO        X N'
+  ],
 
-// BEMW + Q + FL + KTY + RJ.
-const ALPHABET_MAX = [
-  'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
-  ' W  M  IH TFBZO Q   C EXKN',
-  'VE GB DHIR  WNO QJSY AMXTZ',
-  ' MU WL IH Y EZO Q  K  BX N'
-];
+  // BE shared with MW.
+  ALPHABET_BEMW: [
+    'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+    ' W  M  IH   BZO     C EXKN',
+    'VE GB DHI   WNO   S  AMX Z',
+    ' MU W  IH Y EZO       BX N'
+  ],
+
+  // BEMW + Q + FL + KTY + RJ.
+  ALPHABET_MAX: [
+    'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+    'aWcdMfgIHjTFBZO1QrstCvEXKN',
+    'VEuGBlDHIRtfWNO2QJSYcAMXTZ',
+    'vMUgWLdIHrYlEZO3QjsKuaBXtN'
+  ],
+};
