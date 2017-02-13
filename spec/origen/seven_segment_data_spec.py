82bbba45b736594e0af4a3fdb9490952928f2ffd
Phil Harnish <philharnish@gmail.com>
2017-02-12 17:18:51 -0800

    Update seven_segment_data to use data package.

--- spec/origen/seven_segment_data_spec.py
+++ spec/origen/seven_segment_data_spec.py
@@ -3,6 +3,10 @@ from src.origen import seven_segment_data
 
 
 with description('alphabet'):
-  with it('loads a the dictionary'):
-    alphabet = seven_segment_data.alphabet()
-    expect(alphabet).to(have_keys('A', 'a', 'b', 'C', 'c'))
+  with it('loads the alphabet'):
+    expect(seven_segment_data.ALPHABET).to(have_keys('A', 'a', 'b', 'C', 'c'))
+
+with description('words'):
+  with it('loads the dictionary'):
+    words = seven_segment_data.ACCESS_WORDS
+    expect(words).to(have_keys('POSITIVE', 'NEGATIVE', 'FALSE_POSITIVE'))
--- src/origen/seven_segment_data.py
+++ src/origen/seven_segment_data.py
@@ -1,17 +1,13 @@
-import os
-
+from src.origen import data
 from src.origen import seven_segment
 
-_alphabet = None
-_this_path = os.path.dirname(os.path.realpath(__file__))
-_base_path = os.path.join(_this_path.split('/forge/')[0], 'forge')
 
-def _read(project_path):
-  abs_path = os.path.join(_base_path, project_path)
-  return open(abs_path).read()
+class WordList(list):
 
-def alphabet():
-  global _alphabet
-  if not _alphabet:
-    _alphabet = seven_segment.load(_read('data/seven_segment_alphabet.txt'))
-  return _alphabet
+  def __init__(self, name, words):
+    super(WordList, self).__init__(words)
+    self.name = name
+
+
+ALPHABET = data.load('data/seven_segment_alphabet.txt', seven_segment.Glyph)
+ACCESS_WORDS = data.load('data/seven_segment_access_words.txt', WordList)
