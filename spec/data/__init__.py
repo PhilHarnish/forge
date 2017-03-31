3c3898e02f57f4f71c1b71d79026e6b7bae0d94c
Phil Harnish <philharnish@gmail.com>
2017-03-31 16:59:42 -0700

    Move 'project_path' to src.

--- spec/data/__init__.py
+++
@@ -1,7 +0,0 @@
-import os
-
-_this_path = os.path.dirname(os.path.realpath(__file__))
-_base_path = os.path.join(_this_path.split('/forge/')[0], 'forge')
-
-def project_path(path):
-  return os.path.join(_base_path, path)
