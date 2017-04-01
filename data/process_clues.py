d20a58b82ad6c7198fdadcd8104f4c29dec3f743
Phil Harnish <philharnish@gmail.com>
2017-03-31 19:09:08 -0700

    Script for parsing http://www.otsys.com/clue/ crossword DB.

---
+++ data/process_clues.py
@@ -0,0 +1,79 @@
+"""Process http://www.otsys.com/clue/ DB for use with python."""
+import collections
+import sqlite3
+
+from src.data import crossword
+from src.data import data
+
+STOP_WORD = '~'  # Appears after z.
+MAX_KEYWORDS = 50
+MIN_USAGES = 5
+
+VISITED = set()
+ALL_SEEN = collections.defaultdict(int)
+
+def _prune_keywords(keywords):
+  top = sorted(
+      keywords.items(), key=lambda i: i[1], reverse=True
+  )[:MAX_KEYWORDS]
+  results = []
+  for keyword, count in top:
+    ALL_SEEN[keyword] += count
+    results.append(keyword)
+    if keyword.endswith('s') and len(keyword) > 1:
+      # Singularize, just in case. (This won't always produce real words.)
+      results.append(keyword[:-1])
+  return results
+
+conn = sqlite3.connect(data.project_path('data/crossword.sqlite'))
+c = conn.cursor()
+# Erase previous table.
+c.execute('DROP TABLE IF EXISTS clues')
+# Create table.
+c.execute("""
+CREATE TABLE clues (
+  solution TEXT PRIMARY KEY,
+  usages INT,
+  keywords TEXT
+)
+""")
+conn.commit()
+
+def _insert(solution, usages, keywords):
+  try:
+    crossword.add(c, solution, usages, sorted(_prune_keywords(keywords)))
+  except (sqlite3.OperationalError, sqlite3.IntegrityError):
+    conn.commit()
+    conn.close()
+    raise
+
+last_solution = None
+keywords = collections.defaultdict(int)
+usages = 0
+for i, line in enumerate(
+    data.open_project_path('data/clues.txt', errors='ignore')):
+  solution, unused_int, unused_year, unused_source, clue = line.lower().split(
+      None, 4)
+  if solution > STOP_WORD:
+    print(line)
+    break
+  if solution in VISITED:
+    print('Skipping %s' % line)
+    continue
+  if last_solution and solution != last_solution:
+    if usages >= MIN_USAGES and keywords:
+      _insert(last_solution, usages, keywords)
+    VISITED.add(last_solution)
+    keywords.clear()
+    usages = 0
+  usages += 1
+  for keyword in crossword.clue_keywords(clue):
+    keywords[keyword] += 1
+  last_solution = solution
+
+_insert(last_solution, usages, keywords)
+
+conn.commit()
+conn.close()
+
+print(_prune_keywords(ALL_SEEN))
