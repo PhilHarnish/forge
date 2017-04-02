f8c4e4ddc5c2803d18ff4042998b213cf2e78eb2
Phil Harnish <philharnish@gmail.com>
2017-04-01 18:16:04 -0700

    More idiomatic ordered dict initialization.

--- src/puzzle/heuristics/analyze.py
+++ src/puzzle/heuristics/analyze.py
@@ -14,10 +14,8 @@ def identify(src):
     if score:
       scores[t] = t.score(src)
   # Return sorted values, highest first.
-  ordered = collections.OrderedDict()
-  for k, v in sorted(scores.items(), key=lambda x: x[1], reverse=True):
-    ordered[k] = v
-  return ordered
+  return collections.OrderedDict(
+      sorted(scores.items(), key=lambda x: x[1], reverse=True))
 
 
 def init():
