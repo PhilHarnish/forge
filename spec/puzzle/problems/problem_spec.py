628a299713e8758388d35f3d2df22cd60405fd05
Phil Harnish <philharnish@gmail.com>
2017-04-01 19:05:10 -0700

    Problem objects return solutions.

--- spec/puzzle/problems/problem_spec.py
+++ spec/puzzle/problems/problem_spec.py
@@ -1,7 +1,17 @@
 from expects import *
+from unittest import mock
 
 from src.puzzle.problems import problem
 
 with description('Problem'):
   with it('instantiates'):
     expect(problem.Problem('example', [''])).not_to(be_none)
+
+  with it('caches solutions'):
+    class ExampleProblem(problem.Problem):
+      _solve = mock.Mock(return_value={'solution': 1})
+    ex = ExampleProblem('example', [])
+    expect(ex.solutions()).to(equal({'solution': 1}))
+    expect(ex._solve.call_count).to(equal(1))
+    ex.solutions()
+    expect(ex._solve.call_count).to(equal(1))
--- src/puzzle/problems/problem.py
+++ src/puzzle/problems/problem.py
@@ -3,3 +3,12 @@ class Problem(object):
     self.name = name
     self.lines = lines
 
+    self._solutions = None
+
+  def solutions(self):
+    if self._solutions is None:
+      self._solutions = self._solve()
+    return self._solutions
+
+  def _solve(self):
+    raise NotImplementedError()
