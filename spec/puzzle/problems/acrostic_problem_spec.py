93b3c9ca03f5712940851e66e7487f61f9827945
Phil Harnish <philharnish@gmail.com>
2017-04-16 15:39:09 -0700

    Added AcrosticProblem identification (but not solving).

---
+++ spec/puzzle/problems/acrostic_problem_spec.py
@@ -0,0 +1,30 @@
+from expects import *
+
+from src.puzzle.problems import acrostic_problem
+
+
+with description('AcrosticProblem'):
+  with it('ignores empty and garbage input'):
+    expect(acrostic_problem.AcrosticProblem.score([''])).to(equal(0))
+
+  with it('rejects one line'):
+    expect(acrostic_problem.AcrosticProblem.score(
+        ['A quick brown fox jumps over the lazy dog'])).to(equal(0))
+
+  with it('scores lines with multiple words low'):
+    expect(acrostic_problem.AcrosticProblem.score([
+        'dog house', 'cat', 'bat',
+    ])).to(be_below(acrostic_problem.AcrosticProblem.score([
+        'doghouse', 'cat', 'bat',
+    ])))
+
+  with it('favors words with similar lengths'):
+    expect(acrostic_problem.AcrosticProblem.score([
+        'babbling', 'bachelor', 'backbone', 'backward', 'bacteria',
+        'baffling', 'balanced', 'baldness', 'ballroom', 'bankrupt',
+    ])).to(equal(1))
+
+  with it('penalizes few words'):
+    expect(acrostic_problem.AcrosticProblem.score([
+        'babbling', 'bachelor', 'backbone',
+    ])).to(be_below(1))
---
+++ src/puzzle/problems/acrostic_problem.py
@@ -0,0 +1,35 @@
+import statistics
+
+from src.puzzle.problems import problem
+
+class AcrosticProblem(problem.Problem):
+  @staticmethod
+  def score(lines):
+    if len(lines) <= 1:
+      return 0
+    num_words = 0
+    line_lengths = []
+    for line in lines:
+      num_words += line.count(' ') + 1
+      line_lengths.append(len(line))
+    max_line_length = max(line_lengths)
+    # Return a perfect score if:
+    # - There are more than 4 words.
+    # - Average line length is >4 letters.
+    # - The words have identical lengths.
+    # - There aren't spaces.
+    # Max .5 for 2 lines; .75 for 3 lines; 1.0 for 4+ lines.
+    num_lines_weight = min(4, len(lines)) / 4
+    # 1.0 if all lines >4; else ratio of lines >4.
+    line_length_weight = len(lines) / sum([1 for l in line_lengths if l > 4])
+    # Normalize by line length to punish large absolute swings in lengths and
+    # forgive +/- 1 character changes for already-long words.
+    line_stddev = statistics.stdev([l / max_line_length for l in line_lengths])
+    stddev_weight = 1 - line_stddev
+    # Punish results with multiple words per line.
+    line_length_weight = len(lines) / num_words
+    return (num_lines_weight * line_length_weight * stddev_weight *
+            line_length_weight)
+
+  def _solve(self):
+    return {}
