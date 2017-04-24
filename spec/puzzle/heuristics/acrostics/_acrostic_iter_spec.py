280a28073c5852e05722cedad9ac871564b6cf57
Phil Harnish <philharnish@gmail.com>
2017-04-23 18:06:23 -0700

    Acristics can find multi-word solutions.

--- spec/puzzle/heuristics/acrostics/_acrostic_iter_spec.py
+++ spec/puzzle/heuristics/acrostics/_acrostic_iter_spec.py
@@ -1,4 +1,7 @@
+import collections
+
 import mock
+from mock.mock import patch
 
 from spec.mamba import *
 from spec.data.fixtures import tries
@@ -6,67 +9,92 @@ from puzzle.heuristics.acrostics import _acrostic_iter
 from data import word_frequencies
 
 BA_PREFIX_TRIE = word_frequencies.load(
-    zip(('bad', 'bag', 'ban', 'bar', 'bat'), [1]*5))
+    zip(('bad', 'bag', 'ban', 'bar', 'bat'), range(1, 6)))
 
 
 with description('acrostic'):
-  with it('uses a mock trie'):
-    a = _acrostic_iter.Acrostic(['a'], tries.letters())
-    expect(len(a._trie)).to(be_below(100))
+  with description('basics'):
+    with it('uses a mock trie'):
+      a = _acrostic_iter.Acrostic(['a'], tries.letters())
+      expect(len(a._trie)).to(be_below(100))
 
-  with it('yields simple solutions'):
-    a = _acrostic_iter.Acrostic(['a'], tries.letters())
-    expect(list(a)).to(contain('a'))
-
-  with it('is observable'):
-    a = _acrostic_iter.Acrostic(['a'], tries.letters())
-    subs = mock.Mock()
-    a.subscribe(subs)
-    expect(subs.on_next.call_args).to(equal(mock.call('a')))
-
-  with it('yields multi-character solutions'):
-    a = _acrostic_iter.Acrostic(list('bag'), BA_PREFIX_TRIE)
-    expect(list(a)).to(contain('bag'))
-
-  with it('yields unique solutions'):
-    a = _acrostic_iter.Acrostic(list('ba') + ['ggg'], BA_PREFIX_TRIE)
-    expect(list(a)).to(have_len(1))
-
-  with it('yields multiple multi-character solutions'):
-    a = _acrostic_iter.Acrostic(list('ba') + ['dgnrt'], BA_PREFIX_TRIE)
-    expect(list(a)).to(contain('bad', 'bag', 'ban', 'bar', 'bat'))
-
-  with context('benchmark'):
-    with it('finds simple solutions quickly'):
+    with it('yields simple solutions'):
       a = _acrostic_iter.Acrostic(['a'], tries.letters())
       expect(list(a)).to(contain('a'))
-      expect(a.cost()).to(equal(1))
 
-    with it('ignores duplicate characters'):
-      a = _acrostic_iter.Acrostic([
-        'bbbbbb',
-        'a',
-        'g',
-      ], BA_PREFIX_TRIE)
-      expect(list(a)).to(contain('bag'))
-      expect(a.cost()).to(equal(1))
+    with it('is observable'):
+      a = _acrostic_iter.Acrostic(['a'], tries.letters())
+      subs = mock.Mock()
+      a.subscribe(subs)
+      expect(subs.on_next.call_args).to(equal(mock.call('a')))
 
-    with it('ignores invalid words'):
-      a = _acrostic_iter.Acrostic([
-        'b',
-        'a',
-        'abcefg',
-      ], BA_PREFIX_TRIE)
-      expect(list(a)).not_to(contain('baa', 'bab', 'bac', 'bae', 'baf'))
+    with it('yields multi-character solutions'):
+      a = _acrostic_iter.Acrostic(list('bag'), BA_PREFIX_TRIE)
       expect(list(a)).to(contain('bag'))
-      expect(a.cost()).to(be_above_or_equal(1))
-      expect(a.cost()).to(be_below(10))
 
-    with it('skips invalid prefixes'):
-      a = _acrostic_iter.Acrostic([
-        'b',
-        'abcdefghijklmnopqrstuvwxyz',
-        'g',
-      ], BA_PREFIX_TRIE)
-      expect(list(a)).to(contain('bag'))
-      expect(a.cost()).to(be_below(10))
+    with it('yields unique solutions'):
+      a = _acrostic_iter.Acrostic(list('ba') + ['ggg'], BA_PREFIX_TRIE)
+      expect(list(a)).to(have_len(1))
+
+    with it('yields multiple multi-character solutions'):
+      a = _acrostic_iter.Acrostic(list('ba') + ['dgnrt'], BA_PREFIX_TRIE)
+      expect(list(a)).to(contain('bad', 'bag', 'ban', 'bar', 'bat'))
+
+  with description('_iter_phrases'):
+    with before.all:
+      self.subject = _acrostic_iter.Acrostic(['a'], tries.letters())
+
+    with it('yields nothing for empty input'):
+      phrases = self.subject._iter_phrases({})
+      expect(phrases).to(have_len(0))
+
+    with it('yields all items from single dict'):
+      input = [('first', 10), ('second', 9), ('third', 8), ('fourth', 7)]
+      p = {
+        1: collections.OrderedDict(input),
+      }
+      phrases = list(self.subject._iter_phrases(p))
+      expect(phrases).to(have_len(4))
+      expect(phrases).to(equal(input))
+
+    with it('yields items from multiple dicts in sorted order'):
+      input1 = [('a', 10), ('b', 9), ('c', 5), ('d', 4)]
+      input2 = [('e', 25), ('f', 8), ('g', 3), ('h', 1)]
+      input3 = [('i', 15), ('j', 7), ('k', 6), ('l', 2)]
+      p = {
+        1: collections.OrderedDict(input1),
+        2: collections.OrderedDict(input2),
+        3: collections.OrderedDict(input3),
+      }
+      phrases = list(self.subject._iter_phrases(p))
+      expect(phrases).to(have_len(len(input1 + input2 + input3)))
+      expect(phrases).to(equal(
+          sorted(input1 + input2 + input3, key=lambda x: x[1], reverse=True)
+      ))
+
+    with it('is used to cache results from previous walks'):
+      a = _acrostic_iter.Acrostic(list('ba') + ['dgnrt'], BA_PREFIX_TRIE)
+      expect(list(a)).to(contain('bad', 'bag', 'ban', 'bar', 'bat'))
+      expect(list(a._phrase_graph[0][3].items())).to(equal(
+          [('bat', 5), ('bar', 4), ('ban', 3), ('bag', 2), ('bad', 1)]
+      ))
+
+  with context('when given ambiguous input text'):
+    with it('finds multiple words'):
+      a = _acrostic_iter.Acrostic(list('superbowl'), tries.ambiguous())
+      expect(list(a)).to(contain('super bowl', 'superb owl', 'superbowl'))
+
+    with it('finds multiple words in really long string'):
+      text = 'superbowlwarplanesnapshotscrapbookisnowhere'
+      a = _acrostic_iter.Acrostic(list(text), tries.ambiguous())
+      expect(''.join(next(iter(a)).split())).to(equal(text))
+
+    with it('finds same answer quickly'):
+      text = 'superbowlwarplanesnapshotscrapbookisnowhere'
+      a = _acrostic_iter.Acrostic(list(text), tries.ambiguous())
+      first = next(iter(a))
+      # Result should be cached and '_walk' should never be needed.
+      with patch.object(a, '_walk', side_effect=[]) as mock:
+        second = next(iter(a))
+        expect(first).to(equal(second))
+        expect(mock.call_count).to(be_below(10))
--- src/puzzle/heuristics/acrostics/_acrostic_iter.py
+++ src/puzzle/heuristics/acrostics/_acrostic_iter.py
@@ -1,24 +1,95 @@
+import heapq
+
 from rx import Observable
 
+from data import meta
 from puzzle.heuristics import acrostic
 
-class Acrostic(acrostic.BaseAcrostic):
-  """Simple Acrostic solver.
+_EMPTY = (None, 0)
+# Try to average 3 letters per word.
+_TARGET_WORD_LEN = 4
 
-  Lacks the ability to solve for multi-word solutions.
-  """
+class Acrostic(acrostic.BaseAcrostic):
+  """Acrostic solver."""
+
   def __init__(self, words, trie):
     self._words = [set(word) for word in words]
+    self._solution_len = len(words)
     self._trie = trie
     self._source = Observable.from_(self)
     self.subscribe = self._source.subscribe
-    self._cost = 0
+    # Since we can assume there will be 1 letter phrases for every letter
+    # go ahead and create n Meta objects to hold phrases from [n, m].
+    self._phrase_graph = [
+      {} for _ in range(0, self._solution_len)
+    ]
+    self._walks = [
+      self._trie.walk(self._words[i:]) for i in range(0, self._solution_len)
+    ]
 
   def __iter__(self):
-    self._cost = 0
-    for key, value in self._trie.walk(self._words):
-      self._cost += 1
-      yield key
+    for phrase, weight in self._walk_phrase_graph_from(0, []):
+      yield phrase
 
-  def cost(self):
-    return self._cost
+  def _walk_phrase_graph_from(self, pos, acc):
+    target = self._solution_len
+    for phrase, weight in self._phrases_at(pos):
+      phrase_len = len(phrase)
+      acc.append(phrase)
+      pos += phrase_len
+      if pos < target:
+        acc_len = len(acc)
+        interesting = (
+          (acc_len < 4) or (pos / acc_len >= _TARGET_WORD_LEN))
+        if interesting:
+          for result in self._walk_phrase_graph_from(pos, acc):
+            # Forward any findings from recursive calls up the trampoline.
+            yield result
+      elif pos == target:
+        yield ' '.join(acc), weight
+      else:
+        raise Exception('Desired length exceeded.')
+      pos -= phrase_len
+      acc.pop()
+
+  def _phrases_at(self, pos):
+    # phrases_at[0] has words of length 1, etc.
+    phrases_at = self._phrase_graph[pos]
+    # Exhaust known phrases first.
+    for phrase in self._iter_phrases(phrases_at):
+      yield phrase
+    # Then find more.
+    for phrase in self._walk(pos):
+      yield phrase
+
+  def _walk(self, pos):
+    phrases_at = self._phrase_graph[pos]
+    for phrase, weight in self._walks[pos]:
+      length_l_phrases = phrases_at.setdefault(len(phrase), meta.Meta())
+      length_l_phrases[phrase] = weight
+      yield phrase, weight
+
+  def _iter_phrases(self, phrases):
+    # Exhaust known phrases first.
+    best_phrases = []
+    cache = []
+    for pos, l in phrases.items():
+      best_items = iter(l.items())
+      next_best_tuple = next(best_items, _EMPTY)
+      if next_best_tuple is not _EMPTY:
+        item, weight = next_best_tuple
+        # Cache the real data for this queue entry somewhere else.
+        # cache[id] contains a tuple of (next_best_tuple, best_items)
+        heapq.heappush(best_phrases, (1/weight, len(cache)))
+        cache.append((next_best_tuple, best_items))
+    while best_phrases:
+      _, cache_id = best_phrases[0]
+      next_best_tuple, best_items = cache[cache_id]
+      yield next_best_tuple
+      next_best_tuple = next(best_items, _EMPTY)
+      if next_best_tuple is _EMPTY:
+        heapq.heappop(best_phrases)
+      else:
+        item, weight = next_best_tuple
+        cache[cache_id] = (next_best_tuple, best_items)
+        heapq.heapreplace(best_phrases, (1/weight, cache_id))
