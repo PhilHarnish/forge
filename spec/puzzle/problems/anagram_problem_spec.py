9aade2062098b749e0d18aee34ac5c79db27d998
Phil Harnish <philharnish@gmail.com>
2017-05-12 11:16:20 -0700

    Add simple 'AnagramProblem' problem.

---
+++ spec/puzzle/problems/anagram_problem_spec.py
@@ -0,0 +1,30 @@
+from puzzle.heuristics import analyze_word
+from spec.data.fixtures import tries
+from spec.mamba import *
+
+from puzzle.problems import anagram_problem
+
+
+with description('AnagramProblem'):
+  with before.all:
+    analyze_word.init_trie(tries.ambiguous())
+
+  with after.all:
+    analyze_word.reset()
+
+  with it('ignores empty and garbage input'):
+    expect(anagram_problem.AnagramProblem.score([''])).to(equal(0))
+
+  with it('rejects multiple lines'):
+    expect(anagram_problem.AnagramProblem.score(
+        'A quick brown fox jumps over the lazy dog'.split())).to(equal(0))
+
+  with it('accepts anagrams'):
+    expect(anagram_problem.AnagramProblem.score(['snap'])).to(equal(1))
+
+  with description('problem instance'):
+    with before.each:
+      self.subject = anagram_problem.AnagramProblem('example', ['snap'])
+
+    with it('solves anagrams'):
+      expect(list(self.subject.solutions())).to(equal(['snap', 'naps']))
---
+++ src/puzzle/problems/anagram_problem.py
@@ -0,0 +1,24 @@
+import collections
+
+from puzzle.heuristics import analyze_word
+from puzzle.problems import problem
+
+
+class AnagramProblem(problem.Problem):
+
+  @staticmethod
+  def score(lines):
+    # TODO: Support more input.
+    if len(lines) > 1:
+      return 0
+    return analyze_word.score_anagram(lines[0])
+
+  def _solve(self):
+    index = analyze_word.get_anagram_index()
+    results = collections.OrderedDict()
+    if self.lines[0] not in index:
+      return results
+    for index, word in reversed(list(enumerate(index[self.lines[0]]))):
+      # TODO: Use frequency for score.
+      results[word] = index + 1
+    return results
