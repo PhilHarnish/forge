8623091c786eca991f91a87b7e1cfb5aeedc5029
Phil Harnish <philharnish@gmail.com>
2017-05-13 15:31:00 -0700

    Add a "NumberProblem" type for numerical data.

---
+++ spec/puzzle/problems/number_problem_spec.py
@@ -0,0 +1,32 @@
+from puzzle.problems import number_problem
+from spec.mamba import *
+
+with description('NumberProblem'):
+  with it('ignores empty input'):
+    expect(number_problem.NumberProblem.score([''])).to(equal(0))
+
+  with it('rejects multiple lines'):
+    expect(number_problem.NumberProblem.score(['1', '2', '3'])).to(equal(0))
+
+  with it('rejects pseudo-numbers'):
+    expect(number_problem.NumberProblem.score(['1.2.3'])).to(equal(0))
+
+  with it('accepts integers'):
+    expect(number_problem.NumberProblem.score(['123'])).to(be_above(0))
+
+  with it('accepts floats'):
+    expect(number_problem.NumberProblem.score(['1.23'])).to(be_above(0))
+
+  with it('accepts hex'):
+    expect(number_problem.NumberProblem.score(['0xDEADBEEF'])).to(be_above(0))
+
+  with it('accepts octal'):
+    expect(number_problem.NumberProblem.score(['0777'])).to(be_above(0))
+
+  with it('reluctantly accepts 0'):
+    expect(number_problem.NumberProblem.score(['0'])).to(be_between(0, .000001))
+
+  with it('favors data with more information density'):
+    expect(number_problem.NumberProblem.score(['1234'])).to(be_above(
+        number_problem.NumberProblem.score(['123'])
+    ))
---
+++ src/puzzle/problems/number_problem.py
@@ -0,0 +1,46 @@
+import sys
+
+from puzzle.problems import problem
+
+
+class NumberProblem(problem.Problem):
+  def __init__(self, name, lines):
+    super(NumberProblem, self).__init__(name, lines)
+    if len(lines) > 1:
+      raise NotImplementedError()
+    self._value = _parse(lines[0])
+
+  @staticmethod
+  def score(lines):
+    if len(lines) > 1:
+      return 0
+    parsed = _parse(lines[0])
+    if parsed is None:
+      return 0
+    if isinstance(parsed, float):
+      return .5  # Unsure how to score a float.
+    # Weakest score returned for 0 ('epsilon').
+    # Smallest 4 letter word can be made with base-16.
+    return max(sys.float_info.epsilon, min(parsed / 0xAAAA, 1))
+
+  def _solve(self):
+    return {}
+
+
+def _parse(src):
+  result = None
+  try:
+    if src.startswith('0x'):
+      base = 16
+    elif src.startswith('0'):
+      base = 8
+    else:
+      base = 0  # Autodetect.
+    result = int(src, base)
+  except:
+    try:
+      result = float(src)
+    except:
+      # TODO: Base64.
+      pass
+  return result
