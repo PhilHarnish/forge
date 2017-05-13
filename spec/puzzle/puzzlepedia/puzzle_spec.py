89a01d19691d0ab021bf4734b4c5a554d0a5b973
Phil Harnish <philharnish@gmail.com>
2017-05-12 19:23:57 -0700

    Allow subscribing to individual problems in a puzzle.

--- spec/puzzle/puzzlepedia/puzzle_spec.py
+++ spec/puzzle/puzzlepedia/puzzle_spec.py
@@ -1,4 +1,7 @@
 import textwrap
+
+import mock
+
 from spec.mamba import *
 
 from puzzle.heuristics import analyze
@@ -75,7 +78,12 @@ with description('Puzzle'):
     p = puzzle.Puzzle('sample')
     expect(p.solutions()).to(equal(['meta: sample']))
 
-  with context('multiple problems'):
+  with it('allows solution override'):
+    p = puzzle.Puzzle('sample')
+    p.problem(0).solution = 'solution override'
+    expect(p.solutions()).to(equal(['solution override']))
+
+  with description('multiple problems'):
     with it('finds multiple solutions'):
       p = _get_multi_puzzle()
       expect(p.solutions()).to(equal(['meta: sample 1', 'meta: sample 2']))
@@ -87,3 +95,18 @@ with description('Puzzle'):
     with it('finds the solution to the second stage'):
       stage2 = _get_multi_puzzle().get_next_stage()
       expect(stage2.solutions()).to(equal(['final solution'] * 2))
+
+  with description('async changes'):
+    with it('notifies subscribers when first subscribing'):
+      p = puzzle.Puzzle('sample')
+      subs = mock.Mock()
+      p.problem(0).subscribe(subs)
+      expect(subs.on_next.call_args).to(equal(mock.call(p.problem(0))))
+
+    with it('notifies problem subscribers when solution changes'):
+      p = puzzle.Puzzle('sample')
+      subs = mock.Mock()
+      p.problem(0).subscribe(subs)
+      expect(subs.on_next.call_count).to(equal(1))
+      p.problem(0).solution = 'solution override'
+      expect(subs.on_next.call_count).to(equal(2))
--- src/puzzle/puzzlepedia/puzzle.py
+++ src/puzzle/puzzlepedia/puzzle.py
@@ -1,4 +1,7 @@
+import rx
+
 from data import meta
+from data import stream
 from puzzle.heuristics import analyze
 
 class Puzzle(object):
@@ -16,12 +19,19 @@ class Puzzle(object):
     for i, (meta_problem, consumed) in enumerate(
         analyze.identify_problems(lines)):
       self._meta_problems.append(_reify(meta_problem, '#%s' % i, consumed))
+    self._observable = stream.Stream()
+
+  def problem(self, index):
+    return self._meta_problems[index]
 
   def problems(self):
     return [p.active for p in self._meta_problems]
 
   def solutions(self):
-    return [p.active.solution for p in self._meta_problems]
+    return [p.solution for p in self._meta_problems]
+
+  def subscribe(self, observer):
+    self._observable.subscribe(observer)
 
   def get_next_stage(self):
     return Puzzle(self)
@@ -35,8 +45,32 @@ def _reify(meta_problem, name, lines):
 
 
 class _MetaProblem(meta.Meta):
-  _active = None
+  # Sentinel value for 'no solution found'.
+  _NO_SOLUTION = {}
+
+  def __init__(self):
+    super(_MetaProblem, self).__init__()
+    self._active = None
+    self._solution = self._NO_SOLUTION
+    self._observable = stream.Stream()
 
   @property
   def active(self):
     return self._active or self.peek()
+
+  @property
+  def solution(self):
+    if self._solution is self._NO_SOLUTION:
+      return self.active.solutions().peek()
+    return self._solution
+
+  @solution.setter
+  def solution(self, value):
+    if self._solution == value:
+      return
+    self._solution = value
+    self._observable.publish_value(self)
+
+  def subscribe(self, observer):
+    self._observable.subscribe(observer)
+    observer.on_next(self)
