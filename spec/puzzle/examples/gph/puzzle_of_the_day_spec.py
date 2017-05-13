6964af673a78fff01e36bff0119770eafac7b00b
Phil Harnish <philharnish@gmail.com>
2017-05-13 14:25:02 -0700

    Integration test for Galactic Puzzle Hunt "Puzzle of the Day".

---
+++ spec/puzzle/examples/gph/puzzle_of_the_day_spec.py
@@ -0,0 +1,37 @@
+from data import warehouse
+from puzzle.examples.gph import puzzle_of_the_day
+from puzzle.problems import crossword_problem
+from puzzle.puzzlepedia import prod_config
+from spec.mamba import *
+
+with _description('puzzle_of_the_day'):
+  with before.all:
+    warehouse.save()
+    prod_config.init()
+    self.subject = puzzle_of_the_day.get()
+
+  with after.all:
+    prod_config.reset()
+    warehouse.restore()
+
+  with it('parses'):
+    problems = self.subject.problems()
+    expect(problems).to(have_len(len(puzzle_of_the_day.SOLUTIONS)))
+    for problem in problems:
+      expect(problem).to(be_a(crossword_problem.CrosswordProblem))
+
+  with it('solves first problem'):
+    expect(self.subject.problem(0).solution).not_to(be_empty)
+
+  with it('gets some solutions right'):
+    solutions = self.subject.solutions()
+    matches = []
+    for i, (left, right) in enumerate(
+        zip(solutions, puzzle_of_the_day.SOLUTIONS)):
+      if left and right and left.lower() == right.lower():
+        matches.append('%s: %s' % (i, left))
+    expect(matches).to(equal([
+      '2: nitpick',
+      '4: windpipe',
+      '9: pilaf',
+    ]))
---
+++ src/puzzle/examples/gph/puzzle_of_the_day.py
@@ -0,0 +1,52 @@
+from puzzle.puzzlepedia import puzzle
+
+
+def get():
+  return puzzle.Puzzle('Puzzle of the day', SOURCE)
+
+
+SOURCE = """
+Winner or advocate (4|2)
+.
+Criticize small details (3|2)
+The Illuminati, for example (4|4)
+Trachea (4|2)
+C9H13NO3 (1|8)
+Altruistic (10|1)
+Namesake of a triangular fractal (4|4)
+U.S. state whose name may be used to count out roughly one second (9|0)
+Rice cooked in seasoned broth (0|3)
+Fox-like (3|2)
+Vegetable that gives Popeye superhuman strength (1|4)
+Chilean dictator (0|6)
+Animal with sharp quills (5|2)
+Modern camera smartphones have about 10–15 of these (4|4)
+Genus of the Taung child (8|6)
+Newell Brands permanent marker (4|1)
+Brave, as an explorer (5|1)
+Tasteless (4|1)
+French revolutionary figure (5|4)
+"""
+
+SOLUTIONS = """
+CHAMPION
+.
+NITPICK
+CONSPIRACY
+WINDPIPE
+EPINEPHRINE
+PHILANTHROPIC
+SIERPINSKI
+MISSISSIPPI
+PILAF
+VULPINE
+SPINACH
+PINOCHET
+PORCUPINE
+MEGAPIXELS
+AUSTRALOPITHECUS
+SHARPIE
+INTREPID
+INSIPID
+ROBESPIERRE
+""".strip().split()
