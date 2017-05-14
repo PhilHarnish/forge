4f727475577a18fc28ef047c55e51a75db8cb4c9
Phil Harnish <philharnish@gmail.com>
2017-05-14 16:56:37 -0700

    Add morse alphabet module.

---
+++ src/data/alphabets/morse.py
@@ -0,0 +1,57 @@
+ALPHABET = {
+  'a': '.-',
+  'b': '-...',
+  'c': '-.-.',
+  'd': '-..',
+  'e': '.',
+  'f': '..-.',
+  'g': '--.',
+  'h': '....',
+  'i': '..',
+  'j': '.---',
+  'k': '-.-',
+  'l': '.-..',
+  'm': '--',
+  'n': '-.',
+  'o': '---',
+  'p': '.--.',
+  'q': '--.-',
+  'r': '.-.',
+  's': '...',
+  't': '-',
+  'u': '..-',
+  'v': '...-',
+  'w': '.--',
+  'x': '-..-',
+  'y': '-.--',
+  'z': '--..',
+  '0': '-----',
+  '1': '.----',
+  '2': '..---',
+  '3': '...--',
+  '4': '....-',
+  '5': '.....',
+  '6': '-....',
+  '7': '--...',
+  '8': '---..',
+  '9': '----.',
+}
+LOOKUP = {v: k for k, v in ALPHABET.items()}
+
+TRANSLATION_TABLES = [
+  str.maketrans('012', ' .-'),
+  str.maketrans('021', ' .-'),
+  str.maketrans('201', ' .-'),
+  str.maketrans('210', ' .-'),
+]
+
+
+def translate(code):
+  parts = code.split(' ')
+  result = []
+  for part in parts:
+    if part in LOOKUP:
+      result.append(LOOKUP[part])
+    else:
+      return None
+  return result
