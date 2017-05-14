6df4aa0e7ea03a27ce50f8338d5287947a3bdb6f
Phil Harnish <philharnish@gmail.com>
2017-05-14 16:57:02 -0700

    Remove stale comment.

--- src/puzzle/heuristics/acrostic.py
+++ src/puzzle/heuristics/acrostic.py
@@ -1,7 +1,6 @@
 from puzzle.heuristics.acrostics import _acrostic_iter
 
 
-# The "naive acrostic" is currently the best one available.
 class Acrostic(_acrostic_iter.Acrostic):
   """Best available Acrostic solver."""
   pass
