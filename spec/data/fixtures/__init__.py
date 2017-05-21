fac2335ddbd0b3924ab5fe899ce547734b286471
Phil Harnish <philharnish@gmail.com>
2017-05-20 21:16:54 -0700

    Introduce unigram data to warehouse module.

--- spec/data/fixtures/__init__.py
+++ spec/data/fixtures/__init__.py
@@ -1,9 +1,14 @@
+import collections
+
 from data import anagram_index, crossword, warehouse
 from spec.data.fixtures import tries
 
 
+def _get_unigram():
+  return collections.OrderedDict(tries.kitchen_sink_data())
+
 def _get_unigram_anagram_index():
-  return anagram_index.AnagramIndex(warehouse.get('/words/unigram/trie'))
+  return anagram_index.AnagramIndex(warehouse.get('/words/unigram'))
 
 def _get_unigram_trie():
   return tries.kitchen_sink()
@@ -28,5 +33,6 @@ warehouse.init()
 warehouse.register('/phrases/crossword', _get_crossword)
 warehouse.register('/phrases/crossword/connection', _get_crossword_connection)
 warehouse.register('/phrases/crossword/cursor', _get_crossword_cursor)
+warehouse.register('/words/unigram', _get_unigram)
 warehouse.register('/words/unigram/anagram_index', _get_unigram_anagram_index)
 warehouse.register('/words/unigram/trie', _get_unigram_trie)
