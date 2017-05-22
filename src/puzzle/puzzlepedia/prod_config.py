18c4167a924bceb0cfa0d8914a2dfd019e67a959
Phil Harnish <philharnish@gmail.com>
2017-05-21 21:03:31 -0700

    Attempt to normalize data.
    Severely punish words not on the free COCA list.

--- src/puzzle/puzzlepedia/prod_config.py
+++ src/puzzle/puzzlepedia/prod_config.py
@@ -1,13 +1,34 @@
 import collections
 
-from data import anagram_index, crossword, trie, warehouse, word_frequencies
+from data import anagram_index, crossword, data, trie, warehouse, \
+  word_frequencies
 from puzzle.heuristics import analyze
 
 
 # Data sources.
+def _get_words_top():
+  result = collections.OrderedDict()
+  for row in open(data.project_path('data/words_coca.txt')):
+    rank, word, part, freq, dispersion = row.rstrip('\n').split()
+    if word.isalpha():
+      result[word.lower()] = freq
+  return result
+
+
+_TRUNCATION_LIMIT = 2 ** 27
+
 def _get_unigram():
+  results = []
+  top_words = warehouse.get('/words/top')
+  for word, value in word_frequencies.parse_file('data/count_1w.txt'):
+    l = len(word)
+    if word not in top_words:
+      reduction_factor = _TRUNCATION_LIMIT >> (l ** 2) or 1
+      value = value // reduction_factor
+    if value:
+      results.append((word, value))
   return collections.OrderedDict(
-      word_frequencies.parse_file('data/count_1w.txt'))
+      sorted(results, key=lambda x: x[1], reverse=True))
 
 
 def _get_unigram_anagram_index():
@@ -40,6 +61,7 @@ def init():
   warehouse.register('/phrases/crossword', _get_crossword)
   warehouse.register('/phrases/crossword/connection', _get_crossword_connection)
   warehouse.register('/phrases/crossword/cursor', _get_crossword_cursor)
+  warehouse.register('/words/top', _get_words_top)
   warehouse.register('/words/unigram', _get_unigram)
   warehouse.register('/words/unigram/anagram_index', _get_unigram_anagram_index)
   warehouse.register('/words/unigram/trie', _get_unigram_trie, pickle=True)
