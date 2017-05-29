685e215245d6db377a1339e7442767710bbc8f4d
Phil Harnish <philharnish@gmail.com>
2017-05-29 12:26:15 -0700

    Provide min & max solution lengths to subclasses.

--- src/puzzle/problems/crossword/_base_crossword_problem.py
+++ src/puzzle/problems/crossword/_base_crossword_problem.py
@@ -9,17 +9,29 @@ _INTS = re.compile(r'(\d+)')
 class _BaseCrosswordProblem(problem.Problem):
   def __init__(self, name, lines):
     super(_BaseCrosswordProblem, self).__init__(name, lines)
+    if len(lines) > 1:
+      raise NotImplementedError('Only one crossword clue per problem')
+    self._min_length = 1
+    self._max_length = float('inf')
     for line in lines:
       for match in _CROSSWORD_REGEX.finditer(line):
         lengths = _INTS.findall(match.group(1))
         if len(lengths) == 1:
-          self.constrain(lambda x, _: len(x) == int(lengths[0]))
+          target = int(lengths[0])
+          self.constrain(lambda x, _: len(x) == target)
+          self._min_length = target
+          self._max_length = target
         else:
           target = sum(map(int, lengths))
           if '|' in line:
             self.constrain(lambda x, _: len(x) >= target)
+            self._min_length = target
+            self._max_length = float('inf')
           else:
+            # TODO: Verify 2, 3 -> 2 letter word & 3 letter word.
             self.constrain(lambda x, _: len(x) == target)
+            self._min_length = target
+            self._max_length = target
         break
 
   def _solve(self):
