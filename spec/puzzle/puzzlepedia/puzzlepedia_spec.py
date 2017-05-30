5eab6fa1a6e6a2db2d264037e2c6c636d39f3480
Phil Harnish <philharnish@gmail.com>
2017-05-29 21:54:56 -0700

    Commit "puzzlepedia" iPython convenience module.

---
+++ spec/puzzle/puzzlepedia/puzzlepedia_spec.py
@@ -0,0 +1,32 @@
+from puzzle.puzzlepedia import puzzle, puzzlepedia
+from spec.mamba import *
+
+display_patch = mock.patch('puzzle.puzzlepedia.puzzlepedia.display')
+prod_config_patch = mock.patch('puzzle.puzzlepedia.prod_config')
+
+with description('puzzlepedia'):
+  with before.each:
+    self.puzzle = puzzle.Puzzle('test', '')
+    self.mock_display = display_patch.start()
+    self.mock_prod_config = prod_config_patch.start()
+
+  with after.each:
+    display_patch.stop()
+    prod_config_patch.stop()
+    puzzlepedia.reset()
+
+  with it('does not initialize on import'):
+    expect(puzzlepedia._INITIALIZED).to(be_false)
+
+  with description('parse'):
+    with it('initializes after calling parse'):
+      puzzlepedia.parse('')
+      expect(puzzlepedia._INITIALIZED).to(be_true)
+
+  with description('interact_with'):
+    with before.each:
+      self.puzzle = puzzle.Puzzle('test', '')
+
+    with it('runs without exception'):
+      expect(calling(puzzlepedia.interact_with, self.puzzle)).not_to(
+          raise_error)
