e53aa5b9e5554af1a1b52d44c0fae102a212be7a
Phil Harnish <philharnish@gmail.com>
2017-05-29 21:54:56 -0700

    Commit "puzzlepedia" iPython convenience module.

---
+++ src/puzzle/puzzlepedia/puzzlepedia.py
@@ -0,0 +1,28 @@
+from IPython import display
+
+from puzzle.puzzlepedia import prod_config, puzzle, puzzle_widget
+
+_INITIALIZED = False
+
+
+def parse(source, hint=None):
+  _init()
+  return puzzle.Puzzle('first stage', source, hint=hint)
+
+
+def interact_with(puzzle):
+  _init()
+  display.display(puzzle_widget.PuzzleWidget(puzzle))
+
+
+def _init():
+  global _INITIALIZED
+  if not _INITIALIZED:
+    _INITIALIZED = True
+    prod_config.init()
+
+
+def reset():
+  global _INITIALIZED
+  _INITIALIZED = False
+  prod_config.reset()
