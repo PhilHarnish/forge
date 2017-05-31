e5a40886211f5d7dc30a047a5443998243d120d6
Phil Harnish <philharnish@gmail.com>
2017-05-30 18:54:42 -0700

    Add identifying heuristic for CryptogramProblems.

--- spec/puzzle/problems/cryptogram_problem_spec.py
+++ spec/puzzle/problems/cryptogram_problem_spec.py
@@ -5,5 +5,14 @@ with description('CryptogramProblem'):
   with it('ignores empty input'):
     expect(cryptogram_problem.CryptogramProblem.score([''])).to(equal(0))
 
+  with it('rejects purely non-word inputs'):
+    expect(cryptogram_problem.CryptogramProblem.score(['$#!7'])).to(equal(0))
+
+  with it('favors jibberish'):
+    expect(cryptogram_problem.CryptogramProblem.score([
+      'asdf', 'fdsa', 'thesearenot', 'intest', 'dictionary',
+    ])).to(equal(1))
+
   with it('accepts all other input'):
-    expect(cryptogram_problem.CryptogramProblem.score(['asdf'])).to(be_above(0))
+    expect(cryptogram_problem.CryptogramProblem.score(['owl'])).to(
+        be_between(0, .25))
--- src/puzzle/problems/cryptogram_problem.py
+++ src/puzzle/problems/cryptogram_problem.py
@@ -1,12 +1,27 @@
+from data import warehouse
 from puzzle.problems import problem
 
 
 class CryptogramProblem(problem.Problem):
   @staticmethod
   def score(lines):
-    if lines and lines[0]:
-      return 0.1
-    return 0
+    # Look at all of the "words" in all lines.
+    tokens = ' '.join(lines).split()
+    if not tokens:
+      return 0
+    words = list(filter(str.isalpha, tokens))
+    if not words:
+      return 0  # Nothing to cryptogram.
+    if len(words) < len(tokens) // 2:
+      return 0  # Fewer than half of the tokens could be considered words.
+    # How many words appear to be gibberish?
+    known_words = warehouse.get('/words/unigram')
+    are_words = sum(word in known_words for word in words)
+    if are_words < len(words) // 8 + 1:
+      # Fewer than 1 in 8 of the original words are known.
+      return 1
+    # Something with 5+ of words *might* be a cryptic clue.
+    return max(0.0, 0.25 * (min(5, len(words)) / 5))
 
   def _solve(self):
     return {}
