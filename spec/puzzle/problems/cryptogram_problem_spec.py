0c3a953685cefd808bf08b7c07794414cc975406
Phil Harnish <philharnish@gmail.com>
2017-06-02 11:10:00 -0700

    Solve partitioned cryptograms.

--- spec/puzzle/problems/cryptogram_problem_spec.py
+++ spec/puzzle/problems/cryptogram_problem_spec.py
@@ -1,4 +1,6 @@
+from data import warehouse
 from puzzle.problems import cryptogram_problem
+from puzzle.puzzlepedia import prod_config
 from spec.data import fixtures
 from spec.mamba import *
 
@@ -18,7 +20,7 @@ with description('CryptogramProblem'):
     expect(cryptogram_problem.CryptogramProblem.score(['owl'])).to(
         be_between(0, .25))
 
-  with description('basic solutions'):
+  with description('solutions'):
     with before.all:
       fixtures.init()
 
@@ -30,7 +32,9 @@ with description('CryptogramProblem'):
       p = cryptogram_problem.CryptogramProblem('rot14', ['egbqdn'])
       expect(p.solutions()).to(equal({'superb (rot14)': 1}))
 
-    with it('solves mixed translations'):
+    with it('solves mixed rotN translations'):
+      # bjy egbqdn
+      # owl superb
       p = cryptogram_problem.CryptogramProblem('rot13 and 14', [
         'bjy egbqdn'
       ])
@@ -38,3 +42,46 @@ with description('CryptogramProblem'):
         'owl rtodqa (rot13)': 1 / 3,
         'pxm superb (rot14)': 2 / 3,
       }))
+
+    with it('solves ordinary cryptograms'):
+      p = cryptogram_problem.CryptogramProblem('cryptogram', [
+        # superb owl war plane snapshot scrapbook is now here.
+        'fiune zlqg qbe ugbxn fxbufolv fkebuzllh pf xlq onen'
+      ])
+      solutions = p.solutions()
+      expected = 'super bowl war plane snapshot scrapbook is now here'
+      expect(solutions).to(have_key(expected))
+      expect(solutions[expected]).to(be_above(0.5))
+
+  with description('real data'):
+    with before.all:
+      warehouse.save()
+      prod_config.init()
+
+    with after.all:
+      prod_config.reset()
+      warehouse.restore()
+
+    with _it('solves long cryptograms'):
+      initial = """
+        T gxawjhixtq om t jawe hz wkccde jrtj ghumomjm hz t mrhxj woege hz 
+        eugxawjep jebj. Ieuextdda jre gowrex kmep jh eugxawj jre jebj om moqwde 
+        euhkir jrtj jre gxawjhixtq gtu ve mhdnep va rtup. Zxefkeujda kmep txe 
+        mkvmjojkjohu gowrexm lrexe etgr dejjex om xewdtgep va t pozzexeuj 
+        dejjex hx ukqvex. Jh mhdne jre wkccde, hue qkmj xeghnex jre hxoioutd 
+        dejjexoui. Jrhkir huge kmep ou qhxe mexohkm twwdogtjohum, jrea txe uhl 
+        qtouda wxoujep zhx eujexjtouqeuj ou uelmwtwexm tup qtitcouem.
+      """
+      expected = """
+        A cryptogram is a type of puzzle that consists of a short piece of 
+        encrypted text. Generally the cipher used to encrypt the text is simple 
+        enough that the cryptogram can be solved by hand. Frequently used are 
+        substitution ciphers where each letter is replaced by a different 
+        letter or number. To solve the puzzle, one must recover the original 
+        lettering. Though once used in more serious applications, they are now 
+        mainly printed for entertainment in newspapers and magazines.
+      """
+      p = cryptogram_problem.CryptogramProblem('large cryptogram', [initial])
+      solutions = p.solutions()
+      expect(solutions).to(have_key(expected))
+      expect(solutions[expected]).to(be_above(0.9))
--- src/puzzle/problems/cryptogram_problem.py
+++ src/puzzle/problems/cryptogram_problem.py
@@ -1,10 +1,12 @@
 from data import warehouse
+from data.seek_sets import crypto_seek_set
 from puzzle.problems import problem
 
 # Humans will often choose a ROT value which is ~180 degrees away from A=A.
 # For example: ROT13 is common and ROT1 or ROT25 are very uncommon.
 _ROT_OFFSETS = list(sorted(range(1, 25), key=lambda i: abs(26 / 2 - i)))
 _ALPHABET = 'abcdefghijklmnopqrstuvwxyz'
+_ALPHABET_UPPER = _ALPHABET.upper()
 _ROT_TRANSLATIONS = [None] + [
   str.maketrans(_ALPHABET, _ALPHABET[i:] + _ALPHABET[:i]) for i in range(1, 26)
 ]
@@ -14,13 +16,14 @@ _MIN_CONVERSION = 0.2
 _MIN_WORD_THRESHOLD = 45000
 # Minimum number of characters to consider "translated".
 _MIN_WORD = 3
+# If Trie yields results greater than this per character it is "good".
+_TARGET_WORD_SCORE_RATE = 200000000
 
 
 class CryptogramProblem(problem.Problem):
   def __init__(self, name, lines):
     super(CryptogramProblem, self).__init__(name, lines)
     _, self._words = _parse(lines)
-
 
   @staticmethod
   def score(lines):
@@ -42,19 +45,30 @@ class CryptogramProblem(problem.Problem):
   def _solve_iter(self):
     # First attempt a rotN solve.
     all_text = '\n'.join(self.lines)
-    for offset in _ROT_OFFSETS:
-      score = rot_n_score(self._words, offset)
-      if score > _MIN_CONVERSION:
-        solution = all_text.translate(_ROT_TRANSLATIONS[offset])
-        yield '%s (rot%s)' % (solution, offset), score
+    good_match = False
+    for solution, weight in _generate_rot_n(all_text, self._words):
+      good_match = good_match or weight == 1
+      yield solution, weight
+    if good_match:
+      return
+    for solution in _generate_partitioned_cryptograms(all_text, self._words):
+      yield solution
 
   def _solve(self):
     raise NotImplementedError()
 
 
 def _parse(lines):
-  tokens = ' '.join(lines).split()
+  tokens = ' '.join(lines).lower().split()
   return tokens, list(filter(str.isalpha, tokens))
+
+
+def _generate_rot_n(all_text, words):
+  for offset in _ROT_OFFSETS:
+    score = rot_n_score(words, offset)
+    if score > _MIN_CONVERSION:
+      solution = all_text.translate(_ROT_TRANSLATIONS[offset])
+      yield '%s (rot%s)' % (solution, offset), score
 
 
 def rot_n_score(words, n):
@@ -78,3 +92,52 @@ def rot_n_score(words, n):
       score += l * word_weight
     all += l
   return score / all
+
+
+def _generate_partitioned_cryptograms(all_text, words):
+  # Focus on the longest words.
+  sorted_words = sorted(set(words), key=lambda x: -len(x))
+  trie = warehouse.get('/words/unigram/trie')
+  # Note: This score currently includes whitespace etc.
+  target_score = len(all_text) * _TARGET_WORD_SCORE_RATE
+  for trans, score in _partitioned_cryptograms_from(sorted_words, [], trie):
+    yield all_text.translate(trans), min(1, score / target_score)
+
+
+def _partitioned_cryptograms_from(crypto_words, normal_words, trie):
+  pos = len(normal_words)
+  end = len(crypto_words) - 1
+  translation = _make_translation(crypto_words, normal_words)
+  seek_set = crypto_seek_set.CryptoSeekSet(
+      crypto_words[pos], translation=translation)
+  for word, score in trie.walk(seek_set, exact_match=True):
+    normal_words.append(word)
+    if pos == end:
+      yield _make_solution_translation_table(translation, crypto_words[pos],
+          normal_words[pos]), score
+    else:
+      for solution, child_score in _partitioned_cryptograms_from(
+          crypto_words, normal_words, trie):
+        # Up the trampoline, accumulating score.
+        yield solution, score + child_score
+    normal_words.pop()
+
+
+def _make_translation(crypto_words, normal_words):
+  translation = {}
+  for crypto_word, normal_word in zip(crypto_words, normal_words):
+    for crypto_c, normal_c in zip(crypto_word, normal_word):
+      if crypto_c in translation and translation[crypto_c] != normal_c:
+        raise IndexError('Inconsistent translation %s -> %s' % (
+          crypto_words, normal_words))
+      translation[crypto_c] = normal_c
+  return translation
+
+
+def _make_solution_translation_table(translation, last_crypto, last_word):
+  table = str.maketrans(translation)
+  table.update(str.maketrans(last_crypto, last_word))
+  # Determine upper case letters too.
+  table.update(
+      str.maketrans(_ALPHABET_UPPER, _ALPHABET.translate(table).upper()))
+  return table
