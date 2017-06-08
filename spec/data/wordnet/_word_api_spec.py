4878316b909d5bc33d9a4a7e792f0ebf9fd452a2
Phil Harnish <philharnish@gmail.com>
2017-06-08 10:01:29 -0700

    Start implementing expand a word to its "synonyms".
    * Expanding hyponyms and hypernyms.
    * Re-pluralizes if needed.

--- spec/data/wordnet/_word_api_spec.py
+++ spec/data/wordnet/_word_api_spec.py
@@ -17,3 +17,12 @@ with description('_word_api'):
     with it('returns synonyms'):
       results = _word_api.hypernyms('orange')
       expect(results).to(have_keys('pigment', 'color', 'edible fruit'))
+
+  with description('cryptic examples'):
+    with it('GREENBELT: Inexperienced band intended to limit urban sprawl'):
+      # expect(_word_api.expand('inexperienced')).to(have_key('green'))
+      expect(_word_api.expand('band')).to(have_key('belt'))
+
+    with it('PASTRY: Fathers attempt to get pie-crust'):
+      expect(_word_api.expand('fathers')).to(have_key('pas'))
+      expect(_word_api.expand('attempt')).to(have_key('try'))
--- src/data/wordnet/_word_api.py
+++ src/data/wordnet/_word_api.py
@@ -3,35 +3,43 @@ from nltk.corpus import wordnet
 from data import meta
 
 
+def expand(word, pos=None):
+  return synonyms(word, limit_one_word=True)
+
 def synonyms(word, limit_one_word=True):
+  suffix = _suffix(word)
   results = meta.Meta()
   visited = set()
   # Siblings.
   for synset in wordnet.synsets(word):
-    _add_synset_lemmas(results, visited, synset, limit_one_word)
+    _add_synset_lemmas(results, visited, synset, limit_one_word, suffix)
+    # Children.
+    for child in synset.hyponyms():
+      _add_synset_lemmas(results, visited, child, limit_one_word, suffix)
     # Parents.
     for parent in synset.hypernyms():
-      _add_synset_lemmas(results, visited, parent, limit_one_word)
+      _add_synset_lemmas(results, visited, parent, limit_one_word, suffix)
   return results
 
 
 def hypernyms(word, distance=2):
   results = meta.Meta()
   visited = set()
+  suffix = _suffix(word)
   for synset in wordnet.synsets(word):
-    _hypernyms(results, visited, synset, distance)
+    _hypernyms(results, visited, synset, distance, suffix)
   return results
 
 
-def _hypernyms(results, visited, synset, distance):
+def _hypernyms(results, visited, synset, distance, suffix):
   if distance <= 0:
     return
   for parent in synset.hypernyms():
-    _add_synset_lemmas(results, visited, parent, False)
-    _hypernyms(results, visited, parent, distance - 1)
+    _add_synset_lemmas(results, visited, parent, False, suffix)
+    _hypernyms(results, visited, parent, distance - 1, suffix)
 
 
-def _add_synset_lemmas(results, visited, synset, limit_one_word):
+def _add_synset_lemmas(results, visited, synset, limit_one_word, suffix):
   if synset.name() in visited:
     return
   visited.add(synset.name())
@@ -43,3 +51,13 @@ def _add_synset_lemmas(results, visited, synset, limit_one_word):
         continue
       synonym_lemma = synonym_lemma.replace('_', ' ')
     results[synonym_lemma] = 1
+    if suffix:
+      results[synonym_lemma + suffix] = 1
+
+
+def _suffix(word):
+  base_form = wordnet.morphy(word)
+  if base_form != word and word.startswith(base_form):
+    # Only supports re-adding a removed suffix. Only works for some plurals.
+    return word[len(base_form):]
+  return ''
