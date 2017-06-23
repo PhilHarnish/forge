0ea51e94598d82310ec2dd990c8a55c4c15100f6
Phil Harnish <philharnish@gmail.com>
2017-06-23 15:05:40 -0700

    Parse input text to evaluate a LogicProblem.

--- spec/puzzle/problems/logic_problem_spec.py
+++ spec/puzzle/problems/logic_problem_spec.py
@@ -2,13 +2,14 @@ from puzzle.problems import logic_problem
 from spec.mamba import *
 
 with description('LogicProblem'):
-  with it('ignores empty input'):
-    expect(logic_problem.LogicProblem.score([''])).to(equal(0))
+  with description('score'):
+    with it('ignores empty input'):
+      expect(logic_problem.LogicProblem.score([''])).to(equal(0))
 
-  with it('rejects a single line'):
-    expect(logic_problem.LogicProblem.score(['a single line'])).to(equal(0))
+    with it('rejects a single line'):
+      expect(logic_problem.LogicProblem.score(['a single line'])).to(equal(0))
 
-  with it('assigns a non-zero score to many lines'):
-    expect(logic_problem.LogicProblem.score([
-      'this', 'is', 'many', 'lines',
-    ])).to(be_above(0))
+    with it('assigns a non-zero score to many lines'):
+      expect(logic_problem.LogicProblem.score([
+        'this', 'has', 'many', 'lines',
+      ])).to(be_above(0))
--- src/puzzle/problems/logic_problem.py
+++ src/puzzle/problems/logic_problem.py
@@ -1,3 +1,4 @@
+import ast
 import sys
 
 from puzzle.problems import problem
@@ -7,5 +8,10 @@ class LogicProblem(problem.Problem):
   @staticmethod
   def score(lines):
     if len(lines) <= 1:
+      return 0
+    program = '\n'.join(lines)
+    try:
+      ast.parse(program)
+    except:
       return 0
     return sys.float_info.epsilon
