4de5be3b59f21613def786f397e82d971993c158
Phil Harnish <philharnish@gmail.com>
2017-06-23 19:45:00 -0700

    Add a new _DimensionSlice object.
    This will replace the original.

---
+++ spec/data/logic/_dimension_slice_spec.py
@@ -0,0 +1,9 @@
+from data.logic import _dimension_slice
+from spec.mamba import *
+
+with description('_dimension_slice._DimensionSlice'):
+  with description('constructor'):
+    with it('handles simple input'):
+      expect(calling(
+          _dimension_slice._DimensionSlice, None, None, None
+      )).not_to(raise_error)
---
+++ src/data/logic/_dimension_slice.py
@@ -0,0 +1,5 @@
+class _DimensionSlice(object):
+  def __init__(self, parent, dimension, slice):
+    self._parent = parent
+    self._dimension = dimension
+    self._slice = slice
