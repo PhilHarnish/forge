39b4d37c74ddb06a89b05e1a6ef873dfa84689da
Phil Harnish <philharnish@gmail.com>
2017-06-24 12:01:39 -0700

    Update model test to illustrate expression accumulation.

--- spec/data/logic/_model_spec.py
+++ spec/data/logic/_model_spec.py
@@ -1,15 +1,24 @@
-from data.logic import _model
+from data.logic import _dimension_factory, _model
 from spec.mamba import *
 
 with description('_model._Model'):
+  with before.each:
+    self.factory = _dimension_factory._DimensionFactory()
+
   with description('constructor'):
     with it('handles simple input'):
-      expect(calling(_model._Model, None)).not_to(raise_error)
+      expect(calling(_model._Model, self.factory)).not_to(raise_error)
 
   with description('constraints'):
     with before.each:
-      self.subject = _model._Model(None)
+      self.model = _model._Model(self.factory)
+      self.andy, self.bob = self.factory(name=['andy', 'bob'])
+      self.cherries, self.dates = self.factory(fruit=['cherries', 'dates'])
 
     with it('accumulates constraints'):
-      self.subject(1, 2, 3)
-      expect(self.subject.constraints()).to(have_len(3))
+      self.model(
+          self.andy == self.cherries,
+          self.dates == self.bob,
+          self.cherries != self.bob,
+      )
+      expect(self.model.constraints()).to(have_len(3))
--- src/data/logic/_model.py
+++ src/data/logic/_model.py
@@ -1,9 +1,15 @@
+import ast
+
+
 class _Model(object):
   def __init__(self, dimension_factory):
     self._dimension_factory = dimension_factory
     self._constraints = []
 
   def __call__(self, *args):
+    bad = list(filter(lambda i: not isinstance(i, ast.Expr), args))
+    if bad:
+      raise TypeError('Model only accepts expressions (given %s)' % bad)
     self._constraints.extend(args)
 
   def constraints(self):
