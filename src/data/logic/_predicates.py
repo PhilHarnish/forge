c88820f1ab81320ccd244dc4dc3fca95c7d7b0e9
Phil Harnish <philharnish@gmail.com>
2017-07-01 22:03:26 -0700

    Refactor Predicates to use operator module.
    Supports far more operators, and their complements, more simply.

--- src/data/logic/_predicates.py
+++ src/data/logic/_predicates.py
@@ -1,3 +1,5 @@
+import operator
+
 import Numberjack
 
 # Try to de-prioritize any Numberjack primitives so that our operators
@@ -6,50 +8,52 @@ _LOWER_PRIORITY = (
   Numberjack.Predicate,
 )
 
+# Maps binary operators to their swapped complement.
+_OPERATORS = {
+  # Comparators.
+  '__eq__': '__eq__',
+  '__ne__': '__ne__',
+  '__lt__': '__gt__',
+  '__le__': '__ge__',
+  # Binary operators.
+  '__add__': '__radd__',
+  '__sub__': '__rsub__',
+  '__or__': '__ror__',
+  '__xor__': '__rxor__',
+}
+
 
 class Predicates(list):
-  def __init__(self, values):
-    super(Predicates, self).__init__(values)
-
-  def _zip(self, other):
-    for child in self:
-      if isinstance(child, _LOWER_PRIORITY):
-        yield other, child
-      else:
-        yield child, other
-
-  def __eq__(self, other):
-    return Predicates([left == right for left, right in self._zip(other)])
-
-  def __ne__(self, other):
-    return Predicates([left != right for left, right in self._zip(other)])
-
-  def __add__(self, other):
-    return Predicates([left + right for left, right in self._zip(other)])
-
-  def __sub__(self, other):
-    return Predicates([child - other for child in self])
-
-  def __lt__(self, other):
-    return Predicates([child < other for child in self])
-
-  def __le__(self, other):
-    return Predicates([child <= other for child in self])
-
-  def __gt__(self, other):
-    return Predicates([child > other for child in self])
-
-  def __ge__(self, other):
-    return Predicates([child >= other for child in self])
-
-  def __or__(self, other):
-    # TODO: This should really be a cross product.
-    return Predicates([left | right for left, right in self._zip(other)])
-
-  def __xor__(self, other):
-    # TODO: This should really be a cross product.
-    # This shortcut may not always work. When does it fail?
-    return Predicates([left + right == 1 for left, right in self._zip(other)])
-
   def __str__(self):
     return '\n'.join(map(str, self))
+
+
+def _make_operator(op, rop):
+  def fn(self, other):
+    children = []
+    for child in self:
+      if isinstance(child, _LOWER_PRIORITY):
+        children.append(rop(other, child))
+      else:
+        children.append(op(child, other))
+    return Predicates(children)
+
+  return fn
+
+
+def _reversed_operator(op):
+  def reversed(left, right):
+    return op(right, left)
+
+  return reversed
+
+
+for op, rop in _OPERATORS.items():
+  op_fn = getattr(operator, op)
+  if hasattr(operator, rop):
+    rop_fn = getattr(operator, rop)
+  else:
+    rop_fn = _reversed_operator(op_fn)
+  setattr(Predicates, op, _make_operator(op_fn, rop_fn))
+  if op != rop:
+    setattr(Predicates, rop, _make_operator(rop_fn, op_fn))
