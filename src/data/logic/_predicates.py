3c8488c10b7d9efe2cfa4871dfcf6749c6c3fe4f
Phil Harnish <philharnish@gmail.com>
2017-07-02 19:12:27 -0700

    Use operator_overloading module in _predicates.

--- src/data/logic/_predicates.py
+++ src/data/logic/_predicates.py
@@ -1,31 +1,12 @@
-import operator
-
 import Numberjack
 
 # Try to de-prioritize any Numberjack primitives so that our operators
 # take precedence.
+from data import operator_overloading
+
 _LOWER_PRIORITY = (
   Numberjack.Predicate,
 )
-
-# Maps binary operators to their swapped complement.
-_OPERATORS = {
-  # Comparators.
-  '__eq__': '__eq__',
-  '__ne__': '__ne__',
-  '__lt__': '__gt__',
-  '__le__': '__ge__',
-  # Binary operators.
-  '__add__': '__radd__',
-  '__sub__': '__rsub__',
-  '__or__': '__ror__',
-  '__xor__': '__rxor__',
-}
-
-
-class Predicates(list):
-  def __str__(self):
-    return '\n'.join(map(str, self))
 
 
 def _make_operator(op, rop):
@@ -41,19 +22,7 @@ def _make_operator(op, rop):
   return fn
 
 
-def _reversed_operator(op):
-  def reversed(left, right):
-    return op(right, left)
-
-  return reversed
-
-
-for op, rop in _OPERATORS.items():
-  op_fn = getattr(operator, op)
-  if hasattr(operator, rop):
-    rop_fn = getattr(operator, rop)
-  else:
-    rop_fn = _reversed_operator(op_fn)
-  setattr(Predicates, op, _make_operator(op_fn, rop_fn))
-  if op != rop:
-    setattr(Predicates, rop, _make_operator(rop_fn, op_fn))
+@operator_overloading.overload_with_fn(_make_operator)
+class Predicates(list):
+  def __str__(self):
+    return '\n'.join(map(str, self))
