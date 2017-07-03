799fccfea3f681754dc984868e3293b77c31e397
Phil Harnish <philharnish@gmail.com>
2017-07-03 15:36:53 -0700

    Some small updates to LogicProblem.
    Hooking up parser and transformer.

--- spec/puzzle/problems/logic_problem_spec.py
+++ spec/puzzle/problems/logic_problem_spec.py
@@ -1,15 +1,32 @@
 from puzzle.problems import logic_problem
 from spec.mamba import *
 
-with description('LogicProblem'):
-  with description('score'):
-    with it('ignores empty input'):
-      expect(logic_problem.LogicProblem.score([''])).to(equal(0))
+with description('LogicProblem score'):
+  with it('ignores empty input'):
+    expect(logic_problem.LogicProblem.score([''])).to(equal(0))
 
-    with it('rejects a single line'):
-      expect(logic_problem.LogicProblem.score(['a single line'])).to(equal(0))
+  with it('rejects a single line'):
+    expect(logic_problem.LogicProblem.score(['a single line'])).to(equal(0))
 
-    with it('assigns a non-zero score to many lines'):
-      expect(logic_problem.LogicProblem.score([
-        'this', 'has', 'many', 'lines',
-      ])).to(be_above(0))
+  with it('assigns a non-zero score to many lines'):
+    expect(logic_problem.LogicProblem.score([
+      'this', 'has', 'many', 'lines',
+    ])).to(be_above(0))
+
+with description('LogicProblem constructor'):
+  with it('parses empty input'):
+    problem = logic_problem.LogicProblem('test', [''])
+    expect(problem._parsed).to(have_len(0))
+
+  with it('parses comments'):
+    problem = logic_problem.LogicProblem('test', [
+      '# This is a comment.'
+    ])
+    expect(problem._parsed).to(have_len(0))
+
+  with it('parses statements'):
+    problem = logic_problem.LogicProblem('test', [
+      '# This is a comment.',
+      'ages = 10, 11, 12'
+    ])
+    expect(problem._parsed).to(have_len(1))
--- src/puzzle/problems/logic_problem.py
+++ src/puzzle/problems/logic_problem.py
@@ -5,13 +5,26 @@ from puzzle.problems import problem
 
 
 class LogicProblem(problem.Problem):
+  def __init__(self, name, lines):
+    super(LogicProblem, self).__init__(name, lines)
+    program = '\n'.join(lines)
+    self._parsed = self._parse(program)
+
   @staticmethod
   def score(lines):
     if len(lines) <= 1:
       return 0
     program = '\n'.join(lines)
     try:
-      ast.parse(program)
+      parsed = ast.parse(program)
+      if isinstance(parsed, ast.Module):
+        return min(1, len(parsed.body) / 10)
     except:
       return 0
     return sys.float_info.epsilon
+
+  def _parse(self, program):
+    parsed = ast.parse(program)
+    if isinstance(parsed, ast.Module):
+      return parsed.body
+    raise TypeError('Program did not parse:\n%s' % program)
