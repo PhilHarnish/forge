937ef683310e0959cca10f9752310f20d2b418ae
Phil Harnish <philharnish@gmail.com>
2017-07-04 17:12:23 -0700

    Export "abs" override to logic programs.
    Proxy object for Numberjack.Abs.

--- src/data/logic/dsl.py
+++ src/data/logic/dsl.py
@@ -1,5 +1,6 @@
 """These objects are imported into every compiled logic problem"""
-from data.logic import _dimension_factory, _model
+from data.logic import _dimension_factory, _model, _sugar
 
 DimensionFactory = _dimension_factory._DimensionFactory
 Model = _model._Model
+abs = _sugar.sugar_abs
