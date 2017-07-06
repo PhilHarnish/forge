3567c81cb918fa03c2e423f3ebb331fb6d9c6952
Phil Harnish <philharnish@gmail.com>
2017-07-05 20:30:37 -0700

    Implement iterative solving in Problem.
    This makes it possible to peek() a 1+ magnitude solution without
    exhausting all solutions.

--- spec/puzzle/problems/problem_spec.py
+++ spec/puzzle/problems/problem_spec.py
@@ -37,3 +37,39 @@ with description('Problem'):
     ex = ExampleProblem('example', [])
     for solution in ex.solutions():
       expect(ex.notes_for(solution)).to(be_a(list))
+
+  with description('iterative solving'):
+    with before.each:
+      solve_iter_stub = mock.Mock(return_value=iter([
+        ('first value', 0.9),
+        ('better value', 1.0),
+        ('bad value', 0.5),
+      ]))
+      class ExampleProblem(problem.Problem):
+        _solve_iter = solve_iter_stub
+
+      self.solve_iter_stub = solve_iter_stub
+      self.subject = ExampleProblem('example', [])
+
+    with it('solutions() returns all results'):
+      expect(list(self.subject.solutions().items())).to(equal([
+        ('better value', 1.0),
+        ('first value', 0.9),
+        ('bad value', 0.5),
+      ]))
+
+    with it('solutions() only calls _solve_iter once'):
+      expect(self.solve_iter_stub).not_to(have_been_called)
+      self.subject.solutions()
+      expect(self.solve_iter_stub).to(have_been_called_once)
+      self.subject.solutions()
+      expect(self.solve_iter_stub).to(have_been_called_once)
+
+    with it('solutions() only calls _solve_iter once, even if reconstrained'):
+      expect(self.subject.solutions()).to(have_len(3))
+      self.subject.constrain(lambda solution, weight: weight > 0.5)
+      expect(list(self.subject.solutions().items())).to(equal([
+        ('better value', 1.0),
+        ('first value', 0.9),
+      ]))
+      expect(self.solve_iter_stub).to(have_been_called_once)
--- src/puzzle/problems/problem.py
+++ src/puzzle/problems/problem.py
@@ -1,4 +1,5 @@
 import collections
+import itertools
 
 from data import meta
 
@@ -10,7 +11,10 @@ class Problem(object):
     self.name = name
     self.lines = lines
     self._threshold = threshold
-    self._solutions = None
+    self._solutions_iterator = self._take_solutions_iter()
+    self._all_solutions = meta.Meta()
+    self._filtered_solutions_iterator = self._filter_solutions_iter()
+    self._filtered_solutions = meta.Meta()
     self._notes = collections.defaultdict(list)
     self._constraints = [
       lambda k, v: v > self._threshold
@@ -22,29 +26,51 @@ class Problem(object):
 
   @property
   def solution(self):
-    return self.solutions().peek()
+    if self._filtered_solutions.magnitude() >= 1:
+      # At least one satisfactory answer already found.
+      return self._filtered_solutions.peek()
+    # Try to find just 1 good answer.
+    self._filter_solutions_until(min_score=1)
+    # Solutions will either be exhausted or include at least one with 1+ score.
+    return self._filtered_solutions.peek()
 
   def constrain(self, fn):
     self._constraints.append(fn)
     # Invalidate solutions.
-    self._solutions = None
-    self._solutions_iter = None
+    self._filtered_solutions_iterator = self._filter_solutions_iter()
+    self._filtered_solutions = meta.Meta()
 
   def solutions(self):
-    if self._solutions is None:
-      self._solutions_iter = self._solve_iter()
-      results = []
-      for k, v in self._solutions_iter:
-        if all(fn(k, v) for fn in self._constraints):
-          results.append((k, v))
-      self._solutions = meta.Meta(results)
-    return self._solutions
+    for _ in self._filtered_solutions_iterator:
+      # Exhaust the _filtered_solutions_iterator.
+      pass
+    return self._filtered_solutions
 
   def notes_for(self, solution):
     return self._notes.get(solution, [])
 
   def _solve_iter(self):
     return iter(self._solve().items())
+
+  def _take_solutions_iter(self):
+    for k, v in self._solve_iter():
+      self._all_solutions[k] = v
+      yield k, v
+
+  def _filter_solutions_iter(self):
+    # First review any existing values in _all_solutions. This is empty unless
+    # the Problem's constraints changed mid-solve.
+    for k, v in itertools.chain(
+        self._all_solutions.items(), self._solutions_iterator):
+      constrained = all(fn(k, v) for fn in self._constraints)
+      if constrained:
+        self._filtered_solutions[k] = v
+        yield k, v
+
+  def _filter_solutions_until(self, min_score=float('inf')):
+    for k, v in self._filtered_solutions_iterator:
+      if v >= min_score:
+        break
 
   def _solve(self):
     """Solves Problem.
