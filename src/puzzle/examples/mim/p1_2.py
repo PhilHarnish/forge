b8b2ba1e1716ae3f4d422bb090d149045a5b8232
Phil Harnish <philharnish@gmail.com>
2017-08-04 18:44:30 -0700

    Update 1.2 to use cross-product dimensions.

--- src/puzzle/examples/mim/p1_2.py
+++ src/puzzle/examples/mim/p1_2.py
@@ -6,17 +6,8 @@ def get():
 
 
 SOURCE = """
-name in {Beth1, Beth2, Charles1, Charles2, David1, David2, Frank1, Frank2, Jessica1, Jessica2, Karen1, Karen2, Taylor1, Taylor2}
-# Setup: Clubs x4, Diamonds x4, Hearts x3, Spades x3.
-suit in {Club, Club, Club, Club, Diamond, Diamond, Diamond, Diamond, Heart, Heart, Heart, Spade, Spade, Spade}
-
-b = [Beth1, Beth2]
-c = [Charles1, Charles2]
-d = [David1, David2]
-f = [Frank1, Frank2]
-j = [Jessica1, Jessica2]
-k = [Karen1, Karen2]
-t = [Taylor1, Taylor2]
+(name, hand) in ({Beth, Charles, David, Frank, Jessica, Karen, Taylor}, {1, 2})
+suit in {Club*4, Diamond*4, Heart*3, Spade*3}
 
 def has(person, suit):
   a, b = person
@@ -26,29 +17,35 @@ def hand(person, suit1, suit2):
   a, b = person
   return (a[suit1] and b[suit2]) or (a[suit2] and b[suit1])
 
-(hand(b, club, spade)) or (hand(b, heart, spade))
-(hand(c, diamond, heart)) or (has(c, spade) >= 1)
-(hand(d, club, spade)) or (has(d, spade) == 1)
-(hand(f, club, heart)) or (hand(f, club, spade))
-(hand(j, club, spade)) or ((has(j, spade) == 1) and (has(j, diamond) == 0))
-(has(k, diamond) == 0) or (has(k, spade) >= 1)
-(hand(t, club, heart)) or (has(t, club) == 2 or has(t, diamond) == 2 or has(t, heart) == 2 or has(t, spade) == 2)
+(hand(name.Beth, club, spade)) or (hand(name.Beth, heart, spade))
+(hand(name.Charles, diamond, heart)) or (has(name.Charles, spade) >= 1)
+(hand(name.David, club, spade)) or (has(name.David, spade) == 1)
+(hand(name.Frank, club, heart)) or (hand(name.Frank, club, spade))
+(hand(name.Jessica, club, spade)) or (
+    (has(name.Jessica, spade) == 1) and (has(name.Jessica, diamond) == 0))
+(has(name.Karen, diamond) == 0) or (has(name.Karen, spade) >= 1)
+(hand(name.Taylor, club, heart)) or (
+    has(name.Taylor, club) == 2 or
+    has(name.Taylor, diamond) == 2 or
+    has(name.Taylor, heart) == 2 or
+    has(name.Taylor, spade) == 2
+)
 """
 
 SOLUTION = """
-    name |    suit
-   Beth1 |   Spade
-   Beth2 |    Club
-Charles1 |   Heart
-Charles2 | Diamond
-  David1 | Diamond
-  David2 |   Spade
-  Frank1 |    Club
-  Frank2 |   Heart
-Jessica1 |   Heart
-Jessica2 |   Spade
-  Karen1 |    Club
-  Karen2 |    Club
- Taylor1 | Diamond
- Taylor2 | Diamond
+name_hand |    suit
+    Beth1 |   Spade
+    Beth2 |    Club
+ Charles1 |   Heart
+ Charles2 | Diamond
+   David1 | Diamond
+   David2 |   Spade
+   Frank1 |    Club
+   Frank2 |   Heart
+ Jessica1 |   Heart
+ Jessica2 |   Spade
+   Karen1 |    Club
+   Karen2 |    Club
+  Taylor1 | Diamond
+  Taylor2 | Diamond
 """
