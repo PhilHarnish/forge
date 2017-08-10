30cfe948ea1e7c5737fe0c6ae688c6918e1a100f
Phil Harnish <philharnish@gmail.com>
2017-08-09 20:44:46 -0700

    Add specs for 3.4.

---
+++ spec/puzzle/examples/mim/p3_4_spec.py
@@ -0,0 +1,41 @@
+import astor
+
+from data import warehouse
+from puzzle.examples.mim import p3_4
+from puzzle.problems import logic_problem
+from puzzle.puzzlepedia import prod_config
+from spec.mamba import *
+
+with _description('p3_4'):
+  with before.all:
+    warehouse.save()
+    prod_config.init()
+    self.puzzle = p3_4.get()
+
+  with after.all:
+    prod_config.reset()
+    warehouse.restore()
+
+  with description('solution'):
+    with it('scores the source as a LogicProblem'):
+      expect(logic_problem.LogicProblem.score(
+          p3_4.SOURCE.split('\n'))).to(equal(1))
+
+    with it('identifies puzzle type'):
+      problems = self.puzzle.problems()
+      expect(problems).to(have_len(1))
+      problem = problems[0]
+      expect(problem).to(be_a(logic_problem.LogicProblem))
+
+    with it('parses puzzle'):
+      node = logic_problem._parse(p3_4.SOURCE.split('\n'))
+      print(astor.to_source(node))
+
+    with _it('models puzzle'):
+      model = logic_problem._model(p3_4.SOURCE.split('\n'))
+      #model.add(model.dimension_constraints())
+      print(str(model))
+
+    with it('exports a solution'):
+      problem = self.puzzle.problems()[0]
+      expect(problem.solution).to(look_like(p3_4.SOLUTION))
