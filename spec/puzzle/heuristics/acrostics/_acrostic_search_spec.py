3f450087dd387fc9e1aff0bf8ee22c8551b4a6d8
Phil Harnish <philharnish@gmail.com>
2017-09-15 11:52:15 -0700

    Add _acrostic_search.
    This acrostic algorithm will prefer recursing with longer words over
    short words. Behaves a bit like BFS or A*.

---
+++ spec/puzzle/heuristics/acrostics/_acrostic_search_spec.py
@@ -0,0 +1,141 @@
+import collections
+
+from mock.mock import patch
+
+from data import word_frequencies
+from data.seek_sets import seek_set
+from puzzle.heuristics.acrostics import _acrostic_iter, _acrostic_search
+from spec.data.fixtures import tries
+from spec.mamba import *
+
+BA_PREFIX_TRIE = word_frequencies.load(
+    reversed(list(zip(
+        ('bad', 'bag', 'ban', 'bar', 'bat'),
+        range(10**9, 10**9 + 6)))))
+
+
+with description('acrostic'):
+  with description('basics'):
+    with it('uses a mock trie'):
+      a = _acrostic_search.AcrosticSearch(['is'])
+      expect(len(a._trie)).to(be_below(100))
+
+    with it('yields simple solutions'):
+      a = _acrostic_search.AcrosticSearch('is')
+      expect(list(a)).to(contain('is'))
+
+    with it('is observable'):
+      a = _acrostic_search.AcrosticSearch('is')
+      subs = mock.Mock()
+      a.subscribe(subs)
+      expect(subs.on_next.call_args).to(equal(mock.call('is')))
+
+    with it('yields multi-character solutions'):
+      a = _acrostic_search.AcrosticSearch(list('bag'), trie=BA_PREFIX_TRIE)
+      expect(list(a)).to(contain('bag'))
+
+    with it('yields unique solutions'):
+      a = _acrostic_search.AcrosticSearch(list('ba') + ['ggg'], trie=BA_PREFIX_TRIE)
+      expect(list(a)).to(have_len(1))
+
+    with it('yields multiple multi-character solutions'):
+      a = _acrostic_search.AcrosticSearch(list('ba') + ['dgnrt'], trie=BA_PREFIX_TRIE)
+      expect(list(a)).to(contain('bad', 'bag', 'ban', 'bar', 'bat'))
+
+  with description('_iter_phrases'):
+    with before.all:
+      self.subject = _acrostic_search.AcrosticSearch(['a'])
+
+    with it('yields nothing for empty input'):
+      phrases = self.subject._iter_phrases({})
+      expect(phrases).to(have_len(0))
+
+    with it('yields all items from single dict'):
+      input = [('first', 10), ('second', 9), ('third', 8), ('fourth', 7)]
+      p = {
+        1: collections.OrderedDict(input),
+      }
+      phrases = list(self.subject._iter_phrases(p))
+      expect(phrases).to(have_len(4))
+      expect(phrases).to(equal(input))
+
+    with it('yields items from multiple dicts in sorted order'):
+      input1 = [('a', 10), ('b', 9), ('c', 5), ('d', 4)]
+      input2 = [('e', 25), ('f', 8), ('g', 3), ('h', 1)]
+      input3 = [('i', 15), ('j', 7), ('k', 6), ('l', 2)]
+      p = {
+        1: collections.OrderedDict(input1),
+        2: collections.OrderedDict(input2),
+        3: collections.OrderedDict(input3),
+      }
+      phrases = list(self.subject._iter_phrases(p))
+      expect(phrases).to(have_len(len(input1 + input2 + input3)))
+      expect(phrases).to(equal(
+          sorted(input1 + input2 + input3, key=lambda x: x[1], reverse=True)
+      ))
+
+    with it('is used to cache results from previous walks'):
+      a = _acrostic_search.AcrosticSearch(list('ba') + ['dgnrt'], trie=BA_PREFIX_TRIE)
+      expect(list(a)).to(contain('bad', 'bag', 'ban', 'bar', 'bat'))
+      expect(list(a._phrase_graph[0][3].items())).to(equal([
+        ('bat', 1000000004), ('bar', 1000000003), ('ban', 1000000002),
+        ('bag', 1000000001), ('bad', 1000000000),
+      ]))
+
+  with context('when given ambiguous input text'):
+    with before.each:
+      self.patch = patch.object(_acrostic_iter, '_TARGET_WORD_SCORE_RATE', 1)
+      self.patch.start()
+
+    with after.each:
+      self.patch.stop()
+
+    with it('finds multiple words'):
+      a = _acrostic_search.AcrosticSearch(list('superbowl'), tries.ambiguous())
+      expect(list(a)).to(contain('super bowl', 'superb owl', 'superbowl'))
+
+    with it('finds multiple words in really long string'):
+      text = 'superbowlwarplanesnapshotscrapbookisnowhere'
+      a = _acrostic_search.AcrosticSearch(list(text), tries.ambiguous())
+      expect(''.join(next(iter(a)).split())).to(equal(text))
+
+    with it('finds same answer quickly'):
+      text = 'superbowlwarplanesnapshotscrapbookisnowhere'
+      a = _acrostic_search.AcrosticSearch(list(text), tries.ambiguous())
+      first = next(iter(a))
+      # Result should be cached and '_walk' should never be needed.
+      with patch.object(a, '_walk', side_effect=[]) as mock:
+        second = next(iter(a))
+        expect(first).to(equal(second))
+        expect(mock.call_count).to(be_below(10))
+
+    with description('with seek sets'):
+      with it('maintains old functionality'):
+        seeking = seek_set.SeekSet(list('superbowl'))
+        a = _acrostic_search.AcrosticSearch(seeking, tries.ambiguous())
+        expect(list(a)).to(contain('super bowl', 'superb owl', 'superbowl'))
+
+      with it('supports indexing'):
+        seeking = seek_set.SeekSet(['bad', 'bag', 'ban'], indexes=[1, 2, 3])
+        a = _acrostic_search.AcrosticSearch(seeking, trie=BA_PREFIX_TRIE)
+        expect(list(a)).to(equal(['ban']))
+
+      with it('supports ambiguous indexing'):
+        seeking = seek_set.SeekSet(['bad', 'bag', 'dgn'], indexes=[1, 2, None])
+        a = _acrostic_search.AcrosticSearch(seeking, trie=BA_PREFIX_TRIE)
+        expect(list(a)).to(equal(['ban', 'bag', 'bad']))
+
+      with it('supports permuted sets'):
+        seeking = seek_set.SeekSet(
+            ['dgn', 'aaa', 'bbb'],
+            sets_permutable=True)
+        a = _acrostic_search.AcrosticSearch(seeking, trie=BA_PREFIX_TRIE)
+        expect(list(a)).to(equal(['ban', 'bag', 'bad']))
+
+      with it('supports permuted sets (with indexes)'):
+        seeking = seek_set.SeekSet(
+            ['dgn', 'aaa', 'bbb'],
+            sets_permutable=True,
+            indexes=[1, 2, 3])
+        a = _acrostic_search.AcrosticSearch(seeking, trie=BA_PREFIX_TRIE)
+        expect(list(a)).to(equal(['ban']))
---
+++ src/puzzle/heuristics/acrostics/_acrostic_search.py
@@ -0,0 +1,50 @@
+from data import max_heap
+from puzzle.heuristics.acrostics import _acrostic_iter
+
+_EMPTY = (None, 0)
+# Try to average 3 letters per word.
+_TARGET_WORD_LEN = 4
+
+
+class AcrosticSearch(_acrostic_iter.AcrosticIter):
+  """Acrostic solver.
+
+  Acrostic search using A* search.
+  """
+  def _walk_phrase_graph_from(self, pos, acc, acc_weight):
+    # Goal:
+    # - `target` length phrases score 1 * weight.
+    # - 1 length phrases score (pos + 1)/target * weight.
+    target = self._solution_len
+    remaining_distance = target - pos
+    heap = max_heap.MaxHeap()
+    for phrase, weight in self._phrases_at(pos):
+      phrase_len = len(phrase)
+      scale = phrase_len / remaining_distance  # n / target : {0 < scale <= 1}.
+      heap.push(scale * weight, phrase)
+      best_weight = heap.best_weight()
+      while weight < best_weight:
+        # _phrases_at is yielding values which, even when scaled by 100% (i.e.
+        # full length) can not exceed the best score we have in the heap.
+        # Proceed from heap.
+        best_phrase = heap.pop()
+        yield from self._recurse_with(
+            pos, acc, acc_weight, target, best_phrase, best_weight)
+        best_weight = heap.best_weight()
+    while len(heap):
+      best_weight = heap.best_weight()
+      best_phrase = heap.pop()
+      yield from self._recurse_with(
+          pos, acc, acc_weight, target, best_phrase, best_weight)
+
+
+def _scored_solution(target_score, acc):
+  words = ' '.join(i[0] for i in acc)
+  min_weight = min(i[1] for i in acc)
+  num_words = len(acc)
+  weighted_score = (min_weight / num_words) / target_score
+  result = (
+    words,
+    min(1 / num_words, weighted_score)
+  )
+  return result
