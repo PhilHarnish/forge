601277b103eb38753bf1914ac85b6d9320da3066
Phil Harnish <philharnish@gmail.com>
2017-11-26 19:11:02 -0800

    Add end2end tests for AcrosticSearch.

---
+++ end2end/puzzle/heuristics/acrostics/_acrostic_search_e2e_spec.py
@@ -0,0 +1,34 @@
+from data import warehouse
+from data.seek_sets import seek_set
+from puzzle.heuristics.acrostics import _acrostic_search
+from puzzle.puzzlepedia import prod_config
+from spec.mamba import *
+
+
+with description('acrostic e2e'):
+  with before.each:
+    warehouse.save()
+    prod_config.init()
+
+  with after.all:
+    prod_config.reset()
+    warehouse.restore()
+
+  with it('finds saxophones'):
+    seeking = seek_set.SeekSet(
+        textwrap.dedent("""
+          BigOldBell
+          BootyJuker
+          CorkChoker
+          FacePinner
+          FakeTurtle
+          LemurPoker
+          PixieProng
+          SheepStick
+          SqueezeToy
+          TinyStools
+        """.lower()).strip().split('\n'),
+        sets_permutable=True,
+        indexes=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+    a = _acrostic_search.AcrosticSearch(seeking)
+    expect(next(iter(a))).to(equal('saxophones'))
