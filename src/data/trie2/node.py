71c8149b735730668d938680a5c4dec959231f3f
Phil Harnish <philharnish@gmail.com>
2017-12-02 11:50:57 -0800

    Move Node into separate module.

---
+++ src/data/trie2/node.py
@@ -0,0 +1,80 @@
+class Node(object):
+  __slots__ = (
+    '_char_mask',
+    '_length_mask',
+    '_max_weight',
+    '_match_weight',
+    '_children',
+  )
+
+  def __init__(self):
+    self._char_mask = 0
+    self._length_mask = 0
+    self._max_weight = 0
+    self._match_weight = 0
+    self._children = {}
+
+  def get(self, k):
+    return self._children.get(k, None)
+
+  def add(self, k, v):
+    l = len(k)
+    pos = 0
+    cursor = self
+    char_masks = _char_masks(k)
+    while pos < l:
+      c = k[pos]
+      cursor._char_mask |= char_masks[pos]
+      cursor._length_mask |= 2 ** (l - pos)
+      cursor._max_weight = max(cursor._max_weight, v)
+      # Traverse to next.
+      cursor_next = cursor.get(c)
+      if not cursor_next:
+        break
+      cursor = cursor_next
+      pos += 1
+      has_children = bool(cursor._length_mask)
+      if pos == l:
+        if cursor._match_weight:
+          # Duplicate addition.
+          raise KeyError('%s already added' % k)
+        # Exhausted input; this spells something new.
+        cursor._match_weight = v
+        return
+      elif not has_children:
+        break
+    # Add remaining characters.
+    while pos < l:
+      c = k[pos]
+      pos += 1
+      cursor_next = Node()
+      cursor._children[c] = cursor_next
+      cursor = cursor_next
+      cursor._char_mask = char_masks[pos]
+      cursor._length_mask = 2 ** (l - pos)
+      cursor._max_weight = v
+    cursor._match_weight = v
+
+  def __repr__(self):
+    chars = []
+    for i in range(26):
+      if self._char_mask & (2 ** i):
+        chars.append(chr(ord('a') + i))
+    if self._length_mask:
+      # Convert mask to binary, reverse, and swap "01" for " #"
+      lengths = bin(self._length_mask)[:1:-1].replace(
+          '0', ' ').replace('1', '#')
+    else:
+      lengths = ''
+    return '_Node(%s, %s, %s)' % (
+      repr(''.join(chars)), repr(lengths), self._match_weight)
+
+def _char_masks(s):
+  result = [0]
+  acc = 0
+  for c in s[::-1]:
+    if c < 'a' or c > 'z':
+      raise IndexError('Trie2 cannot index %s' % c)
+    acc |= 2 ** (ord(c) - 97)  # ord('a') == 97.
+    result.append(acc)
+  return list(reversed(result))
