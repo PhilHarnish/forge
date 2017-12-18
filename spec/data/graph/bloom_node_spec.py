7525de47dcf9d339fa664d4175de9958ddbe6521
Phil Harnish <philharnish@gmail.com>
2017-12-17 18:31:22 -0800

    BloomNode now supports source nodes.
    This allows graphs of BloomNodes to be composed together to solve
    constraints simultaneously. For example, a Trie of BloomNodes merged
    with a graph representing a query will yield all matching results from
    a tree.

--- spec/data/graph/bloom_node_spec.py
+++ spec/data/graph/bloom_node_spec.py
@@ -6,6 +6,7 @@ with description('construction'):
   with it('constructs without error'):
     expect(calling(bloom_node.BloomNode)).not_to(raise_error)
 
+
 with description('populating'):
   with before.each:
     self.subject = bloom_node.BloomNode()
@@ -27,6 +28,7 @@ with description('populating'):
     with it('retrieves a created node'):
       abc = self.subject.open('abc')
       expect(self.subject.open('abc')).to(equal(abc))
+
 
 with description('api'):
   with before.each:
@@ -51,6 +53,36 @@ with description('api'):
     self.subject.require(0b110)
     expect(self.subject.provide_mask).to(equal(0b111))
 
+
+with description('sources'):
+  with before.each:
+    def add_node(node: bloom_node.BloomNode, key: str) -> None:
+      child = node.open(key)
+      child.require(0b111)
+      child.distance(0)
+      child.weight(1, True)
+    self.a = bloom_node.BloomNode()
+    add_node(self.a, 'common')  # First bit.
+    add_node(self.a, 'a_only')  # Second bit.
+    self.b = bloom_node.BloomNode()
+    add_node(self.b, 'common')  # First bit.
+    add_node(self.b, 'b_only')  # Third bit.
+    self.combined = bloom_node.BloomNode([self.a, self.b])
+
+  with it('does not find missing nodes'):
+    expect(lambda: self.combined['missing']).to(raise_error(KeyError))
+
+  with it('does find common nodes'):
+    expect(lambda: self.combined['common']).not_to(raise_error(KeyError))
+
+  with it('propagates common attributes'):
+    c = self.combined['common']
+    expect(c.provide_mask).to(equal(0b111))
+    expect(c.require_mask).to(equal(0b111))
+    expect(c.lengths_mask).to(equal(0b1))
+    expect(c.match_weight).to(equal(1))
+
+
 with description('repr'):
   with before.each:
     self.subject = bloom_node.BloomNode()
@@ -64,3 +96,36 @@ with description('repr'):
     self.subject.distance(3)
     self.subject.distance(2)
     expect(repr(self.subject)).to(equal("BloomNode('AbC', '  ##', 0)"))
+
+
+with description('reduce'):
+  with before.each:
+    self.a = bloom_node.BloomNode()
+    self.a.require(0b111)
+    self.a.distance(0)
+    self.a.weight(1, True)
+
+  with it('ignores empty input'):
+    expect(calling(bloom_node.reduce, [])).not_to(raise_error)
+
+  with it('returns a similar node for one element'):
+    outer = bloom_node.reduce([self.a])
+    expect(outer.provide_mask).to(equal(self.a.provide_mask))
+    expect(outer.require_mask).to(equal(self.a.require_mask))
+    expect(outer.lengths_mask).to(equal(self.a.lengths_mask))
+    expect(outer.match_weight).to(equal(self.a.match_weight))
+    expect(repr(outer)).to(equal(repr(self.a)))
+
+  with it('merges similar nodes'):
+    b = bloom_node.BloomNode()
+    b.require(0b101)
+    b.provide_mask = 0b1111  # Simulate providing more than needing.
+    self.a.provide_mask = 0b1111
+    b.distance(0)
+    b.weight(0.5, True)
+    outer = bloom_node.reduce([self.a, b])
+    expect(outer.provide_mask).to(equal(0b1111))
+    expect(outer.require_mask).to(equal(0b111))
+    expect(outer.lengths_mask).to(equal(0b1))
+    expect(outer.match_weight).to(equal(0.5))
+    expect(repr(outer)).to(equal("BloomNode('ABCd', '#', 0.5)"))
--- src/data/graph/bloom_node.py
+++ src/data/graph/bloom_node.py
@@ -1,4 +1,5 @@
-from typing import Dict, Optional
+import itertools
+from typing import Dict, Iterable, List, Optional
 
 
 class BloomNode(object):
@@ -10,6 +11,7 @@ class BloomNode(object):
     'max_weight',
     'min_weight',
     'match_weight',
+    '_sources',
     '_edges',
   )
 
@@ -25,16 +27,19 @@ class BloomNode(object):
   min_weight: float
   # Match value for this node.
   match_weight: float
+  # Input sources for this node.
+  _sources: Optional[Iterable['BloomNode']]
   # Outgoing edges of this node.
   _edges: Dict[str, 'BloomNode']
 
-  def __init__(self) -> None:
+  def __init__(self, sources: Optional[Iterable['BloomNode']] = None) -> None:
     self.provide_mask = None
     self.require_mask = None
     self.lengths_mask = None
     self.match_weight = 0
     self.max_weight = 0
     self.min_weight = 0
+    self._sources = sources
     self._edges = {}
 
   def distance(self, length: int) -> None:
@@ -45,11 +50,15 @@ class BloomNode(object):
     else:
       self.lengths_mask |= mask
 
-  def open(self, k: str) -> 'BloomNode':
+  def open(self, key: str) -> 'BloomNode':
     """Return outgoing edge `k`. Create node if necessary."""
-    if k not in self._edges:
-      self._edges[k] = BloomNode()
-    return self._edges[k]
+    if key not in self._edges:
+      child = self._find(key)
+      if child is None:
+        self._edges[key] = BloomNode()
+      else:
+        self._edges[key] = child
+    return self._edges[key]
 
   def require(self, mask: int) -> None:
     """Declare requirements for this node."""
@@ -64,6 +73,9 @@ class BloomNode(object):
       # Anything a node requires is implicitly provided.
       self.provide_mask |= mask
 
+  def satisfies(self, other: 'BloomNode') -> bool:
+    return other.require_mask & self.provide_mask == other.require_mask
+
   def weight(self, weight: float, match: bool = False) -> None:
     """Assign weight to this node, optionally specifying a match here."""
     if match:
@@ -75,7 +87,33 @@ class BloomNode(object):
     self.min_weight = max(weight, self.min_weight)
 
   def __len__(self) -> int:
+    # TODO: Need to expand edges first.
     return len(self._edges)
+
+  def __contains__(self, key: str) -> bool:
+    if key not in self._edges:
+      child = self._find(key)
+      if child is not None:
+        self._edges[key] = child
+    return key in self._edges
+
+  def __getitem__(self, key: str) -> 'BloomNode':
+    if key not in self._edges:
+      child = self._find(key)
+      if child is not None:
+        self._edges[key] = child
+    return self._edges[key]
+
+  def _find(self, key: str) -> Optional['BloomNode']:
+    """Returns common node for `k` from sources, if any."""
+    if not self._sources:
+      return None
+    sources = []
+    for source in self._sources:
+      if key not in source:
+        return None
+      sources.append(source[key])
+    return reduce(sources)
 
   def __repr__(self) -> str:
     chars = []
@@ -93,3 +131,49 @@ class BloomNode(object):
     return '%s(%s, %s, %s)' % (
         self.__class__.__name__, repr(''.join(chars)), repr(lengths),
         self.match_weight)
+
+
+def reduce(sources: List['BloomNode']) -> Optional['BloomNode']:
+  if not sources or any(
+          not source.lengths_mask or
+          not source.provide_mask or
+          not source.max_weight or
+          source.require_mask is None
+              for source in sources):
+    return None
+  # First, verify there are matching masks. It's required for a valid
+  # solution and is trivial to verify satisfaction.
+  lengths_mask = sources[0].lengths_mask
+  provide_mask = sources[0].provide_mask
+  require_mask = sources[0].require_mask
+  match_weight = sources[0].match_weight
+  min_weight = sources[0].min_weight
+  max_weight = sources[0].max_weight
+  pos = 1
+  l = len(sources)
+  while pos < l and lengths_mask and (
+        provide_mask & require_mask) == require_mask:
+    source = sources[pos]
+    lengths_mask &= source.lengths_mask  # Overlapping solution lengths exist.
+    provide_mask &= source.provide_mask  # Overlapping letters are provided.
+    require_mask |= source.require_mask  # Requirements are combined.
+    min_weight = min(min_weight, source.min_weight)
+    max_weight = min(max_weight, source.max_weight)
+    match_weight = min(match_weight, source.match_weight)  # Trends to 0.
+    pos += 1
+  if (not lengths_mask or
+      (provide_mask & require_mask) != require_mask or
+      not max_weight):
+    return None  # Unsatisfiable; no common requirements.
+  # Verify all combinations are mutually satisfiable.
+  for a, b in itertools.combinations(sources, 2):
+    if not a.satisfies(b) or not b.satisfies(a):
+      return None
+  reduced = BloomNode(sources)
+  reduced.provide_mask = provide_mask
+  reduced.require_mask = require_mask
+  reduced.lengths_mask = lengths_mask
+  reduced.min_weight = min_weight
+  reduced.max_weight = max_weight
+  reduced.match_weight = match_weight
+  return reduced
