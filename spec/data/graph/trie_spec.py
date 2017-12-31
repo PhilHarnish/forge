5a5e925df2862301d581a2cad6cb09344512c943
Phil Harnish <philharnish@gmail.com>
2017-12-30 23:32:29 -0800

    Add ngram support to trie module.
    Given a list of unigrams, bigrams, trigrams, etc. produce a graph of
    Tries.
    
    A path from root through unigrams gives a weight equal to the
    probability for that unigram. A path through bigrams will give the
    probability that specific bigram would be chosen relative to the total
    count for the unigram prefix.
    
    Straying away from given ngrams will produce paths with greatly reduced
    probabilities. An invented ngram will have probability equal to the min
    probability for all of its prefixes.

--- spec/data/graph/trie_spec.py
+++ spec/data/graph/trie_spec.py
@@ -1,3 +1,5 @@
+from typing import List
+
 from data.graph import bloom_node, regex, trie
 from spec.mamba import *
 
@@ -19,6 +21,16 @@ _TEST_DATA = _scale([
     ('on', 3750423199),
     ('that', 3400031103),
 ])
+
+
+def path(root: bloom_node.BloomNode, path: str) -> List[str]:
+  result = []
+  cursor = root
+  for c in path:
+    result.append(repr(cursor))
+    cursor = cursor[c]
+  result.append(repr(cursor))
+  return result
 
 
 with description('add'):
@@ -49,7 +61,7 @@ with description('add'):
     node = bloom_node.BloomNode()
     trie.add(node, 'com', 0.5)
     trie.add(node, 'common', 1.0)
-    expecteds = [
+    expect(path(node, 'common')).to(equal([
       # START.
       "BloomNode('CMnO', '   #  #', 0)",
       # -> c.
@@ -64,11 +76,7 @@ with description('add'):
       "BloomNode('N', ' #', 0)",
       # -> n.
       "BloomNode('', '#', 1.0)",
-    ]
-    for c, expected in zip('common', expecteds):
-      expect(str(node)).to(equal(expected))
-      node = node[c]
-    expect(repr(node)).to(equal(expecteds[-1]))
+    ]))
 
 
 with description('merge'):
@@ -143,7 +151,9 @@ with description('test data, multiple word'):
         self.trie['i']['s'][' ']['i']['s'][' ']['i']['s'].match_weight))
 
   with it('should repr correctly in deep test'):
-    expecteds = [
+    expect(path(self.trie, 'is is is')).to(equal([
+      # START.
+      "BloomNode('adefhinorst; ', ' ####', 0)",
       # -> i.
       "BloomNode('ns; ', ' #', 0)",
       # -> s.
@@ -160,8 +170,120 @@ with description('test data, multiple word'):
       "BloomNode('ns; ', ' #', 0.0)",
       # -> s.
       "BloomNode(' ', '#', 0.008414501562828072)",
+    ]))
+
+
+with description('add_ngrams'):
+  with before.all:
+    self.unigrams = [
+      ('a', 50),
+      ('ab', 25),
+      ('abc', 15),
+      ('abcd', 10),
     ]
-    node = self.trie
-    for c, expected in zip('is is is', expecteds):
-      node = node[c]
-      expect(str(repr(node))).to(equal(expected))
+    self.bigrams = [
+      ('a a', 25),
+      ('a ab', 15),
+      ('a abc', 10),
+      ('ab ab', 10),
+      ('ab abc', 10),
+      ('ab abcd', 5),
+      ('abc abc', 10),
+      ('abc abcd', 5),
+      ('abcd abcd', 10),
+    ]
+
+  with it('ignores empty input'):
+    root = bloom_node.BloomNode()
+    trie.add_ngrams(root, [])
+    expect(root).to(have_len(0))
+
+  with it('adds unigrams'):
+    root = bloom_node.BloomNode()
+    trie.add_ngrams(root, [self.unigrams])
+    expect(repr(root)).to(equal("BloomNode('Abcd; ', ' ####', 0)"))
+
+  with it('buckets into percentiles'):
+    root = bloom_node.BloomNode()
+    unigrams = [
+      (c, i + 1) for c, i in zip('abcdefghij', range(26))
+    ]
+    trie.add_ngrams(root, [unigrams], n_percentiles=4)
+    expect(repr(root)).to(equal("BloomNode('abcdefghij; ', ' #', 0)"))
+    expect([
+      '%s: %s' % (c, root[c][' '].op) for c in 'abcdefghij'
+    ]).to(equal([
+      "a: (BloomNode('abcdefghij; ', ' #', 0)*0.01818181818181818)",
+      "b: (BloomNode('abcdefghij; ', ' #', 0)*0.01818181818181818)",
+      "c: (BloomNode('abcdefghij; ', ' #', 0)*0.01818181818181818)",
+      "d: (BloomNode('abcdefghij; ', ' #', 0)*0.07272727272727272)",
+      "e: (BloomNode('abcdefghij; ', ' #', 0)*0.07272727272727272)",
+      "f: (BloomNode('abcdefghij; ', ' #', 0)*0.10909090909090909)",
+      "g: (BloomNode('abcdefghij; ', ' #', 0)*0.10909090909090909)",
+      "h: (BloomNode('abcdefghij; ', ' #', 0)*0.10909090909090909)",
+      "i: (BloomNode('abcdefghij; ', ' #', 0)*0.16363636363636364)",
+      "j: (BloomNode('abcdefghij; ', ' #', 0)*0.16363636363636364)"
+    ]))
+
+  with it('adds unigrams and bigrams'):
+    root = bloom_node.BloomNode()
+    trie.add_ngrams(root, [self.unigrams, self.bigrams])
+    expect(repr(root)).to(equal("BloomNode('Abcd; ', ' ####', 0)"))
+
+  with description('unigrams and bigrams'):
+    with before.each:
+      self.trie = bloom_node.BloomNode()
+      trie.add_ngrams(self.trie, [self.unigrams, self.bigrams])
+
+    with it('expands unigrams'):
+      expect(path(self.trie, 'a')).to(equal([
+        "BloomNode('Abcd; ', ' ####', 0)",
+        "BloomNode('Bcd; ', '####', 0.5)",
+      ]))
+
+    with it('expands specified bigrams'):
+      expect(path(self.trie, 'a ab')).to(equal([
+        "BloomNode('Abcd; ', ' ####', 0)",
+        "BloomNode('Bcd; ', '####', 0.5)",  # c(a)/total = 50/100.
+        "BloomNode('Abcd; ', ' ####', 0)",
+        "BloomNode('Bcd; ', '####', 0.5)",  # c(a a)/c(a) = 25 / 50.
+        "BloomNode('Cd; ', '###', 0.3)",  # c(a ab)/c(a) = 15 / 50.
+      ]))
+
+    with it('expands invented bigrams'):
+      expect(path(self.trie, 'ab a')).to(equal([
+        "BloomNode('Abcd; ', ' ####', 0)",
+        "BloomNode('Bcd; ', '####', 0.5)",  # c(a)/total = 50/100.
+        "BloomNode('Cd; ', '###', 0.25)",  # c(ab)/total = 25/100.
+        "BloomNode('Abcd; ', ' ####', 0)",
+        # P(ab a) = P(ab *)*P(a) = c(ab a)/c(ab)*P(a) = min(ab *)/c(ab)*P(a) =
+        # 5/25 * .5 = .1.
+        "BloomNode('Bcd; ', '####', 0.1)",
+      ]))
+
+    with it('expands invented familiar trigrams'):
+      expect(path(self.trie, 'a a a')).to(equal([
+        "BloomNode('Abcd; ', ' ####', 0)",
+        "BloomNode('Bcd; ', '####', 0.5)",  # c(a)/total = 50/100.
+        "BloomNode('Abcd; ', ' ####', 0)",
+        "BloomNode('Bcd; ', '####', 0.5)",  # c(a a)/c(a) = 25/50.
+        "BloomNode('Abcd; ', ' ####', 0.0)",
+        "BloomNode('Bcd; ', '####', 0.25)",  # c(a a)/c(a) * P(a) = 25/50 * .5.
+      ]))
+
+    with it('expands invented unusual trigrams'):
+      node = self.trie['a']
+      node = node['b']
+      node = node[' ']
+      node = node['a']
+      node = node[' ']
+      node = node['a']
+      expect(path(self.trie, 'ab a a')).to(equal([
+        "BloomNode('Abcd; ', ' ####', 0)",
+        "BloomNode('Bcd; ', '####', 0.5)",  # c(a)/total = 50/100.
+        "BloomNode('Cd; ', '###', 0.25)",  # c(ab)/total = 25/100.
+        "BloomNode('Abcd; ', ' ####', 0)",
+        "BloomNode('Bcd; ', '####', 0.1)",
+        "BloomNode('Abcd; ', ' ####', 0.0)",
+        "BloomNode('Bcd; ', '####', 0.1)",
+      ]))
--- src/data/graph/trie.py
+++ src/data/graph/trie.py
@@ -1,9 +1,11 @@
 """Produce BloomNode graphs from words.
 
 TODO: Create a root Trie which lazy-expands based on sibling length_mask.
+TODO: Is log(p1) + log(p2) better than p1*p2?
 """
 from typing import List
 
+from data import types
 from data.graph import bloom_mask, bloom_node
 
 
@@ -28,6 +30,73 @@ def add_multi_word(
   loop_back = root * value
   cursor.link(' ', loop_back)
   return cursor
+
+
+def add_ngrams(
+    root: bloom_node.BloomNode,
+    ngrams: List[List[types.WeightedWord]],
+    n_percentiles: int = 10) -> None:
+  """Adds `ngrams` to `root`.
+
+  Notes:
+  The probability of a unigram match is:
+    P(w_i) = c(w_i) / total_count
+  The probability of a bigram match is:
+    P(w_i | w_i - 1) = c(w_i - 1, w_i) / c(w_i - 1)
+    Where:
+      * c(w_i - 1, w_i) is the count of n-gram suffixes for (w_i - 1).
+      * 1/c(w_i - 1) is the scale applied to all children of (w_i - 1).
+      * 1/c(w_i) = 1/(total_count * P(w_i)) =
+        1/(total_count * (c(w_i) / total_count)) = 1/(c(w_i))
+
+  :param root: Starting point of trie.
+  :param ngrams: Lists lists of (word, weight) tuples. First list is unigrams,
+      second is bigrams, etc.
+  :param n_percentiles: Number of percentiles to bucket loopback nodes to. Fewer
+      buckets means more word->word subtrees can be reused.
+  """
+  ngram_map = {}
+  for grams in ngrams:
+    total_count = sum(weight for _, weight in grams)
+    length = len(grams)
+    percentile_size = length / n_percentiles
+    min_scale = 0
+    scale_percentile = -1
+    for i, (ngram, count) in enumerate(grams):
+      # Calculate statistics.
+      percentile_group = i // percentile_size
+      if percentile_group != scale_percentile:
+        min_scale = count / total_count
+        scale_percentile = percentile_group
+      # Find prefix parent.
+      words = ngram.split(' ')
+      suffix = words.pop()
+      while words:
+        prefix = ' '.join(words)
+        if prefix in ngram_map:
+          last_leaf, last_count, last_scale, word_root = ngram_map[prefix]
+          word_probability = count / last_count
+          # Use this ngram to rescale prefix. If we learn the smallest n+1gram
+          # is X then any *unknown* n+1grams should be at least that small.
+          min_scale = min(last_scale, word_probability)  # This breaks %tiles.
+          ngram_map[prefix] = (last_leaf, last_count, min_scale, word_root)
+          break
+        raise NotImplementedError('Unable to graft %s, no prefix' % ngram)
+      else:
+        # Unigram: add the entire `suffix` onto `root`.
+        word_root = root
+        word_probability = count / total_count
+      leaf = _add(word_root, suffix, word_probability, True)
+      ngram_map[ngram] = (leaf, count, min_scale, bloom_node.BloomNode())
+  # Graft all of the ngram leaves back to root.
+  scaled_roots = {}
+  for leaf, _, scale, children in ngram_map.values():
+    if scale not in scaled_roots:
+      scaled_roots[scale] = root * scale
+    loop_back = scaled_roots[scale]
+    if children:
+      loop_back += children
+    leaf.link(' ', loop_back)
 
 
 def _add(
