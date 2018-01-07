f8173567e6015fb63d4ecc6ad4b74214fa33dca9
Phil Harnish <philharnish@gmail.com>
2018-01-07 08:51:37 -0800

    Record regex exits for each token.

--- src/data/graph/regex.py
+++ src/data/graph/regex.py
@@ -35,37 +35,54 @@ class _RegexVisitor(object):
       self._any = _LOWER_ALPHA
 
   def visit(self) -> bloom_node.BloomNode:
-    goal = bloom_node.BloomNode()
-    goal.distance(0)
-    goal.weight(self._weight, True)
-    return self._visit(goal, self._data)
+    exit = bloom_node.BloomNode()
+    exit.distance(0)
+    exit.weight(self._weight, True)
+    return self._visit(exit, self._data, [])
 
   def _visit(
-      self, cursor: bloom_node.BloomNode, data: list) -> bloom_node.BloomNode:
+      self,
+      cursor: bloom_node.BloomNode,
+      data: list,
+      exits: list) -> bloom_node.BloomNode:
     data = self._transform(data)
     for kind, value in reversed(data):
       fn_name = '_visit_%s' % kind
       if not hasattr(self, fn_name):
         raise NotImplementedError('Unsupported re type %s' % kind)
       visit_fn = getattr(self, fn_name)
-      cursor = visit_fn(cursor, value)
+      exits.append(('EXIT_%s' % kind, cursor, []))
+      cursor = visit_fn(cursor, value, exits)
     return cursor
 
   def _visit_ANAGRAM(
-      self, cursor: bloom_node.BloomNode, data: list) -> bloom_node.BloomNode:
+      self,
+      cursor: bloom_node.BloomNode,
+      data: list,
+      exits: list) -> bloom_node.BloomNode:
+    del data
+    del exits
     # TODO: If length of list is 1 then all literals can be anagrammed.
     return cursor
 
   def _visit_ANY(
-      self, cursor: bloom_node.BloomNode, data: list) -> bloom_node.BloomNode:
+      self,
+      cursor: bloom_node.BloomNode,
+      data: list,
+      exits: list) -> bloom_node.BloomNode:
+    del exits
     assert data is None
     next_cursor = bloom_node.BloomNode()
     next_cursor.links(self._any, cursor)
     return next_cursor
 
   def _visit_IN(
-      self, cursor: bloom_node.BloomNode, data: list) -> bloom_node.BloomNode:
+      self,
+      cursor: bloom_node.BloomNode,
+      data: list,
+      exits: list) -> bloom_node.BloomNode:
     """Character group."""
+    del exits
     edges = []
     next_cursor = bloom_node.BloomNode()
     for datum in data:
@@ -77,7 +94,11 @@ class _RegexVisitor(object):
     return next_cursor
 
   def _visit_LITERAL(
-      self, cursor: bloom_node.BloomNode, data: int) -> bloom_node.BloomNode:
+      self,
+      cursor: bloom_node.BloomNode,
+      data: int,
+      exits: list) -> bloom_node.BloomNode:
+    del exits
     value = self._visit_value_LITERAL(data)
     next_cursor = bloom_node.BloomNode()
     next_cursor.link(value, cursor)
@@ -87,36 +108,42 @@ class _RegexVisitor(object):
     return next_cursor
 
   def _visit_MAX_REPEAT(
-      self, cursor: bloom_node.BloomNode, data: list) -> bloom_node.BloomNode:
+      self,
+      cursor: bloom_node.BloomNode,
+      data: list,
+      exits: list) -> bloom_node.BloomNode:
     start, end, pattern = data
     if str(end) == 'MAXREPEAT':
       raise NotImplementedError('Unable to repeat MAXREPEAT')
     chain_start = cursor
-    exits = []
+    exit_nodes = []
     if start == 0:
       # Path to cursor is possible either via chain or original cursor.
-      exits.append(cursor)
+      exit_nodes.append(cursor)
       link_count = end - 1
     else:
       link_count = end - start
     for _ in range(link_count + 1):
-      chain_start = self._visit(chain_start, pattern)
+      chain_start = self._visit(chain_start, pattern, exits[-1][-1])
       # Path may go through this chain.
-      exits.append(chain_start)
-    result = exits[0]
-    for exit in exits[1:]:
-      result += exit
+      exit_nodes.append(chain_start)
+    result = exit_nodes[0]
+    for exit_node in exit_nodes[1:]:
+      result += exit_node
     return result
 
   def _visit_SUBPATTERN(
-      self, cursor: bloom_node.BloomNode, data: list) -> bloom_node.BloomNode:
+      self,
+      cursor: bloom_node.BloomNode,
+      data: list,
+      exits: list) -> bloom_node.BloomNode:
     group_id, x, y, pattern = data
     assert x == 0
     assert y == 0
     exit = cursor
     group_name = self._inverted_groups.get(group_id, group_id)
     exit.annotations({'EXIT_%s' % group_name: group_id})
-    enter = self._visit(cursor, pattern)
+    enter = self._visit(cursor, pattern, exits[-1][-1])
     enter.annotations({'ENTER_%s' % group_name: group_id})
     return enter
 
