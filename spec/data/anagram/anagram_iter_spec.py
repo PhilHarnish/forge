6dcf595d555b83efd17a73e4358c40113fcf41c7
Phil Harnish <philharnish@gmail.com>
2018-02-09 10:32:23 -0800

    Introduce AnagramIter module.
    Supports iterating through anagrams (permutations) of input. Features:
    * Understands and removes duplicate inputs
    * Results with common suffix (remaining anagrams) are reused

---
+++ spec/data/anagram/anagram_iter_spec.py
@@ -0,0 +1,75 @@
+from data.anagram import anagram_iter
+from spec.mamba import *
+
+
+with description('anagram_iter.from_choices'):
+  with it('initializes empty input without error'):
+    expect(calling(anagram_iter.from_choices, [])).not_to(
+        raise_error)
+    expect(anagram_iter.from_choices([])).to(
+        be_a(anagram_iter.AnagramIter))
+
+  with it('initializes with different iterables'):
+    cases = (
+        # Primitives.
+        ['a', 'a', 'a'],
+        ['a', 'b', 'c'],
+        [1, 2, 3],
+        'iterable',
+        # Functions.
+        [abs, id, int],  # Returns int.
+        [ascii, str, repr],  # Returns str.
+        [id, id, id],  # Duplicates.
+    )
+    for arg in cases:
+      expect(calling(anagram_iter.from_choices, arg)).not_to(raise_error)
+
+
+with description('simple iterative usage'):
+  with it('iterates 1 result for 1 input'):
+    ats = anagram_iter.from_choices([13])
+    expect(list(ats)).to(equal([13]))
+
+  with it('lists items for 1 input'):
+    ats = anagram_iter.from_choices([13])
+    expect(list(ats.items())).to(equal([(13, ats[13])]))
+
+  with it('raises for invalid requests'):
+    expect(lambda: anagram_iter.from_choices([13])[11]).to(
+        raise_error(KeyError, 11))
+
+  with it('exhausts input'):
+    ats = anagram_iter.from_choices([13])
+    expect(list(ats[13].items())).to(equal([]))
+    expect(lambda: ats[13][13]).to(raise_error(KeyError, 13))
+
+  with it('lists options for 2 unique inputs'):
+    ats = anagram_iter.from_choices([13, 17])
+    expect(list(ats.items())).to(equal([
+      (13, ats[13]),
+      (17, ats[17]),
+    ]))
+
+  with it('lists one option for 2 duplicate inputs'):
+    ats = anagram_iter.from_choices([13, 13])
+    expect(list(ats.items())).to(equal([
+      (13, ats[13]),
+    ]))
+
+  with it('duplicate items are both available'):
+    ats = anagram_iter.from_choices([13, 13])
+    expect(list(ats[13].items())).to(equal([(13, ats[13][13])]))
+
+  with it('duplicate items are exhaustable'):
+    expect(calling(lambda: anagram_iter.from_choices([13, 13])[13][13][13])).to(
+        raise_error(KeyError, 13))
+
+
+with description('complex iterative usage'):
+  with it('merges paths with common suffix'):
+    ats = anagram_iter.from_choices('abc')
+    expect(ats['a']['b']).to(equal(ats['b']['a']))
+
+  with it('merges paths with duplicates'):
+    ats = anagram_iter.from_choices('abcaa')
+    expect(ats['a']['b']).to(equal(ats['b']['a']))
---
+++ src/data/anagram/anagram_iter.py
@@ -0,0 +1,103 @@
+from typing import ItemsView, Iterable, List, Optional, TypeVar
+
+T = TypeVar('T')  # Generic type.
+
+
+class _AnagramIterIndex(object):
+  def __init__(self, choices: List[T]) -> None:
+    num_choices = len(choices)
+    assert num_choices < 63
+    self._choices = {}
+    self._choice_map = {}
+    self._choices_masks = {}
+    self._initial_choice_mask = 0
+    last_choice = None
+    idx = None
+    for i, choice in enumerate(sorted(choices, key=id)):
+      if last_choice == choice:
+        continue
+      last_choice = choice
+      if idx is not None:
+        # 0b01100 = (0b10000 - 1) - (0b00100 - 1)
+        # 0b01100 =  0b01111      -  0b00011
+        self._choices_masks[idx] = ((1 << i) - 1) - (idx - 1)
+      idx = 1 << i
+      self._choices[idx] = choice
+      self._choice_map[choice] = idx
+      self._initial_choice_mask |= idx
+    if idx is not None:
+      self._choices_masks[idx] = ((1 << num_choices) - 1) - (idx - 1)
+    self._child_cache = {}
+    self.initial_available = (1 << num_choices) - 1
+
+  def available(self, available: int) -> Iterable[T]:
+    # Only iterate through IDs which align with initial choice mask.
+    for candidate in _ids(available & self._initial_choice_mask):
+      yield self._choices[candidate]
+
+  def get(self, available: int, item: T) -> 'AnagramIter':
+    idx = self._choice_map[item]
+    mask = self._choices_masks[idx]
+    match = available & mask
+    if not match:
+      raise KeyError(item)
+    available -= (match - (match >> 1)) & mask
+    return self._get(available)
+
+  def _get(self, available: int) -> 'AnagramIter':
+    if available not in self._child_cache:
+      self._child_cache[available] = AnagramIter(self, available)
+    return self._child_cache[available]
+
+  def items(self, available: int) -> ItemsView[T, 'AnagramIter']:
+    # Only iterate through IDs which align with initial choice mask.
+    for idx in _ids(available & self._initial_choice_mask):
+      mask = self._choices_masks[idx]
+      match = available & mask
+      child_available = available - ((match - (match >> 1)) & mask)
+      yield self._choices[idx], self._get(child_available)
+
+
+class AnagramIter(object):
+  __slots__ = ('_index', '_available')
+
+  def __init__(
+      self,
+      index: _AnagramIterIndex,
+      available: Optional[int] = None) -> None:
+    self._index = index
+    if available is None:
+      self._available = index.initial_available
+    else:
+      self._available = available
+
+  def items(self) -> ItemsView[T, 'AnagramIter']:
+    yield from self._index.items(self._available)
+
+  def __iter__(self) -> Iterable[T]:
+    yield from self._index.available(self._available)
+
+  def __getitem__(self, item: T) -> 'AnagramIter':
+    return self._index.get(self._available, item)
+
+  def __repr__(self) -> str:
+    return '%s(%s)' % (
+      self.__class__.__name__,
+      bin(self._available),  # TODO: Improve.
+    )
+
+  __str__ = __repr__
+
+
+def from_choices(choices: Iterable[T]) -> AnagramIter:
+  if not isinstance(choices, list):
+    choices = list(choices)
+  return AnagramIter(_AnagramIterIndex(choices))
+
+
+def _ids(available: int) -> Iterable[int]:
+  while available:
+    next_available = available & (available - 1)
+    lowest_choice = available - next_available
+    yield lowest_choice
+    available = next_available
