8cd7bada93aa91b55485a01845deda76d2d1af6b
Phil Harnish <philharnish@gmail.com>
2018-02-19 13:22:39 -0800

    Update perf to generate reports.
    Also permit temp stubbing for use in tests.

--- spec/util/perf_spec.py
+++ spec/util/perf_spec.py
@@ -11,20 +11,24 @@ _perf_counter_patch = mock.patch(
 with description('perf'):
   with before.each:
     self._patch = _perf_counter_patch.start()
+    perf.push()
 
   with after.each:
     _perf_counter_patch.stop()
+    perf.pop()
 
   with it('constructs for 2 variants, by default'):
-    expect(calling(perf.Head2Head)).not_to(raise_error)
-    expect(str(perf.Head2Head())).to(look_like("""
+    expect(calling(perf.Head2Head, 'test')).not_to(raise_error)
+    expect(str(perf.Head2Head('test'))).to(look_like("""
+        test
         #0: 0 (0 calls)
         #1: 0 (0 calls)
     """))
 
   with it('benchmarks variable number of variants'):
-    b = perf.Head2Head(4)
+    b = perf.Head2Head('test', 4)
     expect(str(b)).to(look_like("""
+        test
         #0: 0 (0 calls)
         #1: 0 (0 calls)
         #2: 0 (0 calls)
@@ -32,7 +36,10 @@ with description('perf'):
     """))
 
   with it('increments time with start & end calls'):
-    b = perf.Head2Head(1)
+    b = perf.Head2Head('test', 1)
     b.start()
     b.end()
-    expect(str(b)).to(equal('#0: 1.0/s (1 calls)'))
+    expect(str(b)).to(look_like("""
+        test
+        #0: 1.0/s, 1.0x (1 calls)
+    """))
--- src/util/perf.py
+++ src/util/perf.py
@@ -2,12 +2,18 @@ import time
 from typing import Optional
 
 
+_STATES = []
+_ALL_PERFS = []
+
+
 class Head2Head(object):
-  def __init__(self, variants: int = 2) -> None:
+  def __init__(self, name, variants: int = 2) -> None:
+    self._name = name
     self._idx = 0
     self._variants = variants
     self._running = None
     self._timings = [(0, 0, 0)] * variants
+    _ALL_PERFS.append(self)
 
   def variant(self) -> int:
     return self._running or (self._idx % self._variants)
@@ -38,13 +44,32 @@ class Head2Head(object):
     self._timings[variant] = (calls, elapsed + (end - last), end)
 
   def __str__(self) -> str:
-    results = []
+    results = [self._name]
+    if any(elapsed for _, elapsed, _ in self._timings):
+      best = min(elapsed for _, elapsed, _ in self._timings if elapsed)
+    else:
+      best = 0
     for variant, (calls, elapsed, last) in enumerate(self._timings):
       if elapsed:
         cps = calls / elapsed
-        results.append('#%s: %s/s (%s calls)' % (variant, cps, calls))
+        x = elapsed / best
+        results.append('#%s: %s/s, %sx (%s calls)' % (variant, cps, x, calls))
       elif calls:
         results.append('#%s: inf (%s calls)' % (variant, calls))
       else:
         results.append('#%s: 0 (0 calls)' % variant)
     return '\n'.join(results)
+
+
+def report() -> str:
+  return '\n\n'.join(str(perf) for perf in _ALL_PERFS)
+
+
+def push() -> None:
+  global _ALL_PERFS
+  _STATES.append(_ALL_PERFS)
+  _ALL_PERFS = []
+
+def pop() -> None:
+  global _ALL_PERFS
+  _ALL_PERFS = _STATES.pop()
