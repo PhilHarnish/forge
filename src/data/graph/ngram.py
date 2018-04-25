cc7e0d0b24ae091ead77b6e1f9ef8e06f846893a
Phil Harnish <philharnish@gmail.com>
2018-04-25 08:02:47 -0700

    Fix ngrams with prefix match_weights.
    Cleanup unused code.

--- src/data/graph/ngram.py
+++ src/data/graph/ngram.py
@@ -39,7 +39,7 @@ def _get(
     lengths_mask: int) -> bloom_node.BloomNode:
   return _NGRAM_ROOT(
       prefix,
-      _ngrams_prefix(prefix, initial_mask, require_mask, lengths_mask),
+      _ngrams(prefix, initial_mask, require_mask, lengths_mask),
       merge=_merge_expand_initial)
 
 
@@ -64,56 +64,13 @@ def _ngrams(
     n_words = 1
   else:
     n_words = prefix.count(' ') + 2
-  for initial, length_entries in index(_FILES[n_words - 1]).items():
-    initial_alpha = bloom_mask.for_alpha(initial)
-    if not initial_alpha & initial_mask:
-      continue
-    length_entries_length = len(length_entries)
-    if lengths_mask:
-      length_mask_remaining = lengths_mask
-    else:
-      length_mask_remaining = (1 << length_entries_length) - 1
-    pos = 0
-    while pos < length_entries_length and length_mask_remaining:
-      cursor = 1 << pos
-      entries = length_entries[pos]
-      pos += 1
-      if not (cursor & length_mask_remaining):
-        continue
-      length_mask_remaining -= cursor  # Remove this bit from mask.
-      for entry in entries:
-        word, weight, masks = entry
-        if not prefix:
-          pass
-        elif not word.startswith(prefix):
-          continue
-        else:
-          # Matching prefix.
-          pass
-        _, word_requires, _ = masks
-        if require_mask and (require_mask & word_requires) != require_mask:
-          continue  # Some of require_mask was missing from word.
-        results.append(entry)
-  return results
-
-
-def _ngrams_prefix(
-    prefix: str,
-    initial_mask: int = bloom_mask.REQUIRE_NOTHING,
-    require_mask: int = bloom_mask.REQUIRE_NOTHING,
-    lengths_mask: int = bloom_mask.ANY_LENGTHS,
-) -> List[NgramEntry]:
-  results = []
-  if not prefix:
-    n_words = 1
-  else:
-    n_words = prefix.count(' ') + 2
   ngram_index = prefix_index(_FILES[n_words - 1])
   key = hash(prefix)
   if key not in ngram_index:
     return results
   for entry in ngram_index[key]:
-    word, weight, masks = entry
+    word, weight, mask_tuples = entry
+    _, masks, _ = mask_tuples
     if not masks & require_mask:
       continue
     word_initial_alpha = bloom_mask.for_alpha(word[0])
@@ -126,37 +83,10 @@ def _ngrams_prefix(
   return results
 
 
-@pickle_cache.cache('data/graph/ngram/index')
-def index(src: str) -> Dict[str, List[List[NgramEntry]]]:
-  """Open `src` and return a list of lists of lists of int.
-
-  The leaves (char, int, tuple), nested recursively, and describe require_mask.
-  These leaf entries are wrapped as (words, weight, masks) in a List.
-  Next, a List, ordered by length.
-  Finally, a Dict keyed by initial character.
-  """
-  result = {c: [] for c in bloom_mask.BASE_ALPHABET}
-  for line in data.open_project_path(src):
-    parts = line.rpartition('\t')
-    words = parts[0]
-    _, _, word = words.rpartition(' ')  # Discard other words.
-    initial = word[0]
-    row = result[initial]
-    length = len(word)
-    missing = length - len(row)
-    if missing > 0:
-      row.extend([] for _ in range(missing))
-    masks = _char_masks(word)
-    weight = int(parts[2])
-    row[length - 1].append((words, weight, masks))
-  return result
-
-
 @pickle_cache.cache('data/graph/ngram/prefix_index')
 def prefix_index(src: str) -> Dict[int, List[List[NgramEntry]]]:
   """TODO: Docs."""
   result = {}
-  collisions = {}  # TODO: Delete. Only needed for 1 round of end2end testing.
   for line in data.open_project_path(src):
     words, _, weight = line.rpartition('\t')
     prefix, _, word = words.rpartition(' ')
@@ -165,9 +95,6 @@ def prefix_index(src: str) -> Dict[int, List[List[NgramEntry]]]:
     key = hash(prefix)
     if key not in result:
       result[key] = []
-      collisions[key] = prefix
-    else:
-      assert collisions[key] == prefix
     result[key].append((word, weight, masks))
   return result
 
@@ -268,10 +195,13 @@ def _merge_expand_entries(
 def _link_child(
     host: bloom_node.BloomNode, prefix: str, initial: str, match_weight: int,
     ngrams: Iterable[ChildEntry]) -> None:
+  if not ngrams and not match_weight:
+    raise TypeError('No children or match_weight for %s' % initial)
   if ngrams:
     child_node = _NGRAM_ROOT(prefix, ngrams, merge=_merge_expand_entries)
-  elif match_weight:
+  else:
     child_node = bloom_node.BloomNode()
+  if match_weight:
     child_node.weight(match_weight, match=True)
     child_node.distance(0)
     root = _get_scaled_root(_LOOPBACK_SCALE)
@@ -280,8 +210,6 @@ def _link_child(
         prefix, bloom_mask.REQUIRE_NOTHING, bloom_mask.REQUIRE_NOTHING,
         bloom_mask.ANY_LENGTHS)
     _cheap_link(child_node, ' ', root + ngrams)
-  else:
-    raise TypeError('No children or match_weight for %s' % initial)
   _cheap_link(host, initial, child_node)
 
 
@@ -293,7 +221,6 @@ def _cheap_link(
   child.op = None  # HACK: Avoid early expansion.
   parent.link(initial, child)
   child.op = tmp_op
-
 
 
 def _unpack_initial_ngrams(
