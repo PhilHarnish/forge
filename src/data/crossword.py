6ecaeca959b82d82fbba02dc6d9d9ea09ffa70d9
Phil Harnish <philharnish@gmail.com>
2019-02-18 09:56:10 -0800

    Add typing to crossword.py.

--- src/data/crossword.py
+++ src/data/crossword.py
@@ -1,5 +1,6 @@
 import re
 import sqlite3
+from typing import Dict, Iterable, List, Tuple
 
 from data import data, warehouse
 
@@ -15,19 +16,21 @@ _IGNORED = frozenset([
   'that', 'the', 'to',
   'with',
 ])
+_KeywordsType = Dict[str, int]
 
-def _normalize_clue(clue):
+
+def _normalize_clue(clue: str) -> str:
   clue = clue.strip()
   clue = re.sub(_WHITESPACE_RE, ' ', clue)
   clue = re.sub(_REMOVE_RE, '', clue)
   return clue
 
 
-def tokenize_clue(clue):
+def tokenize_clue(clue: str) -> List[str]:
   return _normalize_clue(clue.lower()).split()
 
 
-def clue_keywords(clue):
+def clue_keywords(clue: str) -> List[str]:
   words_api = warehouse.get('/api/words')
   results = []
   for keyword in tokenize_clue(clue):
@@ -39,14 +42,14 @@ def clue_keywords(clue):
   return results
 
 
-def connect(db):
+def connect(db: str) -> Tuple[sqlite3.Connection, sqlite3.Cursor]:
   if not db.startswith(':'):
     db = data.project_path(db)
   conn = sqlite3.connect(db)
-  return (conn, conn.cursor())
+  return conn, conn.cursor()
 
 
-def init(db):
+def init(db: str) -> sqlite3.Connection:
   conn, cursor = connect(db)
   # Erase previous table.
   cursor.execute('DROP TABLE IF EXISTS clues')
@@ -62,7 +65,9 @@ def init(db):
   return conn
 
 
-def add(cursor, solution, usages, keywords):
+def add(
+    cursor: sqlite3.Cursor, solution: str, usages: str,
+    keywords: _KeywordsType):
   cmd = 'INSERT INTO clues VALUES (?, ?, ?)'
   try:
     cursor.execute(cmd, (solution, usages, _format_keyword_counts(keywords)))
@@ -71,7 +76,7 @@ def add(cursor, solution, usages, keywords):
     raise
 
 
-def query(cursor, clue):
+def query(cursor: sqlite3.Cursor, clue: str) -> List[Tuple[str, int, _KeywordsType]]:
   results = []
   cmd = """
     SELECT solution, usages, keywords
@@ -85,22 +90,22 @@ def query(cursor, clue):
   return results
 
 
-def _format_keywords(keywords):
+def _format_keywords(keywords: Iterable[str]):
   return '[%s]' % ']['.join(sorted(keywords))
 
 
-def _format_keywords_query(keywords):
+def _format_keywords_query(keywords: List[str]):
   return '[%s(' % '(%['.join(sorted(keywords))
 
 
-def _format_keyword_counts(keywords):
+def _format_keyword_counts(keywords: _KeywordsType) -> str:
   parts = []
   for kvp in keywords.items():
     parts.append('%s(%s)' % kvp)
   return _format_keywords(parts)
 
 
-def _query_keywords(keywords):
+def _query_keywords(keywords: str) -> _KeywordsType:
   parts = keywords.strip('][').split('][')
   keywords = {}
   for part in parts:
