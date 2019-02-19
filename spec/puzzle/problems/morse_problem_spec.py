cab5d2d0ef4e09f9521dbc0cbd961d81e0fa9da3
Phil Harnish <philharnish@gmail.com>
2019-02-18 18:37:05 -0800

    Implement sufficient "morse_problem" to solve samples available.
    Ranking results would require comparison against a Trie. Shouldn't be
    hard but... doesn't seem necessary right now.
    
    Also: doesn't support brute force. I.e., delimiters are required to get
    reasonable output.

--- spec/puzzle/problems/morse_problem_spec.py
+++ spec/puzzle/problems/morse_problem_spec.py
@@ -2,42 +2,130 @@ from puzzle.problems import morse_problem
 from spec.mamba import *
 
 SAMPLE = '- .... .. ... / .. ... / -- --- .-. ... . / -.-. --- -.. .'
+PUZZLE = """
+|.|..
+.-|.-
+|-.-.
+|..-|
+.-|-|
+..|--
+-|-.|
+""".strip().split('\n')
+
 
 with description('MorseProblem'):
-  with it('ignores empty input'):
-    expect(morse_problem.MorseProblem.score([''])).to(equal(0))
+  with description('score'):
+    with it('ignores empty input'):
+      expect(morse_problem.MorseProblem.score([''])).to(equal(0))
 
-  with it('matches multiple lines'):
-    expect(morse_problem.MorseProblem.score([
-      '...',
-      '---',
-      '...',
-    ])).to(equal(1))
+    with it('matches multiple lines'):
+      expect(morse_problem.MorseProblem.score([
+        '...',
+        '---',
+        '...',
+      ])).to(equal(1))
 
-  with it('matches morse explicitly'):
-    expect(morse_problem.MorseProblem.score([
-      '... --- ...'
-    ])).to(equal(1))
+    with it('matches morse explicitly'):
+      expect(morse_problem.MorseProblem.score([
+        '... --- ...'
+      ])).to(equal(1))
 
-  with it('matches morse "," delimited'):
-    expect(morse_problem.MorseProblem.score([
-      '.../---/...'
-    ])).to(equal(1))
+    with it('matches morse "/" delimited'):
+      expect(morse_problem.MorseProblem.score([
+        '.../---/...'
+      ])).to(equal(1))
 
-  with it('matches morse "|" delimited'):
-    expect(morse_problem.MorseProblem.score([
-      '...|---|...'
-    ])).to(be_above(0.9))
+    with it('matches morse "|" delimited'):
+      expect(morse_problem.MorseProblem.score([
+        '...|---|...'
+      ])).to(be_above(0.9))
 
-  with it('scores longer non-morse strings than short ones'):
-    long = morse_problem.MorseProblem.score(['aaabbbaaa'])
-    short = morse_problem.MorseProblem.score(['aba'])
-    expect(long).to(be_above(short))
+    with it('scores longer non-morse strings than short ones'):
+      long = morse_problem.MorseProblem.score(['aaabbbaaa'])
+      short = morse_problem.MorseProblem.score(['aba'])
+      expect(long).to(be_above(short))
 
-  with it('scores dot and dash more confidently'):
-    good = morse_problem.MorseProblem.score(['...;---;...'])
-    ambiguous = morse_problem.MorseProblem.score(['aaa;bbb;aaa'])
-    expect(good).to(be_above(ambiguous))
+    with it('scores dot and dash more confidently'):
+      good = morse_problem.MorseProblem.score(['...;---;...'])
+      ambiguous = morse_problem.MorseProblem.score(['aaa;bbb;aaa'])
+      expect(good).to(be_above(ambiguous))
 
-  with it('accepts SAMPLE'):
-    expect(morse_problem.MorseProblem.score([SAMPLE])).to(equal(1))
+    with it('accepts SAMPLE'):
+      expect(morse_problem.MorseProblem.score([SAMPLE])).to(equal(1))
+
+    with it('accepts PUZZLE'):
+      expect(morse_problem.MorseProblem.score(PUZZLE)).to(be_above(.9))
+
+  with description('_generate_interpretations') as self:
+    with before.all:
+      self.fn = getattr(morse_problem, '_generate_interpretations')
+
+    with it('suggests attempting a swap for clean input'):
+      expect([tuple(pair) for _, pair in self.fn('...---...')]).to(
+          equal([
+            ('.', '-', None, None, set()),
+            ('-', '.', None, None, set())
+          ]))
+
+    with it('resists attempting a swap for improbable but valid input'):
+      expect([tuple(pair) for _, pair in self.fn('---...---')]).to(
+          equal([
+            ('.', '-', None, None, set()),
+            ('-', '.', None, None, set())
+          ]))
+
+    with it('attempts to identify delimiters'):
+      expect([tuple(pair) for _, pair in self.fn(SAMPLE)]).to(
+          equal([
+            ('.', '-', ' ', '/', set()),
+            ('.', '-', ' ', None, {'/'}),
+            ('.', '-', None, None, {'/', ' '}),
+            ('-', '.', ' ', '/', set()),
+            ('-', '.', ' ', None, {'/'}),
+            ('-', '.', None, None, {'/', ' '}),
+          ]))
+
+    with it('cannot decide which delimiter is which'):
+      expect([tuple(pair) for _, pair in self.fn('.../--- ...')]).to(
+          equal([
+            ('.', '-', '/', ' ', set()),
+            ('.', '-', ' ', '/', set()),
+            ('.', '-', '/', None, {' '}),
+            ('.', '-', ' ', None, {'/'}),
+            ('.', '-', None, None, {'/', ' '}),
+            ('.', '-', None, None, {'/', ' '}),
+            ('-', '.', '/', ' ', set()),
+            ('-', '.', ' ', '/', set()),
+            ('-', '.', '/', None, {' '}),
+            ('-', '.', ' ', None, {'/'}),
+            ('-', '.', None, None, {'/', ' '}),
+            ('-', '.', None, None, {'/', ' '}),
+          ]))
+
+    with it('is willing to interpret puzzle with newlines'):
+      expect([tuple(pair) for _, pair in self.fn('\n'.join(PUZZLE))]).to(
+          equal([
+            ('.', '-', '|', '\n', set()),
+            ('.', '-', '\n', '|', set()),
+            ('.', '-', '|', None, {'\n'}),
+            ('.', '-', '\n', None, {'|'}),
+            ('.', '-', None, None, {'|', '\n'}),
+            ('.', '-', None, None, {'|', '\n'}),
+            ('-', '.', '|', '\n', set()),
+            ('-', '.', '\n', '|', set()),
+            ('-', '.', '|', None, {'\n'}),
+            ('-', '.', '\n', None, {'|'}),
+            ('-', '.', None, None, {'|', '\n'}),
+            ('-', '.', None, None, {'|', '\n'}),
+          ]))
+
+  with description('solutions'):
+    with it('solves SAMPLE'):
+      solutions = morse_problem.MorseProblem('ex', [SAMPLE]).solutions()
+      expect(solutions).to(have_keys('this is morse code', 'thisismorsecode'))
+      expect(solutions['this is morse code']).to(
+          be_above(solutions['thisismorsecode']))
+
+    with it('solves PUZZLE'):
+      solutions = morse_problem.MorseProblem('ex', PUZZLE).solutions()
+      expect(solutions).to(have_key('evacuation'))
--- src/puzzle/problems/morse_problem.py
+++ src/puzzle/problems/morse_problem.py
@@ -1,24 +1,109 @@
 import collections
-from typing import ItemsView, Iterable, List
+import itertools
+from typing import Iterable, List, NamedTuple, Optional, Tuple
 
+from data import max_heap
+from data.alphabets import morse
 from puzzle.problems import problem
 
-_ALPHABET = '.- /'  # " " = letter separator, "/" = word separator.
+_IGNORE_DELIMITER = 'ignored'
+_CHARACTER_DELIMITER = 'character delimiter'
+_WORD_DELIMITER = 'word delimiter'
+_DOT = '.'
+_DASH = '-'
+
+class _Interpretation(NamedTuple):
+  dot: str
+  dash: str
+  character_delimiter: Optional[str]
+  word_delimiter: Optional[str]
+  ignored: set
+
+
+# "Most common" interpretation.
+_ALPHABET = _Interpretation(
+    '.',
+    '-',
+    ' ',
+    '/',
+    set(),
+)
 
 
 class MorseProblem(problem.Problem):
   def __init__(self, name: str, lines: List[str], **kwargs) -> None:
     super(MorseProblem, self).__init__(name, lines, **kwargs)
-    self._frequencies = collections.Counter()
-    for line in lines:
-      self._frequencies.update(line)
+    self._normalized = '\n'.join(lines)
+    self._frequencies = collections.Counter(self._normalized)
+    for c in list(self._frequencies.keys()):
+      if c.isspace():  # Ignore whitespace in input.
+        del self._frequencies[c]
 
   @staticmethod
   def score(lines: List[str]) -> float:
     return _score(lines)
 
-  def _solve_iter(self) -> Iterable[ItemsView[str, float]]:
-    yield 'asdf', 1
+  def _solve_iter(self) -> problem.Solutions:
+    fringe: max_heap.MaxHeap[problem.Solutions] = max_heap.MaxHeap()
+    solution_buffer: max_heap.MaxHeap[problem.Solutions] = max_heap.MaxHeap()
+    # Initialize options with weights.
+    for weight, interpretation in _generate_interpretations(self._normalized):
+      fringe.push(weight, self._iter_interpretation(interpretation))
+    while fringe:
+      group_generator, group_weight = fringe.pop_with_weight()
+      if fringe:
+        next_best_weight = fringe.best_weight()
+      else:
+        next_best_weight = float('-inf')
+      yield from solution_buffer.pop_with_weight_until(next_best_weight)
+      for result in group_generator:
+        (solution, weight), notes = result
+        self._notes[solution] = notes  # Save (and throw away) notes.
+        solution_weight = group_weight * weight
+        if solution_weight > next_best_weight:
+          yield solution, solution_weight
+        else:
+          solution_buffer.push(solution_weight, solution)
+          # Abandon this generator for now.
+          fringe.push(group_weight, group_generator)
+          break
+    yield from solution_buffer.pop_with_weight_until(float('-inf'))
+
+  def _iter_interpretation(
+      self, interpretation: _Interpretation) -> problem.Solutions:
+    acc = []
+    result = []
+    for c in self._normalized:
+      if c == interpretation.dot:
+        acc.append('.')
+      elif c == interpretation.dash:
+        acc.append('-')
+      elif c in (
+          interpretation.character_delimiter, interpretation.word_delimiter):
+        buffer = ''.join(acc)
+        acc.clear()
+        if not buffer:
+          pass
+        elif buffer in morse.LOOKUP:
+          result.append(morse.LOOKUP[buffer])
+        else:
+          # Turns out this was an invalid interpretation?
+          return
+        if c == interpretation.word_delimiter:
+          result.append(' ')
+      elif c in interpretation.ignored:
+        continue
+      else:
+        raise ValueError(
+            'Unexpected "%s" for interpretation %s' % (c, interpretation))
+    if acc:
+      buffer = ''.join(acc)
+      if buffer in morse.LOOKUP:
+        result.append(morse.LOOKUP[buffer])
+      else:
+        return
+    solution = (''.join(result), 1.0)
+    yield solution, _interpretation_notes(interpretation)
 
 
 def _score(lines: List[str]) -> float:
@@ -29,7 +114,7 @@ def _score(lines: List[str]) -> float:
     size += len(line)
     if len(counts) > len(_ALPHABET):
       return 0
-  if len(counts) <= 1:
+  if len(counts) < 2:
     return 0  # Minimal input.
   # There are [1, _ALPHABET] symbols.
   if all(c in _ALPHABET for c in counts):
@@ -41,3 +126,122 @@ def _score(lines: List[str]) -> float:
   # Increase confidence asymptotically with the size of input. Long inputs
   # with only 3 symbols are very morse-like.
   return (1 - margin_of_error) + (margin_of_error * (1 - 1 / size))
+
+
+def _generate_interpretations(
+    given: str) -> Iterable[Tuple[float, _Interpretation]]:
+  results = []
+  frequencies = collections.Counter(given)
+  (_, first_n), (_, second_n) = frequencies.most_common(2)
+  max_score = first_n * first_n + second_n + 1  # +1 to reserve true 1.0.
+  def _scored_interpretation(
+      dot: str, dash: str, character_delimiter: Optional[str],
+      word_delimiter: Optional[str], ignored: set
+  ) -> Tuple[float, _Interpretation]:
+    interpretation = _Interpretation(
+        dot, dash, character_delimiter, word_delimiter, ignored)
+    if dot == '.' and dash == '-':
+      # Very probable assignment.
+      base_score = max_score
+    else:
+      base_score = (first_n * frequencies[dot] + frequencies[dash])
+    if ignored and not word_delimiter:
+      penalty = len(ignored)  # Discourage wasted characters.
+    else:
+      penalty = 0
+    # Prefer assigning (dot, dash) to 1st and 2nd most common, respectively.
+    return (
+      base_score / (max_score + penalty),
+      interpretation,
+    )
+  n_chars = len(frequencies)
+  for x, y in itertools.combinations(frequencies, 2):
+    if n_chars == 2:
+      character_delimiter = None
+      word_delimiter = None
+      ignored = set()
+      results.append(_scored_interpretation(
+          x, y, character_delimiter, word_delimiter, ignored))
+      results.append(_scored_interpretation(
+          y, x, character_delimiter, word_delimiter, ignored))
+    elif n_chars == 3:
+      character_delimiter = None
+      for c in frequencies:
+        if c in (x, y):
+          continue
+        character_delimiter = c
+        break
+      word_delimiter = None
+      ignored = set()
+      results.append(_scored_interpretation(
+          x, y, character_delimiter, word_delimiter, ignored))
+      results.append(_scored_interpretation(
+          y, x, character_delimiter, word_delimiter, ignored))
+    else:
+      # Need to choose both a character and word delimiter.
+      options = []
+      for c in frequencies:
+        if c in (x, y):
+          continue
+        options.append(c)
+      # 2+ items in options. Validate delimiters never repeat.
+      last = None
+      for character_delimiter, word_delimiter in itertools.permutations(
+          options, 2):
+        need_char = False  # Allow arbitrary delimiters to start.
+        saw_word_delimiter = False  # Require character between word_delimiter.
+        for c in given:
+          if c != word_delimiter:
+            pass
+          elif saw_word_delimiter:
+            break  # Two word delimiters occurred between a character.
+          else:
+            saw_word_delimiter = True
+          if c in (x, y):
+            need_char = False  # Found a character. Reset expectations.
+            saw_word_delimiter = False
+          elif need_char and c == last:
+            break  # Needed a character and delimiter repeated. Break.
+          elif c in (character_delimiter, word_delimiter):
+            need_char = True  # Expect a character.
+          last = c
+        else:
+          # 'break' never happened
+          ignored = set()
+          for c in options:
+            if c in (character_delimiter, word_delimiter):
+              continue
+            ignored.add(c)
+          results.append(_scored_interpretation(
+              x, y, character_delimiter, word_delimiter, ignored))
+          results.append(_scored_interpretation(
+              y, x, character_delimiter, word_delimiter, ignored))
+          # Perhaps delimiters are ignored.
+          ignored = ignored.copy()
+          ignored.add(word_delimiter)
+          results.append(_scored_interpretation(
+              x, y, character_delimiter, None, ignored))
+          results.append(_scored_interpretation(
+              y, x, character_delimiter, None, ignored))
+          ignored = ignored.copy()
+          ignored.add(character_delimiter)
+          results.append(_scored_interpretation(
+              x, y, None, None, ignored))
+          results.append(_scored_interpretation(
+              y, x, None, None, ignored))
+  return sorted(results, key=lambda key: key[0], reverse=True)
+
+
+def _interpretation_notes(interpretation) -> List[str]:
+  parts = [
+    'dot: %s' % repr(interpretation.dot),
+    'dash: %s' % repr(interpretation.dash),
+  ]
+  if interpretation.character_delimiter:
+    parts.append(
+        'character delimiter: %s' % repr(interpretation.character_delimiter))
+  if interpretation.word_delimiter:
+    parts.append('space: %s' % repr(interpretation.word_delimiter))
+  if interpretation.ignored:
+    parts.append('ignored: %s' % repr(''.join(sorted(interpretation.ignored))))
+  return parts
