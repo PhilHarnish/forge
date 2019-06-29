879f43373a0a3626ce07886d62689dae374b2634
Phil Harnish <philharnish@gmail.com>
2019-06-28 17:23:57 -0700

    Update grid_e2e_spec with latest behavior.

--- spec/data/image/grid_e2e_spec.py
+++ spec/data/image/grid_e2e_spec.py
@@ -6,11 +6,14 @@ import cv2
 import numpy as np
 from PIL import Image
 
-from data import data
+from data import data, warehouse
 from data.image import grid
+from puzzle.puzzlepedia import prod_config
 from spec.mamba import *
 
 _FILE_PATTERN = '*.png'
+_FOCUS = {
+}
 
 
 def image_path(pattern: str, subdir: str = 'original') -> str:
@@ -26,6 +29,8 @@ def images(subdir: str = 'original') -> Iterator[np.ndarray]:
 
 def grids(subdir: str = 'original') -> Iterator[Tuple[str, grid.Grid]]:
   for name, image in images(subdir):
+    if _FOCUS and name not in _FOCUS:
+      continue
     yield name, grid.Grid(image)
 
 
@@ -38,6 +43,14 @@ def diff_property(prop: str) -> None:
 
 
 with description('grid', 'end2end') as self:
+  with before.all:
+    warehouse.save()
+    prod_config.init()
+
+  with after.all:
+    prod_config.reset()
+    warehouse.restore()
+
   with it('diffs grayscale'):
     with benchmark(2500):
       diff_property('grayscale')
@@ -69,30 +82,28 @@ with description('grid', 'end2end') as self:
         'askew.png',  # Strange angles.
         'nonogram.png',  # Number bank.
         'pentopia.png',  # Shape bank.
-        'skyscraper.png',  # Numbers on perimeter.
         'strimko.png',  # Round shapes.
         'wordsearch.png',  # Fractional cell sizes.
-        'wordsearch_with_bank.png',  # Strange shape with word bank.
+        'wordsearch_with_bank.png',  # Strange shape and word bank.
         # Temporarily broken:
-        'fillomino.png',  # Grid optimizations.
+        'kakuro.png',  # With numbers extracted borders are too thick.
         'thermo.png',  # Grid optimizations.
       }
-      focus = {}
       expected = {
         'cages.png': grid.Dimensions(rows=14, columns=14),
         'crossword.png': grid.Dimensions(rows=15, columns=15),
+        'castlewall.png': grid.Dimensions(rows=11, columns=12),
         'fillomino.png': grid.Dimensions(rows=10, columns=10),
         'kakuro.png': grid.Dimensions(rows=11, columns=11),
         'kenken.png': grid.Dimensions(rows=6, columns=6),
-        'masyu.png': grid.Dimensions(rows=12, columns=12),
+        'masyu.png': grid.Dimensions(rows=12, columns=13),
         'multi.png': grid.Dimensions(rows=15, columns=15),
         'nurimaze.png': grid.Dimensions(rows=17, columns=17),
+        'skyscraper.png': grid.Dimensions(rows=6, columns=6),
         'slitherlink.png': grid.Dimensions(rows=9, columns=10),
         'thermo.png': grid.Dimensions(rows=10, columns=9),  # Has title row.
       }
       for name, g in grids():
-        if focus and name not in focus:
-          continue
-        elif name in todo:
+        if name in todo:
           continue
         expect(g.dimensions).to(equal(call(expected.get, name)))
