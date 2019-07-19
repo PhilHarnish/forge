eeb19ebdde0abd942c87ac51c24e58b3d4e5146a
Phil Harnish <philharnish@gmail.com>
2019-07-18 19:25:58 -0700

    Add method for clustering top color values. Add tests for coloring.

---
+++ spec/data/image/coloring_spec.py
@@ -0,0 +1,46 @@
+import numpy as np
+
+from data.image import coloring
+from spec.mamba import *
+
+with description('coloring'):
+  with description('colors') as self:
+    with before.each:
+      self.tolist = lambda x: np.array(list(x)).tolist()
+
+    with it('returns 0 colors if requested'):
+      expect(coloring.colors(0)).to(be_empty)
+
+    with it('returns only black and white if requested'):
+      expect(self.tolist(coloring.colors(2, with_black_and_white=True))).to(
+          equal(np.array([
+            [0, 0, 0],
+            [255, 255, 255]
+          ]).tolist()))
+
+    with it('returns unique colors'):
+      expect(self.tolist(coloring.colors(200))).to(be_unique)
+
+  with description('top_n_color_clusters'):
+    with it('generates empty output for empty input'):
+      expect(coloring.top_n_color_clusters(np.array([]), 10)).to(be_empty)
+
+    with it('ignores values near threshold'):
+      expect(list(coloring.top_n_color_clusters(np.array([
+        1000, 0, 1000,
+      ]), 2, threshold=1))).to(be_empty)
+
+    with it('yields values away from threshold'):
+      expect(list(coloring.top_n_color_clusters(np.array([
+        1000, 0, 500, 0, 1000,
+      ]), 3, threshold=1))).to(equal([[2]]))
+
+    with it('large values near each other are clustered'):
+      expect(list(coloring.top_n_color_clusters(np.array([
+        1000, 0, 700, 500, 600, 0, 1000,
+      ]), 6, threshold=1))).to(equal([[2, 3, 4]]))
+
+    with it('clusters are split if there are gaps'):
+      expect(list(coloring.top_n_color_clusters(np.array([
+        1000, 0, 700, 0, 600, 0, 1000,
+      ]), 6, threshold=1))).to(equal([[2], [4]]))
--- src/data/image/coloring.py
+++ src/data/image/coloring.py
@@ -1,48 +1,23 @@
 import colorsys
-from typing import List
+from typing import Iterable, List
 
-import cv2
 import numpy as np
 
 _MAX_COLORS_PER_HLS_SLICE = 7
-_BLACK = np.fromiter([0, 0, 0], dtype=np.int32)
-_WHITE = np.fromiter([255, 255, 255], dtype=np.int32)
+_MAX = 255
+_THRESHOLD = 5
+_BLACK = np.array([0, 0, 0], dtype=np.uint8)
+_WHITE = np.array([_MAX, _MAX, _MAX], dtype=np.uint8)
 
 
-def color_components(
-    n_components: int, result: np.ndarray, labels: np.ndarray,
-    stats: np.ndarray) -> np.ndarray:
-  """Colors n largest components."""
-  n_stats = stats.shape[0]
-  color_list = colors(n_stats, with_black_and_white=True)
-  sizes_sorted = sorted(
-      list(range(n_stats)),
-      key=lambda i: stats[i, cv2.CC_STAT_AREA],
-      reverse=True)
-  # Invert sizes_sorted array to produce a color map.
-  color_map = [0] * len(sizes_sorted)
-  for idx, i in enumerate(sizes_sorted):
-    color_map[i] = idx
-  height, width = labels.shape
-  for y in range(height):
-    for x in range(width):
-      color_idx = color_map[labels[y, x]]
-      if color_idx >= n_components:
-        color_idx = 0  # Erase this component to leave only n_components.
-      color = color_list[color_idx]
-      result[y, x] = color
-  return result
-
-
-def colors(n: int, with_black_and_white=False) -> List[np.ndarray]:
+def colors(n: int, with_black_and_white=False) -> Iterable[np.ndarray]:
   """Returns n (or more) colors."""
   if with_black_and_white:
-    result = [_BLACK, _WHITE]
-  else:
-    result = []
-  n -= len(result)
+    n -= 2
+    yield _BLACK
+    yield _WHITE
   if n <= 0:
-    return result
+    return
   n_slices = int(n / _MAX_COLORS_PER_HLS_SLICE) + 1
   lightness_scale = 0.25  # 50% +/- 25%.
   for slice_n in range(n_slices):
@@ -56,9 +31,34 @@ def colors(n: int, with_black_and_white=False) -> List[np.ndarray]:
       lightness = .5 + lightness_scale * (slice_n / n_slices)
       saturation = 1.0
       r, g, b = colorsys.hls_to_rgb(hue, lightness, saturation)
-      result.append(
-          np.fromiter(
-              [int(r * 255), int(g * 255), int(b * 255)], dtype=np.uint8))
+      yield np.array(
+          [int(r * 255), int(g * 255), int(b * 255)], dtype=np.uint8)
     n -= _MAX_COLORS_PER_HLS_SLICE
     lightness_scale *= -1
-  return result
+
+
+def top_n_color_clusters(
+    counts: np.ndarray, n: int, threshold: int = _THRESHOLD
+) -> Iterable[List[int]]:
+  """Given a histogram of counts, cluster and return top N values."""
+  max_value = len(counts)
+  if not max_value:
+    return
+  top_n = list(range(max_value - 1, max_value - n - 1, -1))
+  partitioned = np.argpartition(counts, top_n)
+  batch = []
+  last = None
+  positions = []
+  for i in top_n:
+    if (counts[partitioned[i]] and
+        threshold < partitioned[i] < (max_value - threshold)):
+      positions.append(partitioned[i])
+  for position in sorted(positions):
+    if last and abs(last - position) > threshold:
+      yield batch
+      # Start a new batch.
+      batch = []
+    batch.append(position)
+    last = position
+  if batch:
+    yield batch
