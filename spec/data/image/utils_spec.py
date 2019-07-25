178921dac90939ec2aa69be217c8c4d14afb3886
Phil Harnish <philharnish@gmail.com>
2019-07-25 08:26:21 -0700

    Move image manipulation from geometry/np2d to image/utils.

--- spec/data/image/utils_spec.py
+++ spec/data/image/utils_spec.py
@@ -29,3 +29,33 @@ with description('utils'):
       ])
       actual = utils.crop(given, w)
       expect(calling(np.array_equal, expected, actual)).to(equal(True))
+
+  with description('kernel_circle'):
+    with it('returns a point for size 1'):
+      expect(utils.kernel_circle(1).tolist()).to(equal([[1]]))
+
+    with it('returns a cross at small sizes'):
+      expect(utils.kernel_circle(3).tolist()).to(
+          equal(utils.kernel_cross(3).tolist()))
+
+    with it('returns a circle shape at larger sizes'):
+      expect(utils.kernel_circle(5).tolist()).to(equal([
+        [0, 0, 1, 0, 0],
+        [0, 1, 1, 1, 0],
+        [1, 1, 1, 1, 1],
+        [0, 1, 1, 1, 0],
+        [0, 0, 1, 0, 0]
+      ]))
+
+  with description('kernel_cross'):
+    with it('returns a point for size 1'):
+      expect(utils.kernel_cross(1).tolist()).to(equal([[1]]))
+
+    with it('returns a cross shape at larger sizes'):
+      expect(utils.kernel_cross(5).tolist()).to(equal([
+        [0, 0, 1, 0, 0],
+        [0, 0, 1, 0, 0],
+        [1, 1, 1, 1, 1],
+        [0, 0, 1, 0, 0],
+        [0, 0, 1, 0, 0]
+      ]))
--- src/data/image/utils.py
+++ src/data/image/utils.py
@@ -16,3 +16,22 @@ def crop(
     src = src[:-1]
   src = np.swapaxes(src, 0, 1)  # Swap x/y axis.
   return src
+
+
+def kernel_circle(size: int) -> np.ndarray:
+  radius = size // 2
+  # Create a slice which centers cursor at origin.
+  y, x = np.ogrid[-radius:radius + 1, -radius:radius + 1]
+  mask = x ** 2 + y ** 2 <= radius ** 2
+  array = np.zeros((size, size), np.uint8)
+  array[mask] = 1
+  return array
+
+
+def kernel_cross(size: int) -> np.ndarray:
+  array = np.zeros((size, size), np.uint8)
+  for x in range(size):
+    middle = (size - 1) >> 1
+    array[middle][x] = 1
+    array[x][middle] = 1
+  return array
