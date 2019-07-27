528c324c0674d5653bc7c9af5a5fa1f311a59ee0
Phil Harnish <philharnish@gmail.com>
2019-07-27 11:55:08 -0700

    Improve typing for puzzle module.

--- src/puzzle/puzzlepedia/puzzle.py
+++ src/puzzle/puzzlepedia/puzzle.py
@@ -1,48 +1,64 @@
+from typing import List, Type, Union
+
+import numpy as np
 from rx import subjects
 
-from data import observable_meta
+from data import meta, observable_meta
 from puzzle.heuristics import analyze
+from puzzle.problems import problem
 from puzzle.puzzlepedia import solution_stream
+
+PuzzleTypes = Union['Puzzle', problem.ProblemData]
 
 
 class Puzzle(subjects.Subject):
-  def __init__(self, name, source, hint=None, **kwargs):
+  _meta_problems: List['_MetaProblem']
+  _child_streams: List[solution_stream.SolutionStream]
+
+  def __init__(
+      self, name: str, source: PuzzleTypes, hint: str = None, **kwargs) -> None:
     super(Puzzle, self).__init__()
     self._meta_problems = []
     self._child_streams = []
     if isinstance(source, str):
-      lines = [line for line in source.split('\n') if line]
+      data = [line for line in source.split('\n') if line]
     elif isinstance(source, list):
-      lines = source
+      data = source
     elif isinstance(source, Puzzle):
-      lines = source.solutions()
+      data = source.solutions()
+    elif isinstance(source, np.ndarray):
+      data = source
     else:
       raise NotImplementedError(
           'Puzzle source type %s unsupported' % type(source))
     for i, (meta_problem, consumed) in enumerate(
-        analyze.identify_problems(lines, hint=hint)):
-      problem = _reify(meta_problem, '#%s' % i, consumed, **kwargs)
-      self._meta_problems.append(problem)
-      self._child_streams.append(solution_stream.SolutionStream(
-          str(i), problem))
+        analyze.identify_problems(data, hint=hint)):
+      p = _reify(meta_problem, '#%s' % i, consumed, **kwargs)
+      self._meta_problems.append(p)
+      self._child_streams.append(solution_stream.SolutionStream(str(i), p))
     self._observable = solution_stream.SolutionStream(
         name, observable_meta.ObservableMeta(), self._child_streams)
     self._observable.subscribe(self)
 
-  def problem(self, index):
+  def problem(self, index: int) -> '_MetaProblem':
     return self._meta_problems[index]
 
-  def problems(self):
+  def problems(self) -> List['problem.Problem']:
     return [p.active for p in self._meta_problems]
 
-  def solutions(self):
+  def solutions(self) -> List[str]:
     return [p.solution for p in self._meta_problems]
 
-  def get_next_stage(self):
+  def get_next_stage(self) -> 'Puzzle':
     return Puzzle('meta', self)
 
 
-def _reify(meta_problem, name, lines, **kwargs):
+def _reify(
+    meta_problem: meta.Meta[Type[problem.Problem]],
+    name: str,
+    lines: problem.ProblemData,
+    **kwargs,
+) -> '_MetaProblem':
   result = _MetaProblem()
   for value, weight in meta_problem.items():
     result[value(name, lines, **kwargs)] = weight
@@ -51,25 +67,25 @@ def _reify(meta_problem, name, lines, **kwargs):
 
 class _MetaProblem(observable_meta.ObservableMeta):
   # Sentinel value for 'no solution found'.
-  _NO_SOLUTION = {}
+  _NO_SOLUTION = object()
 
-  def __init__(self):
+  def __init__(self) -> None:
     super(_MetaProblem, self).__init__()
     self._active = None
     self._solution = self._NO_SOLUTION
 
   @property
-  def active(self):
+  def active(self) -> problem.Problem:
     return self._active or self.peek()
 
   @property
-  def solution(self):
+  def solution(self) -> str:
     if self._solution is self._NO_SOLUTION:
-      return self.active.solutions().peek()
+      self._solution = self.active.solutions().peek()
     return self._solution
 
   @solution.setter
-  def solution(self, value):
+  def solution(self, value: str):
     if self._solution == value:
       return
     self._solution = value
