975ed316bf042f49dee41eba324c3022a420c804
Phil Harnish <philharnish@gmail.com>
2019-08-03 21:14:42 -0700

    Updated types for observable_meta.

--- src/data/observable_meta.py
+++ src/data/observable_meta.py
@@ -1,15 +1,15 @@
 from rx import subjects
 
-from data import meta
+from data import meta, types
 
 
-class ObservableMeta(meta.Meta):
-  def __init__(self, *args, **kwargs):
+class ObservableMeta(meta.Meta[meta.Key]):
+  def __init__(self, *args, **kwargs) -> None:
     super(ObservableMeta, self).__init__(*args, **kwargs)
     self.subject = subjects.Subject()
 
-  def subscribe(self, observer):
+  def subscribe(self, observer: types.Observer) -> None:
     self.subject.subscribe(observer)
 
-  def _changed(self):
+  def _changed(self) -> None:
     self.subject.on_next(self)
