36f543a70f0f59fd236513bd1a7c204fda7a9557
Phil Harnish <philharnish@gmail.com>
2019-08-07 19:09:39 -0700

    Add a little test coverage for annotation_widget_spec.

---
+++ spec/puzzle/puzzlepedia/annotation_widget_spec.py
@@ -0,0 +1,23 @@
+from puzzle.constraints import constraints, validator
+from puzzle.puzzlepedia import annotation_widget
+from spec.mamba import *
+
+
+widget_patch = mock.patch('puzzle.puzzlepedia.annotation_widget.widgets')
+
+
+class TestConstraints(constraints.Constraints):
+  pass
+
+
+with description('annotation_widget'):
+  with before.each:
+    self.mock_widgets = widget_patch.start()
+
+  with after.each:
+    widget_patch.stop()
+
+  with it('renders NumberInRange as a FloatSlider'):
+    annotation_widget.AnnotationWidget(
+        validator.NumberInRange(0.0, 2.0), TestConstraints(), 'test', 1.0)
+    expect(self.mock_widgets.FloatSlider).to(have_been_called)
