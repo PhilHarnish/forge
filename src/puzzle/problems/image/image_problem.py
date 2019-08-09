9db0f0430466b9d4d70c7803f7d39ecdeb85e375
Phil Harnish <philharnish@gmail.com>
2019-08-09 12:05:57 -0700

    Update ImageProblem to use PrepareImage step.

--- src/puzzle/problems/image/image_problem.py
+++ src/puzzle/problems/image/image_problem.py
@@ -1,11 +1,21 @@
 import numpy as np
 
+from data.image import image
+from puzzle.constraints.image import prepare_image_constraints
 from puzzle.problems import problem
+from puzzle.steps.image import prepare_image
 
 
 class ImageProblem(problem.Problem):
+  _source_image: image.Image
+  _prepare_image: prepare_image.PrepareImage
+
   def __init__(self, name: str, data: np.ndarray, *args, **kwargs) -> None:
     super(ImageProblem, self).__init__(name, data, *args, **kwargs)
+    self._source_image = image.Image(data)
+    self._prepare_image = prepare_image.PrepareImage(
+        prepare_image_constraints.PrepareImageConstraints(), self._source_image)
+    self._solutions_generator.depends_on(self._prepare_image)
 
   @staticmethod
   def score(data: problem.ProblemData) -> float:
