f371a36b190a15fb13a2049c8e2186f912a0d885
Phil Harnish <philharnish@gmail.com>
2019-08-09 11:22:46 -0700

    Add a base class for image manipulation steps.

---
+++ spec/puzzle/steps/image/_base_image_step_spec.py
@@ -0,0 +1,49 @@
+import numpy as np
+
+from data.image import image
+from puzzle.constraints import constraints
+from puzzle.steps.image import _base_image_step
+from spec.mamba import *
+
+
+class TestConstraints(constraints.Constraints):
+  key: str = 'value'
+
+
+class TestImageStep(_base_image_step.BaseImageStep):
+  def _modify_result(self, result: image.Image) -> image.Image:
+    return result
+
+
+with description('_base_image_step') as self:
+  with before.each:
+    self.data = np.zeros((3, 3))
+    self.source = image.Image(self.data)
+
+  with description('constructor'):
+    with it('constructs without error'):
+      expect(calling(TestImageStep, self.source)).not_to(raise_error)
+
+  with description('get_result'):
+    with before.each:
+      self.constraints = TestConstraints()
+      self.step = TestImageStep(self.source, [self.constraints])
+
+    with it('returns an image'):
+      expect(self.step.get_result()).to(be_a(image.Image))
+
+    with it('returns the image if called repeatedly'):
+      first = self.step.get_result()
+      expect(self.step.get_result()).to(be(first))
+
+    with it('operates on a fork() of original image'):
+      expect(self.step.get_result()).not_to(be(self.source))
+
+    with it('returns a new result if constraints change'):
+      first = self.step.get_result()
+      self.constraints.key = 'changed'
+      expect(self.step.get_result()).not_to(be(first))
+
+  with it('provides debug data'):
+    expect(TestImageStep(self.source).get_debug_data().tolist()).to(
+        equal(self.data.tolist()))
---
+++ src/puzzle/steps/image/_base_image_step.py
@@ -0,0 +1,41 @@
+from typing import Any, NamedTuple, Optional
+
+import numpy as np
+
+from data.image import image
+from puzzle.steps import step
+
+
+class ImageChangeEvent(NamedTuple):
+  pass
+
+
+class BaseImageStep(step.Step):
+  _source: image.Image
+  _result: Optional[image.Image]
+
+  def __init__(self,
+      source: image.Image,
+      constraints: Optional[step.Constraints] = None) -> None:
+    super(BaseImageStep, self).__init__(constraints=constraints)
+    if constraints:
+      for constraint in constraints:
+        constraint.subscribe(self._on_constraints_changed)
+    self._source = source
+    self._result = None
+
+  def get_result(self) -> image.Image:
+    if self._result is None:
+      self._result = self._modify_result(self._source.fork())
+    return self._result
+
+  def get_debug_data(self) -> np.ndarray:
+    return self.get_result().get_debug_data()
+
+  def _modify_result(self, result: image.Image) -> image.Image:
+    raise NotImplementedError()
+
+  def _on_constraints_changed(self, change: Any) -> None:
+    del change
+    self._result = None
+    self._subject.on_next(ImageChangeEvent())
