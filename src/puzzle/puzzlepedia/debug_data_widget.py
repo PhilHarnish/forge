64a0aa054769a9bbfe0cfe9c8c4c75ed789aa378
Phil Harnish <philharnish@gmail.com>
2019-08-23 20:05:15 -0700

    Explicitly set width and height.

--- src/puzzle/puzzlepedia/debug_data_widget.py
+++ src/puzzle/puzzlepedia/debug_data_widget.py
@@ -20,8 +20,13 @@ def DebugDataWidget(s: step.Step) -> widgets.Widget:
 
 def _data_widget(data: Any) -> widgets.Widget:
   if isinstance(data, np.ndarray) and data.dtype == np.uint8:
+    width, height = data.shape[:2]
     f = io.BytesIO()
     Image.fromarray(data).save(f, 'png')
-    return widgets.Image(value=f.getvalue())
+    return widgets.Image(
+        value=f.getvalue(),
+        width=width,
+        height=height,
+    )
   # Fallback assumes data is text.
   return widgets.HTML(_common.preformat_html(str(data)))
