8fdc6f96f8594a2507e81819d640ade3be38de5f
Phil Harnish <philharnish@gmail.com>
2019-09-02 12:32:06 -0700

    Add custom repr formatter for np.ndarray.
    The default repr(np.ndarray) behavior is far too verbose.

--- spec/data/convert/repr_format_spec.py
+++ spec/data/convert/repr_format_spec.py
@@ -1,5 +1,6 @@
-from spec.mamba import *
+import numpy as np
 
+from spec.mamba import *
 
 with description('as_args'):
   with it('returns nothing for empty input'):
@@ -17,3 +18,32 @@ with description('as_args'):
     expect(repr_format.as_args(
         a=1, z=2, b=3, y=4, c=5, x=6,
     )).to(equal('a=1, b=3, c=5, x=6, y=4, z=2'))
+
+
+with description('repr_ndarray'):
+  with it('handles zeros'):
+    expect(repr_format.repr_ndarray(np.zeros((1, 2)))).to(equal(
+        'np.zeros((1, 2))'))
+
+  with it('handles ones'):
+    expect(repr_format.repr_ndarray(np.ones((3, 4)))).to(equal(
+        'np.ones((3, 4))'))
+
+  with it('handles modified ones'):
+    v = np.ones((3, 4))
+    v[0][0] = 0
+    expect(repr_format.repr_ndarray(v)).to(equal('np.array(shape=(3, 4))'))
+
+  with it('handles custom dtype'):
+    expect(repr_format.repr_ndarray(np.ones((3, 4), dtype=np.uint8))).to(equal(
+        'np.ones((3, 4), dtype=np.uint8)'))
+
+
+with description('repr_value'):
+  with it('reprs primitives'):
+    for v in (None, True, 1, 'string'):
+      expect(calling(repr_format.repr_value, v)).to(equal(repr(v)))
+
+  with it('reprs ndarrays'):
+    v = np.array([1, 2])
+    expect(repr_format.repr_value(v)).not_to(equal(repr(v)))
--- src/data/convert/repr_format.py
+++ src/data/convert/repr_format.py
@@ -1,10 +1,36 @@
 from typing import Any
 
+import numpy as np
+
+_DEFAULT_NDARRAY_DTYPE = np.array(()).dtype
+
 
 def as_args(*args: Any, **kwargs: Any) -> str:
   return ', '.join([
-                     repr(arg) for arg in args
+                     repr_value(arg) for arg in args
                    ] + [
-                     '%s=%s' % (key, repr(value)) for key, value in
+                     '%s=%s' % (key, repr_value(value)) for key, value in
                      sorted(kwargs.items(), key=lambda x: x[0])
                    ])
+
+
+def repr_ndarray(value: np.ndarray) -> str:
+  args = []
+  if np.all(value == 0):
+    fn = 'zeros'
+    args.append(repr(value.shape))
+  elif np.all(value == 1):
+    fn = 'ones'
+    args.append(repr(value.shape))
+  else:
+    fn = 'array'
+    args.append('shape=%s' % repr(value.shape))
+  if value.dtype != _DEFAULT_NDARRAY_DTYPE:
+    args.append('dtype=np.%s' % value.dtype)
+  return 'np.%s(%s)' % (fn, ', '.join(args))
+
+
+def repr_value(value: Any) -> str:
+  if isinstance(value, np.ndarray):
+    return repr_ndarray(value)
+  return repr(value)
