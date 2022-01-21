5e6b27ae9780f27c89d40c8008ececf99ba97216
Phil Harnish <philharnish@gmail.com>
2022-01-20 19:29:00 -0800

    Add node traversal to cursor.go.

--- src/data/graph/bloom/cursor.go
+++ src/data/graph/bloom/cursor.go
@@ -24,15 +24,50 @@ func NewCursor(root *Node) *Cursor {
 }
 
 func (cursor *Cursor) Get(path string) (*Cursor, error) {
-	for _, c := range path {
+	runes := []rune(path)
+	// Preprocess path to determine the requirements along path.
+	requirements := make([]BitMask, len(runes))
+	required := BitMask(0)
+	for i := len(runes) - 1; i >= 0; i-- {
+		mask, err := AlphabetMask(runes[i])
+		if err != nil {
+			return cursor, fmt.Errorf(
+				"%s traversal error for '%s': %w", cursor, path, err)
+		}
+		required |= mask
+		requirements[i] = required
+	}
+	node := cursor.node
+	for i, c := range path {
+		// Ensure requirements are met at this layer.
+		required := requirements[i]
+		if node.provideMask&required != required {
+			// Eventually one of our requested letters will be missing.
+			missing := required ^ cursor.node.provideMask&required
+			return cursor, fmt.Errorf(
+				"%s traversal error for '%s': '%s' not provided",
+				cursor, path, MaskAlphabet(missing, missing))
+		}
+		position, _ := Position(c)
+		// NB: Here we assume `links` is defined, `position` is valid,
+		// because the `provide` check has passed.
+		link := node.links[position]
+		if link == nil {
+			return cursor, fmt.Errorf(
+				"%s traversal error for '%s': '%c' not linked",
+				cursor, path, c)
+		}
+		if link.prefix != "" {
+			return cursor, fmt.Errorf(
+				"%s traversal error for '%s': '%s' has unsupported prefix '%s'",
+				cursor, path, link.node, link.prefix)
+		}
+		// Traversal successful, descend into `node` and continue looping.
+		node = link.node
 		cursor.path = append(cursor.path, string(c)...)
+		cursor.node = node
 	}
 	return cursor, nil
-}
-
-func (cursor *Cursor) Has(path string) bool {
-	_, err := cursor.Get(path)
-	return err == nil
 }
 
 func (cursor *Cursor) Select(path string) *Cursor {
--- src/data/graph/bloom/cursor_test.go
+++ src/data/graph/bloom/cursor_test.go
@@ -1,32 +1,94 @@
 package bloom_test
 
 import (
+	"errors"
+
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 	"github.com/philharnish/forge/src/data/graph/bloom"
 )
 
-var _ = Describe("Cursor",
+func extend(node *bloom.Node, path string) *bloom.Node {
+	runes := []rune(path)
+	for i := len(runes) - 1; i >= 0; i-- {
+		parent := bloom.NewNode()
+		parent.Link(string(runes[i]), node)
+		node = parent
+	}
+	return node
+}
+
+var _ = Describe("Cursor.NewNode",
 	func() {
 		It("Initially empty", func() {
-			node := bloom.NewNode()
+			node := extend(bloom.NewNode(1.0), "abc")
 			cursor := bloom.NewCursor(node)
-			Expect(cursor.String()).To(Equal("Cursor('', Node('', '', 0))"))
+			Expect(cursor.String()).To(Equal("Cursor('', Node('ABC', '   #', 0))"))
 		})
+	})
 
+var _ = Describe("Cursor.Get",
+	func() {
 		It("Moves to children", func() {
-			node := bloom.NewNode()
+			node := extend(bloom.NewNode(1.0), "abc")
 			cursor := bloom.NewCursor(node)
-			cursor.Get("abc")
-			Expect(cursor.String()).To(Equal("Cursor('abc', Node('', '', 0))"))
+			_, err := cursor.Get("abc")
+			Expect(err).ShouldNot(HaveOccurred())
+			Expect(cursor.String()).To(Equal("Cursor('abc', Node('', '#', 1))"))
 		})
 
 		It("Moves to children iteratively", func() {
-			node := bloom.NewNode()
+			node := extend(bloom.NewNode(1.0), "abc")
 			cursor := bloom.NewCursor(node)
 			for _, c := range "abc" {
-				cursor.Get(string(c))
+				_, err := cursor.Get(string(c))
+				Expect(err).ShouldNot(HaveOccurred())
 			}
-			Expect(cursor.String()).To(Equal("Cursor('abc', Node('', '', 0))"))
+			Expect(cursor.String()).To(Equal("Cursor('abc', Node('', '#', 1))"))
+		})
+
+		It("Raises error attempting to traverse illegal character", func() {
+			node := extend(bloom.NewNode(1.0), "abc")
+			cursor := bloom.NewCursor(node)
+			_, err := cursor.Get("🚫")
+			Expect(err).Should(MatchError(
+				"Cursor('', Node('ABC', '   #', 0)) traversal error for '🚫': '🚫' not supported",
+			))
+		})
+
+		It("Raises error attempting to traverse missing character", func() {
+			node := extend(bloom.NewNode(1.0), "abc")
+			cursor := bloom.NewCursor(node)
+			_, err := cursor.Get("xyz")
+			Expect(err).Should(MatchError(
+				"Cursor('', Node('ABC', '   #', 0)) traversal error for 'xyz': 'XYZ' not provided",
+			))
+		})
+
+		It("Raises error attempting to traverse out of order", func() {
+			node := extend(bloom.NewNode(1.0), "abc")
+			cursor := bloom.NewCursor(node)
+			_, err := cursor.Get("cba")
+			Expect(err).Should(MatchError(
+				"Cursor('', Node('ABC', '   #', 0)) traversal error for 'cba': 'c' not linked",
+			))
+		})
+	})
+
+var _ = Describe("Cursor.Select",
+	func() {
+		It("Moves to children", func() {
+			node := extend(bloom.NewNode(1.0), "abc")
+			cursor := bloom.NewCursor(node)
+			cursor.Select("abc")
+			Expect(cursor.String()).To(Equal("Cursor('abc', Node('', '#', 1))"))
+		})
+
+		It("Panics when performing an illegal selection", func() {
+			node := extend(bloom.NewNode(1.0), "abc")
+			cursor := bloom.NewCursor(node)
+			Expect(func() {
+				cursor.Select("xyz")
+			}).To(PanicWith(errors.New("path xyz not found on Node('ABC', '   #', 0)")))
 		})
 	})
