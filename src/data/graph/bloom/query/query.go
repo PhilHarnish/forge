6f59fa30107443031367a63ddac1a3a5a2f3fd28
Phil Harnish <philharnish@gmail.com>
2022-01-23 15:05:23 -0800

    Add "From" and "As" for selecting from a source.

--- src/data/graph/bloom/query/query.go
+++ src/data/graph/bloom/query/query.go
@@ -6,11 +6,50 @@ import (
 )
 
 type Query struct {
-	limit int
+	limit        int
+	namedSources map[string]QuerySource
+	sources      []*querySource
+	iterator     func() *QueryResult
 }
 
+type querySource struct {
+	name   string
+	source QuerySource
+}
+
+type QuerySource interface {
+	Next() *QueryResult
+	String() string
+}
+
+type QueryResult = struct{}
+
 func Select() *Query {
-	return &Query{}
+	return &Query{
+		namedSources: map[string]QuerySource{},
+	}
+}
+
+func (query *Query) As(name string) *Query {
+	if len(query.sources) == 0 {
+		panic("no sources available")
+	}
+	lastSource := query.sources[len(query.sources)-1]
+	if lastSource.name != "" {
+		panic(fmt.Sprintf(
+			"cannot name source %s '%s', already assigned name '%s'",
+			lastSource.source.String(), name, lastSource.name))
+	}
+	lastSource.name = name
+	query.namedSources[name] = lastSource.source
+	return query
+}
+
+func (query *Query) From(sources ...QuerySource) *Query {
+	for _, source := range sources {
+		query.sources = append(query.sources, &querySource{source: source})
+	}
+	return query
 }
 
 func (query *Query) Limit(count int) *Query {
@@ -18,13 +57,41 @@ func (query *Query) Limit(count int) *Query {
 	return query
 }
 
-func (query *Query) Next() *struct{} {
-	return nil
+func (query *Query) Next() *QueryResult {
+	if query.iterator == nil {
+		query.iterator = func() *QueryResult {
+			for _, source := range query.sources {
+				result := source.source.Next()
+				if result != nil {
+					return result
+				}
+			}
+			return nil
+		}
+	}
+
+	return query.iterator()
 }
 
 func (query *Query) String() string {
 	lines := []string{
 		"SELECT *",
+	}
+	if len(query.sources) == 1 && len(query.namedSources) == 0 {
+		// Special case for one unnamed source.
+		lines = append(lines,
+			fmt.Sprintf("FROM %s", query.sources[0].source.String()))
+	} else if len(query.namedSources)+len(query.sources) > 0 {
+		sources := []string{}
+		for _, source := range query.sources {
+			if source.name == "" {
+				sources = append(sources, fmt.Sprintf("\t%s", source.source.String()))
+			} else {
+				sources = append(sources, fmt.Sprintf("\t%s AS %s", source.source.String(), source.name))
+			}
+		}
+		lines = append(lines, "FROM")
+		lines = append(lines, strings.Join(sources, ",\n"))
 	}
 	if query.limit > 0 {
 		lines = append(lines, fmt.Sprintf("LIMIT %d", query.limit))
--- src/data/graph/bloom/query/query_test.go
+++ src/data/graph/bloom/query/query_test.go
@@ -5,6 +5,7 @@ import (
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
+	. "github.com/philharnish/forge/spec/matchers"
 	"github.com/philharnish/forge/src/data/graph/bloom/query"
 )
 
@@ -26,12 +27,87 @@ var _ = Describe("Select", func() {
 	})
 })
 
+type testSource struct {
+	name      string
+	exhausted bool
+}
+
+func (source *testSource) Next() *query.QueryResult {
+	source.exhausted = true
+	return nil
+}
+
+func (source *testSource) String() string {
+	return source.name
+}
+
+var _ = Describe("From", func() {
+	It("Adds 1 source", func() {
+		src := &testSource{name: "example"}
+		q := query.Select().From(src)
+		Expect(q.String()).To(LookLike(`
+			SELECT *
+			FROM example
+		`))
+	})
+
+	It("Adds multiple sources", func() {
+		q := query.Select().From(
+			&testSource{name: "example1"},
+			&testSource{name: "example2"},
+		)
+		Expect(q.String()).To(LookLike(`
+			SELECT *
+			FROM
+				example1,
+				example2
+		`))
+	})
+
+	It("Adds multiple named sources", func() {
+		q := query.Select().From(
+			&testSource{name: "example1"},
+		).As("A").From(
+			&testSource{name: "example2"},
+		).As("B")
+		Expect(q.String()).To(LookLike(`
+			SELECT *
+			FROM
+				example1 AS A,
+				example2 AS B
+		`))
+	})
+
+	It("Rejects naming zero sources", func() {
+		q := query.Select()
+		Expect(func() {
+			q.As("A")
+		}).Should(Panic())
+	})
+
+	It("Rejects duplicate names", func() {
+		q := query.Select().From(
+			&testSource{name: "example1"},
+		).As("A")
+		Expect(func() {
+			q.As("B")
+		}).Should(Panic())
+	})
+
+	It("Reads from 1 source", func() {
+		src := &testSource{name: "example"}
+		q := query.Select().From(src)
+		q.Next()
+		Expect(src.exhausted).To(BeTrue())
+	})
+})
+
 var _ = Describe("Limits", func() {
 	It("Sets a count limit", func() {
 		q := query.Select().Limit(30)
-		Expect(q.String()).To(Equal(
-			"SELECT *\n" +
-				"LIMIT 30",
-		))
+		Expect(q.String()).To(LookLike(`
+			SELECT *
+			LIMIT 30
+		`))
 	})
 })
