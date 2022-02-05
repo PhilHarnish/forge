de8f8ea3b88b42c2cb1504c181e930a8ae4903e0
Phil Harnish <philharnish@gmail.com>
2022-02-05 14:49:13 -0800

    Implement a read-only "null" node.
    This will be used for dead-ends.

---
+++ src/data/graph/bloom/null/null.go
@@ -0,0 +1,29 @@
+package null
+
+import "github.com/philharnish/forge/src/data/graph/bloom/node"
+
+var nullNode = node.NewNode()
+
+type null struct{}
+
+var Null = &null{}
+
+func (root *null) Root() *node.Node {
+	return nullNode
+}
+
+func (root *null) Items(acceptor node.NodeAcceptor) node.NodeItems {
+	return root
+}
+
+func (root *null) HasNext() bool {
+	return false
+}
+
+func (root *null) Next() (string, node.NodeIterator) {
+	panic("Null has no children")
+}
+
+func (root *null) String() string {
+	return "Null()"
+}
