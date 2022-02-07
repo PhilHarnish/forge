bf8ef83dd2eea3e19906589ac70f1fcfa638279c
Phil Harnish <philharnish@gmail.com>
2022-02-07 07:59:29 -0800

    Implement a multi-source query results iterator.

---
+++ src/data/graph/bloom/query/queryresultsparallel.go
@@ -0,0 +1,62 @@
+package query
+
+import "container/heap"
+
+type queryResultsParallel struct {
+	query    *Query
+	returned []string
+	heap     queryResultHeap
+}
+
+type queryResultHeap []QueryResult
+
+func (results *queryResultsParallel) HasNext() bool {
+	return (results.query.limit == 0 || len(results.returned) < results.query.limit) &&
+		len(results.getHeap()) > 0
+}
+
+func (results *queryResultsParallel) Next() QueryResult {
+	panic("Not implemented")
+}
+
+func (results *queryResultsParallel) getHeap() queryResultHeap {
+	if results.heap == nil {
+		results.heap = queryResultHeap{}
+		for _, source := range results.query.sources {
+			sourceResults := source.Results()
+			for sourceResults.HasNext() {
+				results.heap = append(results.heap, sourceResults.Next())
+			}
+		}
+	}
+	heap.Init(&results.heap)
+	return results.heap
+}
+
+func (h queryResultHeap) Len() int {
+	return len(h)
+}
+
+func (h queryResultHeap) Less(i int, j int) bool {
+	return h[i].Weight > h[j].Weight
+}
+
+func (h queryResultHeap) Swap(i int, j int) {
+	h[i], h[j] = h[j], h[i]
+}
+
+func (h *queryResultHeap) Push(item interface{}) {
+	*h = append(*h, item.(QueryResult))
+}
+
+func (h *queryResultHeap) Pop() interface{} {
+	original := *h
+	end := len(original) - 1
+	result := original[end]
+	*h = original[:end]
+	return result
+}
+
+func (h *queryResultHeap) Next() QueryResult {
+	return heap.Pop(h).(QueryResult)
+}
---
+++ src/data/graph/bloom/query/queryresultsparallel_test.go
@@ -0,0 +1,31 @@
+package query_test
+
+import (
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	"github.com/philharnish/forge/spec/matchers"
+	"github.com/philharnish/forge/src/data/graph/bloom/query"
+	"github.com/philharnish/forge/src/data/graph/bloom/weight"
+)
+
+var _ = Describe("QueryResults", func() {
+	It("Reads from 1 source", func() {
+		src := &testSource{
+			name: "example",
+			results: []weight.WeightedString{
+				{
+					Weight: 0.0,
+					String: "result",
+				},
+			},
+		}
+		q := query.Select().From(src)
+		Expect(q.String()).To(matchers.LookLike(`
+		SELECT *
+		FROM example;
+		Score | example
+		---------------
+		0.00  | result
+		`))
+	})
+})
