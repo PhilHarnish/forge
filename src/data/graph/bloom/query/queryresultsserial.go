ed4f10afb947a5726b63c61cc723a232dd8676d5
Phil Harnish <philharnish@gmail.com>
2022-02-07 07:55:21 -0800

    Implement a single-source query results iterator.

---
+++ src/data/graph/bloom/query/queryresultsserial.go
@@ -0,0 +1,24 @@
+package query
+
+type queryResultsSerial struct {
+	query    *Query
+	returned int
+	results  QueryResults
+}
+
+func (results *queryResultsSerial) HasNext() bool {
+	return (results.query.limit == 0 || results.returned < results.query.limit) &&
+		results.getResults().HasNext()
+}
+
+func (results *queryResultsSerial) Next() QueryResult {
+	results.returned++
+	return results.results.Next()
+}
+
+func (results *queryResultsSerial) getResults() QueryResults {
+	if results.results == nil {
+		results.results = results.query.sources[0].Results()
+	}
+	return results.results
+}
