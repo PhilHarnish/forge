a96e93838b1a284d4f546eb39afc460acaa410f9
Phil Harnish <philharnish@gmail.com>
2022-02-12 21:38:15 -0800

    Delete unused WeightedStrings (plural).

--- src/data/graph/bloom/weight/weight.go
+++ src/data/graph/bloom/weight/weight.go
@@ -1,30 +1,10 @@
 package weight
 
-import (
-	"fmt"
-	"strings"
-)
-
 type Weight = float64
 
 type WeightedString struct {
 	Weight Weight
 	String string
-}
-
-type WeightedStrings struct {
-	Weight  Weight
-	Strings []string
-}
-
-func (ws *WeightedStrings) String() string {
-	var joined string
-	if len(ws.Strings) == 0 {
-		joined = "∅"
-	} else {
-		joined = strings.Join(ws.Strings, "\t")
-	}
-	return fmt.Sprintf("%.2f\t%s", ws.Weight, joined)
 }
 
 func CumulativeWeight(strings []WeightedString) Weight {
--- src/data/graph/bloom/weight/weight_test.go
+++ src/data/graph/bloom/weight/weight_test.go
@@ -13,23 +13,6 @@ func Test(t *testing.T) {
 	RunSpecs(t, "Tests")
 }
 
-var _ = Describe("WeightedStrings::String", func() {
-	It("begins empty", func() {
-		ws := weight.WeightedStrings{}
-		Expect(ws.String()).To(Equal("0.00\t∅"))
-	})
-
-	It("formats 1 string", func() {
-		ws := weight.WeightedStrings{Strings: []string{"example"}}
-		Expect(ws.String()).To(Equal("0.00\texample"))
-	})
-
-	It("formats multiple strings", func() {
-		ws := weight.WeightedStrings{Strings: []string{"a", "b", "c"}}
-		Expect(ws.String()).To(Equal("0.00\ta\tb\tc"))
-	})
-})
-
 var _ = Describe("CumulativeWeight", func() {
 	It("returns 0 for nil", func() {
 		Expect(weight.CumulativeWeight(nil)).To(Equal(0.0))
