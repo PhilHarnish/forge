4843c5ddff91d72e1b37f253561181b5f96e3980
Phil Harnish <philharnish@gmail.com>
2022-03-12 22:17:33 -0800

    Split out retrielink code.

---
+++ src/data/graph/bloom/retrie/retrielink.go
@@ -0,0 +1,71 @@
+package retrie
+
+import (
+	"container/heap"
+	"unicode/utf8"
+
+	"github.com/philharnish/forge/src/data/graph/bloom/mask"
+)
+
+type reTrieLink struct {
+	prefix   string
+	runes    []rune
+	node     *reTrieNode
+	edgeMask mask.Mask
+}
+
+func newReTrieLinkFromRunes(runes []rune, node *reTrieNode) *reTrieLink {
+	edgeMask, err := mask.AlphabetMaskRanges(runes)
+	if err != nil {
+		panic(err)
+	}
+	return &reTrieLink{
+		runes:    runes,
+		node:     node,
+		edgeMask: edgeMask,
+	}
+}
+
+func newReTrieLinkForPrefix(prefix string, node *reTrieNode) *reTrieLink {
+	prefixRune, _ := utf8.DecodeRuneInString(prefix)
+	edgeMask, _ := mask.AlphabetMask(prefixRune)
+	return &reTrieLink{
+		prefix:   prefix,
+		runes:    []rune{prefixRune, prefixRune},
+		node:     node,
+		edgeMask: edgeMask,
+	}
+}
+
+type reTrieLinkList []*reTrieLink
+
+func (edges reTrieLinkList) Len() int {
+	return len(edges)
+}
+
+func (edges reTrieLinkList) Less(i int, j int) bool {
+	if edges[i].runes[0] == edges[j].runes[0] {
+		return edges[i].runes[1] < edges[j].runes[1]
+	}
+	return edges[i].runes[0] < edges[j].runes[0]
+}
+
+func (h reTrieLinkList) Swap(i int, j int) {
+	h[i], h[j] = h[j], h[i]
+}
+
+func (h *reTrieLinkList) Push(item interface{}) {
+	*h = append(*h, item.(*reTrieLink))
+}
+
+func (h *reTrieLinkList) Pop() interface{} {
+	original := *h
+	end := len(original) - 1
+	result := original[end]
+	*h = original[:end]
+	return result
+}
+
+func (h *reTrieLinkList) Next() *reTrieLink {
+	return heap.Pop(h).(*reTrieLink)
+}
--- src/data/graph/bloom/retrie/retrienode.go
+++ src/data/graph/bloom/retrie/retrienode.go
@@ -3,7 +3,6 @@ package retrie
 import (
 	"container/heap"
 	"unicode"
-	"unicode/utf8"
 
 	"github.com/philharnish/forge/src/data/graph/bloom/mask"
 	"github.com/philharnish/forge/src/data/graph/bloom/node"
@@ -17,38 +16,6 @@ type reTrieNode struct {
 	edgeMask    mask.Mask
 	overlapping mask.Mask
 }
-
-type reTrieLink struct {
-	prefix   string
-	runes    []rune
-	node     *reTrieNode
-	edgeMask mask.Mask
-}
-
-func newReTrieLinkFromRunes(runes []rune, node *reTrieNode) *reTrieLink {
-	edgeMask, err := mask.AlphabetMaskRanges(runes)
-	if err != nil {
-		panic(err)
-	}
-	return &reTrieLink{
-		runes:    runes,
-		node:     node,
-		edgeMask: edgeMask,
-	}
-}
-
-func newReTrieLinkForPrefix(prefix string, node *reTrieNode) *reTrieLink {
-	prefixRune, _ := utf8.DecodeRuneInString(prefix)
-	edgeMask, _ := mask.AlphabetMask(prefixRune)
-	return &reTrieLink{
-		prefix:   prefix,
-		runes:    []rune{prefixRune, prefixRune},
-		node:     node,
-		edgeMask: edgeMask,
-	}
-}
-
-type reTrieLinkList []*reTrieLink
 
 func newReTrieNode(directory *reTrieDirectory, id dfaId, root *node.Node) *reTrieNode {
 	return &reTrieNode{
@@ -261,37 +228,6 @@ func (root *reTrieNode) splitEdges() {
 		parent.mergeNode(newNodes[i+2])
 	}
 	root.overlapping = 0
-}
-
-func (edges reTrieLinkList) Len() int {
-	return len(edges)
-}
-
-func (edges reTrieLinkList) Less(i int, j int) bool {
-	if edges[i].runes[0] == edges[j].runes[0] {
-		return edges[i].runes[1] < edges[j].runes[1]
-	}
-	return edges[i].runes[0] < edges[j].runes[0]
-}
-
-func (h reTrieLinkList) Swap(i int, j int) {
-	h[i], h[j] = h[j], h[i]
-}
-
-func (h *reTrieLinkList) Push(item interface{}) {
-	*h = append(*h, item.(*reTrieLink))
-}
-
-func (h *reTrieLinkList) Pop() interface{} {
-	original := *h
-	end := len(original) - 1
-	result := original[end]
-	*h = original[:end]
-	return result
-}
-
-func (h *reTrieLinkList) Next() *reTrieLink {
-	return heap.Pop(h).(*reTrieLink)
 }
 
 func min(a rune, b rune) rune {
