0296a70f51a71aa0a65033f90cb70963a07bd033
Phil Harnish <philharnish@gmail.com>
2022-05-28 15:14:51 -0700

    Add Subscribe() to NodeGenerator.

--- src/data/graph/bloom/node/nodegenerator.go
+++ src/data/graph/bloom/node/nodegenerator.go
@@ -5,10 +5,14 @@ type nodeGenerator struct{}
 
 type NodeGenerator interface {
 	Items(iterator NodeIterator) NodeItems
+	Subscribe(setter NodeStateUpdater)
 }
 
 var NodeGenerateAll *nodeGenerator = nil
 
 func (generator *nodeGenerator) Items(iterator NodeIterator) NodeItems {
 	return iterator.Items(generator)
+}
+
+func (generator *nodeGenerator) Subscribe(setter NodeStateUpdater) {
 }
